bd921a1d1d8b8295beb24d902b6ab9441231b36c
mahsanghani <ghaniahsan@outlook.com>
2022-06-27 11:00:03 -0700

    errors

---
+++ basics/errors.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Ahsan Ghani on 2022-06-27.
+//
+#include <iostream>
+
+int main(){
+
+    //Compile time error
+    std::cout << "Hello Word!" << std::endl;
+
+    //Run time error
+    int value = 7/0;
+    std::cout << "value : " << value << std::endl;
+
+    return 0;
+}
