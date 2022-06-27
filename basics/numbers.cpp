dc24ec238b5ec2d52c3d67743c83969a24f5623d
mahsanghani <ghaniahsan@outlook.com>
2022-06-27 10:58:26 -0700

    numbers

---
+++ basics/numbers.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Ahsan Ghani on 2022-06-27.
+//
+
+#include <iostream>
+
+int main(){
+
+    std::cout << "Number1" << std::endl;
+    std::cout << "Number2" << std::endl;
+    std::cout << "Number3" << std::endl;
+
+    return 0;
+}
