2f56312fc592593d17f38bf9cf7c23e36a6f8833
mahsanghani <ghaniahsan@outlook.com>
2022-06-27 11:03:08 -0700

    printing

---
+++ basics/printing.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Ahsan Ghani on 2022-06-27.
+//
+#include <iostream>
+#include <string>
+
+int main(){
+//    Printing data
+    std::cout << "Hello C++20 " << std::endl;
+    int age{21};
+    std::cout << "Age : " << age << std::endl;
+    std::cerr << "Error message : Something is wrong" << std::endl;
+    std::clog << "Log message : Something happened " << std::endl;
+    return 0;
+}
