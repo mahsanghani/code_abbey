aad9bb9ef1a16fb6025c6f5a31c77883f9ff14dc
mahsanghani <ghaniahsan@outlook.com>
2022-06-28 07:21:02 -0700

    booleans

---
+++ basics/booleans.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Ahsan Ghani on 2022-06-28.
+//
+#include <iostream>
+
+
+int main(){
+
+    bool red_light {false};
+    bool green_light{true};
+
+    if(red_light == true){
+        std::cout << "Stop!" << std::endl;
+    }else{
+        std::cout << "Go through!" << std::endl;
+    }
+
+    if(green_light){
+        std::cout << "The light is green!" << std::endl;
+    }else{
+        std::cout << "The light is NOT green!" << std::endl;
+    }
+    std::cout << "sizeof(bool) : " << sizeof(bool) << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "red_light : " << red_light << std::endl;
+    std::cout << "green_light : " << green_light << std::endl;
+
+    std::cout << std::boolalpha;
+    std::cout << "red_light : " << red_light << std::endl;
+    std::cout << "green_light : " << green_light << std::endl;
+
+    return 0;
+}
