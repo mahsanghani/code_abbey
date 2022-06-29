8145f05a11a67674888f482e51f58c8b804b7170
mahsanghani <ghaniahsan@outlook.com>
2022-06-29 11:31:27 -0700

    loop do while

---
+++ basics/loopdowhile.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Ahsan Ghani on 2022-06-29.
+//
+#include <iostream>
+
+int main(){
+    //Print I love C++ 10 times
+    std::cout << "I love C++" << std::endl;
+    std::cout << "I love C++" << std::endl;
+    std::cout << "I love C++" << std::endl;
+    std::cout << "I love C++" << std::endl;
+    std::cout << "I love C++" << std::endl;
+    std::cout << "I love C++" << std::endl;
+    std::cout << "I love C++" << std::endl;
+    std::cout << "I love C++" << std::endl;
+    std::cout << "I love C++" << std::endl;
+    std::cout << "I love C++" << std::endl;
+    const int COUNT{0};
+    size_t i{0}; // Iterator declaration
+
+    do{
+        std::cout << i << " : I love C++" << std::endl;
+        ++i; // Incrementation
+    }while( i < COUNT);
+
+    std::cout << "Loop done!" << std::endl;
+
+    return 0;
+}
