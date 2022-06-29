d19d8109fde640460c489c849b5052fd4eec1187
mahsanghani <ghaniahsan@outlook.com>
2022-06-29 09:35:07 -0700

    flow switch scope

--- basics/flowswitchscope.cpp
+++ basics/flowswitchscope.cpp
@@ -2,20 +2,15 @@
 // Created by Ahsan Ghani on 2022-06-29.
 //
 #include <iostream>
-
 int main(){
-
-    int condition{0};
-
+    int condition{2};
     int my_var{4};
-
     switch (int data{7}; condition)
     {
 //      int x{9};
-//      Never going to run
+// Never going to run
         int x;
         case 0 :
-
             //int y {5};
             int y;
             x = 6;
@@ -25,7 +20,6 @@ int main(){
             std::cout << "Statement1" << std::endl;
             std::cout << "Statement2" << std::endl;
             break;
-
         case 1 :
             int z;
             my_var++;
@@ -34,10 +28,12 @@ int main(){
             std::cout << "Statement3" << std::endl;
             std::cout << "Statement4" << std::endl;
             break;
-
         default:
             int u;
             z = 10;
+            std::cout << "x : " << x << std::endl;
+            std::cout << "y : " << y << std::endl;
+            std::cout << "z : " << z << std::endl;
             std::cout << "Statement5" << std::endl;
             std::cout << "Statement6" << std::endl;
             break;
