c4bec3ed368708bbe21813f85afe1ff73e836e36
mahsanghani <ghaniahsan@outlook.com>
2022-06-29 11:48:41 -0700

    loop infinite practice

--- basics/loopinfinite.cpp
+++ basics/loopinfinite.cpp
@@ -4,25 +4,23 @@
 #include <iostream>
 int main(){
     //Infinite loop : for loop
-    /*
     for(size_t i{};true ; ++i){
         std::cout << i <<  " : I love C++" << std::endl;
     }
-    */
 
     //Infinite loop : while loop
-    /*
     size_t i{0};
     while(true){
         std::cout << i  << " : I love C++" << std::endl;
         ++i;
     }
-    */
+
     //Infinite loop : do while loop
-    size_t i{0};
+    size_t j{0};
     do{
-        std::cout << i  << " : I love C++" << std::endl;
-        ++i;
+        std::cout << j  << " : I love C++" << std::endl;
+        ++j;
     }while(true);
+
     return 0;
 }
