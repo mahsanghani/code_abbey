ccee7ac8681e8d94767390ce9cc70b4b3de3872b
mahsanghani <ghaniahsan@outlook.com>
2022-06-30 20:20:49 -0700

    declaring and using pointers

--- intermediate/pointersnew.cpp
+++ intermediate/pointersnew.cpp
@@ -37,27 +37,24 @@ int main(){
     //We know that pointers store addresses of variables
     int int_var {43};
     int *p_int{&int_var};// The address of operator (&);
-
     std::cout << "Int var : " << int_var << std::endl;
     std::cout << "p_int (Address in memory)  : " << p_int << std::endl;
 
     //You can also change the address stored in a pointer any time
     int int_var1 {65};
     p_int = &int_var1; // Assign a different address to the pointer
+    std::cout << "Int var : " << int_var1 << std::endl;
     std::cout << "p_int (with different address)  : " << p_int << std::endl;
 
     //Can't cross assign between pointers of different types
     int *p_int1{nullptr};
     double double_var{33};
-
     //p_int = &double_var; // Compiler error
 
     //Dereferencing a pointer :
     int* p_int2 {nullptr};
     int int_data {56};
     p_int2 = &int_data;
-
     std::cout << "value : " << *p_int2 << std::endl; // Dereferencing a pointer
-
     return 0;
 }
