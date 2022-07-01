07f75c219065f9258cad7831f94b187bbc9ad27f
mahsanghani <ghaniahsan@outlook.com>
2022-06-30 20:28:03 -0700

    pointers for characters

---
+++ intermediate/pointers for characters.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Ahsan Ghani on 2022-06-30.
+//
+#include <iostream>
+
+
+int main(){
+
+    const char * message {"Hello World!"};
+    std::cout << "message : " << message << std::endl;
+
+    //*message = "B"; // Compiler error
+    std::cout << "*message : " << *message << std::endl;
+
+    //Allow users to modify the string
+    char message1[] {"Hello World!"};
+    message1[0] = 'B';
+    std::cout << "message1 : " << message1 << std::endl;
+
+    return 0;
+}
