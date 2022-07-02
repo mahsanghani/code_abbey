6286f72a9de914a2b3d188d19c93a77328c6a632
mahsanghani <ghaniahsan@outlook.com>
2022-07-02 15:03:22 -0700

    insert after a node in a linked list

--- DSA/LinkedList/insert.cpp
+++ DSA/LinkedList/insert.cpp
@@ -16,3 +16,14 @@ void push(Node** head_ref, int new_data){
     new_node->next = (*head_ref);
     (*head_ref) = new_node;
 }
+
+void insertAfter(Node* prev_node, int new_data){
+    if(prev_node==NULL){
+        cout<<"The given previous node cannot be NULL"<<endl;
+        return;
+    }
+    Node* new_node = new Node;
+    new_node->data = new_data;
+    new_node->next = prev_node->next;
+    prev_node->next = new_node;
+}
