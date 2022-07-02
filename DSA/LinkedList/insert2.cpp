43368b55baf44db26d8922de94240f727947696e
mahsanghani <ghaniahsan@outlook.com>
2022-07-02 15:43:06 -0700

    insert after

--- DSA/LinkedList/insert2.cpp
+++ DSA/LinkedList/insert2.cpp
@@ -20,3 +20,21 @@ void insertahead(node*& head, int value){
     n->next = head;
     head = n;
 }
+
+void insertafter(node* head, int key, int val){
+    node* n = new node(val);
+    if (key == head->data){
+        n->next = head->next;
+        head->next = n;
+        return;
+    }
+    node* temp = head;
+    while(temp->data != key){
+        temp = temp->next;
+        if(temp==NULL){
+            return;
+        }
+    }
+    n->next = temp->next;
+    temp->next = n;
+}
