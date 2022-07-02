6bb165d1f94b019b1e93f8aa95a23db2d42de9e5
mahsanghani <ghaniahsan@outlook.com>
2022-07-02 15:47:49 -0700

    print function

--- DSA/LinkedList/insert2.cpp
+++ DSA/LinkedList/insert2.cpp
@@ -52,3 +52,12 @@ void insertlast(node*& head, int val){
     temp->next = n;
 }
 
+void print(node*& head){
+    node* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<" -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
