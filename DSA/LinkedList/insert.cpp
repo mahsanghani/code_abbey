7550f4bf761568de4114eabc30b12dac8ba185c0
mahsanghani <ghaniahsan@outlook.com>
2022-07-02 14:59:00 -0700

    pushing a linked list

---
+++ DSA/LinkedList/insert.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Ahsan Ghani on 2022-07-02.
+//
+void push(Node** head_ref, int new_data){
+    Node* new_node = new Node();
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
