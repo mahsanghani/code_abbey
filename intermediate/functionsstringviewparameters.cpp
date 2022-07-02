b72821002cb1bbcd2b7eede33156cda027b21a19
mahsanghani <ghaniahsan@outlook.com>
2022-07-02 11:41:00 -0700

    functions string view parameters

---
+++ intermediate/functionsstringviewparameters.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Ahsan Ghani on 2022-07-02.
+//
+#include <iostream>
+#include <string_view>
+
+void say_my_name(std::string_view name){
+    std::cout << "Hello your name is : " << name << std::endl;
+}
+
+int main(){
+
+    std::string some_name{"John"};
+    say_my_name("John");
+    say_my_name(some_name);
+    say_my_name(std::string_view("Samuel"));
+
+    return 0;
+}
