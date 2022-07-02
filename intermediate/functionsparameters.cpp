ce6330fb95d7e648ec150af373723362ec689957
mahsanghani <ghaniahsan@outlook.com>
2022-07-02 11:16:36 -0700

    functions sized parameters

--- intermediate/functionsparameters.cpp
+++ intermediate/functionsparameters.cpp
@@ -9,9 +9,9 @@ double sum (double* scores , size_t count){
 
     double score_sum{};
 
-    // std::cout << "sizeof(scores)  : " << sizeof(scores) << std::endl;
-    // std::cout << "sizeof(int*) : " << sizeof (int *) << std::endl;
-    //size_t arr_size = std::size(scores);
+    std::cout << "sizeof(scores)  : " << sizeof(scores) << std::endl;
+    std::cout << "sizeof(int*) : " << sizeof (int *) << std::endl;
+    // size_t arr_size = std::size(scores);
 
     for(size_t i{0} ; i < count ; ++i){
         score_sum += scores[i];
@@ -21,8 +21,7 @@ double sum (double* scores , size_t count){
 
 int main(){
     double my_scores[]{10.5,34.3,4.8,6.5};
-    //std::cout << "sizeof(my_scores) : " << sizeof(my_scores) << std::endl;
-
+    std::cout << "sizeof(my_scores) : " << sizeof(my_scores) << std::endl;
 
     double result = sum(my_scores,std::size(my_scores));
     std::cout << "result : " << result << std::endl;
