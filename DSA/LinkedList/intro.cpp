732dae28940c0be4771755649ddfcda8f65e59ec
mahsanghani <ghaniahsan@outlook.com>
2022-07-02 14:41:09 -0700

    linked list introduction

---
+++ DSA/LinkedList/intro.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Ahsan Ghani on 2022-07-02.
+//
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+};
+
+int main(){
+    Node* head = NULL;
+    Node* second = NULL;
+    Node* third = NULL;
+
+    head = new Node;
+    second = new Node;
+    third = new Node;
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    return 0;
+}
