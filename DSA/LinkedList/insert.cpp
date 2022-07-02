4fb830e8248f7ce98d41e620e0e5edcb16ee49c2
mahsanghani <ghaniahsan@outlook.com>
2022-07-02 15:13:45 -0700

    append after last node in a linked list

--- DSA/LinkedList/insert.cpp
+++ DSA/LinkedList/insert.cpp
@@ -27,3 +27,21 @@ void insertAfter(Node* prev_node, int new_data){
     new_node->next = prev_node->next;
     prev_node->next = new_node;
 }
+
+void append(Node** head_ref, int new_data){
+    Node* new_node = new Node();
+    Node *last = *head_ref;
+    new_node->data = new_data;
+    new_node->next = NULL;
+
+    if(*head_ref == NULL){
+        *head_ref = new_node;
+        return;
+    }
+    while(last->next != NULL){
+        last = last->next;
+    }
+
+    last->next = new_node;
+    return;
+}
