68005dd4cc31e9fa410510164f4a79121e9f492b
mahsanghani <ghaniahsan@outlook.com>
2022-07-02 14:59:39 -0700

    pushing a node in a linked list

--- DSA/LinkedList/insert.cpp
+++ DSA/LinkedList/insert.cpp
@@ -1,6 +1,15 @@
 //
 // Created by Ahsan Ghani on 2022-07-02.
 //
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+};
+
 void push(Node** head_ref, int new_data){
     Node* new_node = new Node();
     new_node->data = new_data;
