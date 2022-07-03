9ba072be31ba959b399296bc66be00d7970efacf
mahsanghani <ghaniahsan@outlook.com>
2022-07-03 09:14:05 -0700

    binary search tree introduction

---
+++ DSA/Trees/intro.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Ahsan Ghani on 2022-07-03.
+//
+struct node
+{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+class Node
+{
+public:
+    int data;
+    Node* left;
+    Node* right;
+};
