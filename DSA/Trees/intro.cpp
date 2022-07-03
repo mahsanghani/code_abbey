b97cf19a238ccf9ad2d313a950b69eceda32d2f6
mahsanghani <ghaniahsan@outlook.com>
2022-07-03 09:18:09 -0700

    binary search tree introduction

--- DSA/Trees/intro.cpp
+++ DSA/Trees/intro.cpp
@@ -1,6 +1,9 @@
 //
 // Created by Ahsan Ghani on 2022-07-03.
 //
+#include <iostream>
+using namespace std;
+
 struct node
 {
     int data;
@@ -14,4 +17,20 @@ public:
     int data;
     Node* left;
     Node* right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
 };
+
+int main()
+{
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    return 0;
+}
