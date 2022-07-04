2656e939acb6bce449b2e8530cf96d02e9796f1e
mahsanghani <ghaniahsan@outlook.com>
2022-07-04 07:12:32 -0700

    overloading with constant parameters by value

---
+++ intermediate/overloadingconstvalue.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Ahsan Ghani on 2022-07-04.
+//
+#include <iostream>
+
+/*
+int max(int a, int b){
+    return (a > b)? a : b;
+}
+*/
+
+int max( int a,  int b);
+
+int main(){
+    return 0;
+}
+
+int max( int a,  int b){
+    ++a;
+    return (a > b)? a : b;
+}
