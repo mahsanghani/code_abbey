603d90a45c55d9ab41dac4ccec21ad79747303ba
mahsanghani <ghaniahsan@outlook.com>
2022-07-03 22:01:59 -0700

    using arguments

---
+++ intermediate/argumentsusing.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Ahsan Ghani on 2022-07-03.
+//
+#include <iostream>
+
+
+int main(int argc , char * argv[]){
+
+    std::cout << "We have " << argc << " parameters in our program" << std::endl;
+
+    for(size_t i {0}; i < argc ; ++i){
+        std::cout << "parameter [" << i << "] :" <<  argv[i] << std::endl;
+    }
+
+    return 0;
+}
