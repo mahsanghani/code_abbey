dfc1e2bae294bfe330a7c97e7bebefa41391b450
mahsanghani <ghaniahsan@outlook.com>
2022-07-03 22:02:41 -0700

    calculator v1

---
+++ intermediate/calculatorv1.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Ahsan Ghani on 2022-07-03.
+//
+#include <iostream>
+
+int main(){
+
+    double first_number {5.0};
+    double second_number {33.1};
+    char c{'+'};
+
+    switch(c){
+        case '+':
+            std::cout << first_number << " + " << second_number << " = "
+                      << first_number + second_number << std::endl;
+            break;
+
+        case '-':
+            std::cout << first_number << " - " << second_number << "="
+                      << first_number - second_number << std::endl;
+            break;
+
+        case 'x':
+            std::cout << first_number << " * " << second_number << " = "
+                      << first_number * second_number << std::endl;
+            break;
+
+        case '/':
+            std::cout << first_number << " / " << second_number << " = "
+                      << first_number / second_number << std::endl;
+            break;
+
+    }
+    return 0;
+}
