4cc63fbeca7013574b640761ca6748f7b29eadd2
mahsanghani <ghaniahsan@outlook.com>
2022-07-04 10:03:30 -0700

    recursive function call

---
+++ intermediate/functionsrecursive.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Ahsan Ghani on 2022-07-04.
+//
+#include <iostream>
+
+size_t sum_up_to_zero(size_t value){
+    if(value!=0)
+        return value + sum_up_to_zero(value-1);
+    return 0;
+}
+
+int main(){
+
+    std::cout << "result : " << sum_up_to_zero(10) << std::endl;
+
+    return 0;
+}
