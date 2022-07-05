771ba468ba4b16d55f45b2b77b724c22cd59ea9d
mahsanghani <ghaniahsan@outlook.com>
2022-07-05 12:31:12 -0700

    new node

--- DSA/Trees/seriailze.cpp
+++ DSA/Trees/seriailze.cpp
@@ -9,3 +9,12 @@ struct Node
     int key;
     struct Node* left, *right;
 };
+
+struct Node* newNode(int key)
+{
+    struct Node* temp = new Node();
+    temp->key = key;
+    temp->left = NULL;
+    temp->right = NULL;
+    return (temp);
+}
