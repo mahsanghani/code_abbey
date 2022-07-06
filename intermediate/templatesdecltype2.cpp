cd292d7441f1b98470a5b15605c80fb860778424
mahsanghani <ghaniahsan@outlook.com>
2022-07-06 08:32:53 -0700

    using namespace std

--- intermediate/templatesdecltype2.cpp
+++ intermediate/templatesdecltype2.cpp
@@ -3,6 +3,7 @@
 //
 #include <iostream>
 #include <type_traits>
+using namespace std;
 
 struct A { double x; };
 const A* a;
@@ -18,17 +19,17 @@ auto add(T t, U u) -> decltype(t + u) // return type depends on template paramet
 }
 
 const int& getRef(const int* p) { return *p; }
-static_assert(std::is_same_v<decltype(getRef), const int&(const int*)>);
+static_assert(is_same_v<decltype(getRef), const int&(const int*)>);
 auto getRefFwdBad(const int* p) { return getRef(p); }
-static_assert(std::is_same_v<decltype(getRefFwdBad), int(const int*)>,
+static_assert(is_same_v<decltype(getRefFwdBad), int(const int*)>,
               "Just returning auto isn't perfect forwarding.");
 decltype(auto) getRefFwdGood(const int* p) { return getRef(p); }
-static_assert(std::is_same_v<decltype(getRefFwdGood), const int&(const int*)>,
+static_assert(is_same_v<decltype(getRefFwdGood), const int&(const int*)>,
               "Returning decltype(auto) perfectly forwards the return type.");
 
 // Alternatively:
 auto getRefFwdGood1(const int* p) -> decltype(getRef(p)) { return getRef(p); }
-static_assert(std::is_same_v<decltype(getRefFwdGood1), const int&(const int*)>,
+static_assert(is_same_v<decltype(getRefFwdGood1), const int&(const int*)>,
               "Returning decltype(return expression) also perfectly forwards the return type.");
 
 int main()
@@ -36,11 +37,9 @@ int main()
     int i = 33;
     decltype(i) j = i * 2;
 
-    std::cout << "i and j are the same type? " << std::boolalpha
-              << std::is_same_v<decltype(i), decltype(j)> << '\n';
+    cout << "i and j are the same type? " << boolalpha << is_same_v<decltype(i), decltype(j)> << '\n';
 
-    std::cout << "i = " << i << ", "
-              << "j = " << j << '\n';
+    cout << "i = " << i << ", j = " << j << '\n';
 
     auto f = [](int a, int b) -> int
     {
@@ -51,6 +50,5 @@ int main()
     i = f(2, 2);
     j = g(3, 3);
 
-    std::cout << "i = " << i << ", "
-              << "j = " << j << '\n';
+    cout << "i = " << i << ", j = " << j << '\n';
 }
