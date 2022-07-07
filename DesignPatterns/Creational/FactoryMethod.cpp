c82d71ea47a330e1c6c0800ecdc7a158d7158e49
mahsanghani <ghaniahsan@outlook.com>
2022-07-07 07:29:33 -0700

    Creator

--- DesignPatterns/Creational/FactoryMethod.cpp
+++ DesignPatterns/Creational/FactoryMethod.cpp
@@ -29,3 +29,16 @@ public:
     }
 };
 
+class Creator {
+public:
+    virtual ~Creator(){};
+    virtual Product* FactoryMethod() const = 0;
+    std::string SomeOperation() const {
+        // Call the factory method to create a Product object.
+        Product* product = this->FactoryMethod();
+        // Now, use the product.
+        std::string result = "Creator: The same creator's code has just worked with " + product->Operation();
+        delete product;
+        return result;
+    }
+};
