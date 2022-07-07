35d251b1c8d3a4e84b7e59d67997218d87e778cf
mahsanghani <ghaniahsan@outlook.com>
2022-07-07 12:27:17 -0700

    main

--- DesignPatterns/Creational/AbstractFactory.cpp
+++ DesignPatterns/Creational/AbstractFactory.cpp
@@ -96,3 +96,17 @@ void ClientCode(const AbstractFactory &factory) {
     delete product_a;
     delete product_b;
 }
+
+int main() {
+    cout << "Client: Testing client code with the first factory type:" << endl;
+    ConcreteFactory1 *f1 = new ConcreteFactory1();
+    ClientCode(*f1);
+    delete f1;
+
+    cout << "Client: Testing client code with the second factory type:" << endl;
+    ConcreteFactory2 *f2 = new ConcreteFactory2();
+    ClientCode(*f2);
+    delete f2;
+
+    return 0;
+}
