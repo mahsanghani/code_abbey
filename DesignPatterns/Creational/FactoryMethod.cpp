7cc0c7c36939e8dd96a4fb56d3e304b1fd102613
mahsanghani <ghaniahsan@outlook.com>
2022-07-07 10:43:49 -0700

    concreteproduct1

--- DesignPatterns/Creational/FactoryMethod.cpp
+++ DesignPatterns/Creational/FactoryMethod.cpp
@@ -16,3 +16,10 @@ public:
     virtual ~Product() {}
     virtual string Operation() const = 0;
 };
+
+class ConcreteProduct1 : public Product {
+public:
+    string Operation() const override {
+        return "{Result of the ConcreteProduct1}";
+    }
+};
