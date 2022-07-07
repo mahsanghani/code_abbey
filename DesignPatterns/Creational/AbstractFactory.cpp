a3e9aec45999c8e4ce2cfbae6c88b22f2231914c
mahsanghani <ghaniahsan@outlook.com>
2022-07-06 22:43:01 -0700

    ConcreteProductA2

--- DesignPatterns/Creational/AbstractFactory.cpp
+++ DesignPatterns/Creational/AbstractFactory.cpp
@@ -17,3 +17,9 @@ public:
     }
 };
 
+class ConcreteProductA2 : public AbstractProductA {
+    std::string UsefulFunctionA() const override {
+        return "The result of the product A2.";
+    }
+};
+
