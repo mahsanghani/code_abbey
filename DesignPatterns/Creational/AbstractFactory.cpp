c52daff95f3d911d03ff3b33a5bfd59b28efa94f
mahsanghani <ghaniahsan@outlook.com>
2022-07-06 22:45:37 -0700

    ConcreteFactory2

--- DesignPatterns/Creational/AbstractFactory.cpp
+++ DesignPatterns/Creational/AbstractFactory.cpp
@@ -67,3 +67,14 @@ public:
         return new ConcreteProductB1();
     }
 };
+
+class ConcreteFactory2 : public AbstractFactory {
+public:
+    AbstractProductA *CreateProductA() const override {
+        return new ConcreteProductA2();
+    }
+    AbstractProductB *CreateProductB() const override {
+        return new ConcreteProductB2();
+    }
+};
+
