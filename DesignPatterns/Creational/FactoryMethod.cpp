fb086a549c253569102d4adc5da4c32c27282fb4
mahsanghani <ghaniahsan@outlook.com>
2022-07-07 07:30:53 -0700

    main

--- DesignPatterns/Creational/FactoryMethod.cpp
+++ DesignPatterns/Creational/FactoryMethod.cpp
@@ -61,3 +61,17 @@ void ClientCode(const Creator& creator) {
     std::cout << "Client: I'm not aware of the creator's class, but it still works.\n"
               << creator.SomeOperation() << std::endl;
 }
+
+int main() {
+    std::cout << "App: Launched with the ConcreteCreator1.\n";
+    Creator* creator = new ConcreteCreator1();
+    ClientCode(*creator);
+    std::cout << std::endl;
+    std::cout << "App: Launched with the ConcreteCreator2.\n";
+    Creator* creator2 = new ConcreteCreator2();
+    ClientCode(*creator2);
+
+    delete creator;
+    delete creator2;
+    return 0;
+}
