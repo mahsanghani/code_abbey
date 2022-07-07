9c592e7ff878b55965732e30d58a0c5f9b3203d0
mahsanghani <ghaniahsan@outlook.com>
2022-07-07 10:42:56 -0700

    product

--- DesignPatterns/Creational/FactoryMethod.cpp
+++ DesignPatterns/Creational/FactoryMethod.cpp
@@ -7,71 +7,12 @@
 // but allows subclasses to alter the type of objects that will be created.
 
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 class Product {
 public:
     virtual ~Product() {}
-    virtual std::string Operation() const = 0;
+    virtual string Operation() const = 0;
 };
-
-class ConcreteProduct1 : public Product {
-public:
-    std::string Operation() const override {
-        return "{Result of the ConcreteProduct1}";
-    }
-};
-
-class ConcreteProduct2 : public Product {
-public:
-    std::string Operation() const override {
-        return "{Result of the ConcreteProduct2}";
-    }
-};
-
-class Creator {
-public:
-    virtual ~Creator(){};
-    virtual Product* FactoryMethod() const = 0;
-    std::string SomeOperation() const {
-        // Call the factory method to create a Product object.
-        Product* product = this->FactoryMethod();
-        // Now, use the product.
-        std::string result = "Creator: The same creator's code has just worked with " + product->Operation();
-        delete product;
-        return result;
-    }
-};
-
-class ConcreteCreator1 : public Creator {
-public:
-    Product* FactoryMethod() const override {
-        return new ConcreteProduct1();
-    }
-};
-
-class ConcreteCreator2 : public Creator {
-public:
-    Product* FactoryMethod() const override {
-        return new ConcreteProduct2();
-    }
-};
-
-void ClientCode(const Creator& creator) {
-    std::cout << "Client: I'm not aware of the creator's class, but it still works.\n"
-              << creator.SomeOperation() << std::endl;
-}
-
-int main() {
-    std::cout << "App: Launched with the ConcreteCreator1.\n";
-    Creator* creator = new ConcreteCreator1();
-    ClientCode(*creator);
-    std::cout << std::endl;
-    std::cout << "App: Launched with the ConcreteCreator2.\n";
-    Creator* creator2 = new ConcreteCreator2();
-    ClientCode(*creator2);
-
-    delete creator;
-    delete creator2;
-    return 0;
-}
