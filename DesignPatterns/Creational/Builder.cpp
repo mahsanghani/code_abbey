def5058523615468e093d557a7242f900f87b632
mahsanghani <ghaniahsan@outlook.com>
2022-07-07 10:06:10 -0700

    concretebuilder1

--- DesignPatterns/Creational/Builder.cpp
+++ DesignPatterns/Creational/Builder.cpp
@@ -32,6 +32,43 @@ class Builder{
 public:
     virtual ~Builder(){}
     virtual void ProducePartA() const = 0;
-    virtual void ProductPartB() const = 0;
-    virtual void ProductPartC() const = 0;
+    virtual void ProducePartB() const = 0;
+    virtual void ProducePartC() const = 0;
 };
+
+class ConcreteBuilder1 : public Builder {
+private:
+    Product1* product;
+
+public:
+    ConcreteBuilder1(){
+        this->Reset();
+    }
+
+    ~ConcreteBuilder1(){
+        delete product;
+    }
+
+    void Reset(){
+        this->product = new Product1();
+    }
+
+    void ProducePartA()const override{
+        this->product->parts_.push_back("PartA1");
+    }
+
+    void ProducePartB()const override{
+        this->product->parts_.push_back("PartB1");
+    }
+
+    void ProducePartC()const override{
+        this->product->parts_.push_back("PartC1");
+    }
+
+    Product1* GetProduct() {
+        Product1* result = this->product;
+        this->Reset();
+        return result;
+    }
+};
+
