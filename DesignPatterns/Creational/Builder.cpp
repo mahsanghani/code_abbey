25fc0881b8db22e53fefa98e4fa3bcce8131f041
mahsanghani <ghaniahsan@outlook.com>
2022-07-07 09:17:47 -0700

    builder

---
+++ DesignPatterns/Creational/Builder.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Ahsan Ghani on 2022-07-07.
+//
+
+// Builder is a creational design pattern that
+// lets you construct complex objects step by step.
+// The pattern allows you to produce different types and
+// representations of an object using the same construction code.
+
