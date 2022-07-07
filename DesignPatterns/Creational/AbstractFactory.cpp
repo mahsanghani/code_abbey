ad33a2eb20866df07602c1376fa908866dab66e2
mahsanghani <ghaniahsan@outlook.com>
2022-07-07 06:48:46 -0700

    ClientCode

--- DesignPatterns/Creational/AbstractFactory.cpp
+++ DesignPatterns/Creational/AbstractFactory.cpp
@@ -78,3 +78,11 @@ public:
     }
 };
 
+void ClientCode(const AbstractFactory &factory) {
+    const AbstractProductA *product_a = factory.CreateProductA();
+    const AbstractProductB *product_b = factory.CreateProductB();
+    std::cout << product_b->UsefulFunctionB() << "\n";
+    std::cout << product_b->AnotherUsefulFunctionB(*product_a) << "\n";
+    delete product_a;
+    delete product_b;
+}
