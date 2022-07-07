02a0d6e0512646e85d54b4c67cfa7f3a2be71694
mahsanghani <ghaniahsan@outlook.com>
2022-07-07 11:12:21 -0700

    concretecreator1

--- DesignPatterns/Creational/FactoryMethod.cpp
+++ DesignPatterns/Creational/FactoryMethod.cpp
@@ -42,3 +42,10 @@ public:
         return result;
     }
 };
+
+class ConcreteCreator1 : public Creator {
+public:
+    Product* FactoryMethod() const override {
+        return new ConcreteProduct1();
+    }
+};
