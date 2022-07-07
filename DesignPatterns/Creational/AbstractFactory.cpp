0e99afc10099952b50c93d2fa6af3337ff8f020c
mahsanghani <ghaniahsan@outlook.com>
2022-07-06 22:44:47 -0700

    ConcreteProductB2

--- DesignPatterns/Creational/AbstractFactory.cpp
+++ DesignPatterns/Creational/AbstractFactory.cpp
@@ -40,3 +40,14 @@ public:
         return "The result of the B1 collaborating with ( " + result + " )";
     }
 };
+
+class ConcreteProductB2 : public AbstractProductB {
+public:
+    std::string UsefulFunctionB() const override {
+        return "The result of the product B2.";
+    }
+    std::string AnotherUsefulFunctionB(const AbstractProductA &collaborator) const override {
+        const std::string result = collaborator.UsefulFunctionA();
+        return "The result of the B2 collaborating with ( " + result + " )";
+    }
+};
