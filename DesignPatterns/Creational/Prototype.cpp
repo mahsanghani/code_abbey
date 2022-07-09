f65d8d22864e65a814ddf0024ddd9f343b2a17e0
mahsanghani <ghaniahsan@outlook.com>
2022-07-08 19:03:22 -0700

    prototype

--- DesignPatterns/Creational/Prototype.cpp
+++ DesignPatterns/Creational/Prototype.cpp
@@ -13,3 +13,21 @@ enum Type
     PROTOTYPE_2;
 };
 
+class Prototype {
+protected:
+    string prototype_name_;
+    float prototype_field_;
+
+public:
+    Prototype() {}
+    Prototype(string prototype_name)
+    : prototype_name_(prototype_name) {}
+    virtual ~Prototype() {}
+    virtual Prototype *Clone() const = 0;
+    virtual void Method(float prototype_field)
+    {
+        this->prototype_field_ = prototype_field;
+        cout << "Call Method from " << prototype_name_
+        << " with field: " << prototype_field << endl;
+    }
+};
