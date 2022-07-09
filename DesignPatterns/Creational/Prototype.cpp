b53c373e08b49a41aaaa02b33c058715f4c7612d
mahsanghani <ghaniahsan@outlook.com>
2022-07-08 18:45:56 -0700

    type

---
+++ DesignPatterns/Creational/Prototype.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Ahsan Ghani on 2022-07-08.
+//
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+enum Type
+{
+    PROTOTYPE_1 = 0;
+    PROTOTYPE_2;
+};
+
