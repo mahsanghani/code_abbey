f27214eb3d0622c613fe89a1dea045ea2564bea4
mahsanghani <ghaniahsan@outlook.com>
2022-07-10 15:14:31 -0700

    Composite

---
+++ DesignPatterns/Structural/Composite.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Ahsan Ghani on 2022-07-10.
+//
+
+// Composite is a structural design pattern that
+// allows composing objects into a tree-like structure
+// and work with it as if it was a singular object.
+
+#include <algorithm>
+#include <iostream>
+#include <list>
+#include <string>
+using namespace std;
+
