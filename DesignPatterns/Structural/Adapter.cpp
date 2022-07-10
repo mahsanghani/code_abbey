ed6abd3c819fa12eff93b4fb6ac48ff36efb2e0b
mahsanghani <ghaniahsan@outlook.com>
2022-07-10 14:23:22 -0700

    Adapter

--- DesignPatterns/Structural/Adapter.cpp
+++ DesignPatterns/Structural/Adapter.cpp
@@ -27,3 +27,19 @@ public:
         return ".eetpadA eht fo roivaheb laicepS";
     }
 };
+
+class Adapter: public Target
+{
+private:
+    Adaptee *adaptee_;
+
+public:
+    Adapter(Adaptee *adaptee) : adaptee_(adaptee) {}
+    string Request() const override
+    {
+        string to_reverse = this->adaptee_->SpecificRequest();
+        reverse(to_reverse.begin(), to_reverse.end());
+        return "Adapter: (TRANSLATED)" + to_reverse;
+    }
+};
+
