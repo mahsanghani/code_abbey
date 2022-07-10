cc724225aeb491f358fd83565c75ba250d7b7075
mahsanghani <ghaniahsan@outlook.com>
2022-07-10 14:28:08 -0700

    Bridge

---
+++ DesignPatterns/Structural/Bridge.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Ahsan Ghani on 2022-07-10.
+//
+
+
+// Bridge is a structural design pattern that
+// divides business logic or huge class into separate
+// class hierarchies that can be developed independently.
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
