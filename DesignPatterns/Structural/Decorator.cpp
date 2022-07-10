48201bc5247ec33469d0fc1d478883e642d70625
mahsanghani <ghaniahsan@outlook.com>
2022-07-10 16:45:27 -0700

    Decorator

---
+++ DesignPatterns/Structural/Decorator.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Ahsan Ghani on 2022-07-10.
+//
+
+// Decorator is a structural pattern that
+// allows adding new behaviors to objects
+// dynamically by placing them inside
+// special wrapper objects, called decorators.
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+
