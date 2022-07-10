f635692ee381d8b579efeee82ce4a5cc86e4e19b
mahsanghani <ghaniahsan@outlook.com>
2022-07-10 16:43:34 -0700

    main

--- DesignPatterns/Structural/Composite.cpp
+++ DesignPatterns/Structural/Composite.cpp
@@ -98,3 +98,41 @@ void ClientCode2(Component *component1, Component *component2)
     cout << "RESULT: " << component1->Operation() << endl;
 }
 
+int main()
+{
+    Component *simple = new Leaf;
+    cout << "Client: I've got a simple component:\n" << endl;
+    ClientCode(simple);
+    cout << "\n\n" << endl;
+
+    Component *tree = new Composite;
+    Component *branch1 = new Composite;
+    Component *branch2= new Composite;
+
+    Component *leaf_1 = new Leaf;
+    Component *leaf_2 = new Leaf;
+    Component *leaf_3 = new Leaf;
+
+    branch1->Add(leaf_1);
+    branch1->Add(leaf_2);
+    branch2->Add(leaf_3);
+
+    tree->Add(branch1);
+    tree->Add(branch2);
+
+    cout << "Client: Now I've got a composite tree:\n";
+    ClientCode(tree);
+    cout << "\n\n" << endl;
+    cout << "Client: I don't need ot check the components classes even when managing the tree:\n";
+    ClientCode2(tree,simple);
+    cout << "\n\n" << endl;
+
+    delete simple;
+    delete tree;
+    delete branch1;
+    delete branch2;
+    delete leaf_1;
+    delete leaf_2;
+    delete leaf_3;
+    return 0;
+}
