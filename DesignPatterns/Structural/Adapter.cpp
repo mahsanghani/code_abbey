a5367cfefe5ecec9b07e0474f004d36d9f3df4db
mahsanghani <ghaniahsan@outlook.com>
2022-07-10 14:25:05 -0700

    main

--- DesignPatterns/Structural/Adapter.cpp
+++ DesignPatterns/Structural/Adapter.cpp
@@ -48,3 +48,24 @@ void ClientCode(const Target *target)
     cout << target->Request() << endl;
 }
 
+int main()
+{
+    std::cout << "Client: I can work just fine with the Target objects:\n";
+    Target *target = new Target;
+    ClientCode(target);
+    std::cout << "\n\n";
+    Adaptee *adaptee = new Adaptee;
+    std::cout << "Client: The Adaptee class has a weird interface. See, I don't understand it:\n";
+    std::cout << "Adaptee: " << adaptee->SpecificRequest();
+    std::cout << "\n\n";
+    std::cout << "Client: But I can work with it via the Adapter:\n";
+    Adapter *adapter = new Adapter(adaptee);
+    ClientCode(adapter);
+    std::cout << "\n";
+
+    delete target;
+    delete adaptee;
+    delete adapter;
+
+    return 0;
+}
