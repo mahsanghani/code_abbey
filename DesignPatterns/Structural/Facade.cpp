0af5d6777b866383c7082977be79a86f579704c3
mahsanghani <ghaniahsan@outlook.com>
2022-07-11 18:45:56 -0700

    Facade

---
+++ DesignPatterns/Structural/Facade.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Ahsan Ghani on 2022-07-11.
+//
+
+// Facade is a structural design pattern that
+// provides a simplified (but limited) interface
+// to a complex system of classes, library or framework.
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+
