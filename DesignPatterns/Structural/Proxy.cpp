63d3dc3e032bfc96e10ce9d9aa66eba04586e35e
mahsanghani <ghaniahsan@outlook.com>
2022-07-13 14:57:44 -0700

    main

--- DesignPatterns/Structural/Proxy.cpp
+++ DesignPatterns/Structural/Proxy.cpp
@@ -66,3 +66,16 @@ void ClientCode(const Subject &subject)
     subject.Request();
 }
 
+int main()
+{
+    cout << "Client: Executing the client code with a real subject: \n";
+    RealSubject *real_subject = new RealSubject;
+    ClientCode(*real_subject);
+    cout << "\n Client: Executing the same client code with a proxy: \n";
+    Proxy *proxy = new Proxy(real_subject);
+    ClientCode(*proxy);
+
+    delete real_subject;
+    delete proxy;
+    return 0;
+}
