7db1b2e2d9ccbda43a85b4611915faad1316cf7f
mahsanghani <ghaniahsan@outlook.com>
2022-07-13 11:09:41 -0700

    Facade

--- DesignPatterns/Structural/Facade.cpp
+++ DesignPatterns/Structural/Facade.cpp
@@ -18,7 +18,7 @@ public:
     {
         return "Subsystem1: Ready!\n";
     }
-    string Operation2() const
+    string OperationN() const
     {
         return "Subsystem1: Go!\n"
     }
@@ -31,9 +31,37 @@ public:
     {
         return "Subsystem2: Get Ready!\n";
     }
-    string Operation2() const
+    string OperationZ() const
     {
         return "Subsystem2: Fire!\n";
     }
 };
 
+class Facade
+{
+protected:
+    Subsystem1 *subsystem1_;
+    Subsystem2 *subsystem2_;
+public:
+    Facade(Subsystem1 *subsystem1 = nullptr,
+            Subsystem2 *subsystem2 = nullptr)
+    {
+        this->subsystem1_ = subsystem1 ?: new Subsystem1;
+        this->subsystem2_ = subsystem2 ?: new Subsystem2;
+    }
+    ~Facade()
+    {
+        delete subsystem1_;
+        delete subsystem2_;
+    }
+    string Operation()
+    {
+        string result = "Facade initializes subsystem:\n";
+        result += this->subsystem1_->Operation1();
+        result += this->subsystem2_->Operation1();
+        result += "Facade orders subsystems to perform the action:\n";
+        result += this->subsystem1_->OperationN();
+        result += this->subsystem2_->OperationZ();
+        return result;
+    }
+};
