819b6f2d212cbc75c22ee69c69133bc41079048c
mahsanghani <ghaniahsan@outlook.com>
2022-07-13 11:17:03 -0700

    Flyweight

---
+++ DesignPatterns/Structural/Flyweight.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Ahsan Ghani on 2022-07-13.
+//
+
+// Flyweight is a structural design pattern that
+// lets you fit more objects into the available
+// amount of RAM by sharing common parts of state
+// between multiple objects instead of keeping all data.
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+
