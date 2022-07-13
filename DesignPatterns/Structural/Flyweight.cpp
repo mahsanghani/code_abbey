666bcb44d0302bb07fb3a227b509ad40a53daa86
mahsanghani <ghaniahsan@outlook.com>
2022-07-13 14:25:49 -0700

    main

--- DesignPatterns/Structural/Flyweight.cpp
+++ DesignPatterns/Structural/Flyweight.cpp
@@ -128,3 +128,27 @@ void AddCarToPoliceDatabase(
     flyweight.Operation({owner, plates});
 }
 
+int main()
+{
+    FlyweightFactory *factory = new FlyweightFactory({{"Chevrolet", "Camaro2018", "pink"}, {"Mercedes Benz", "C300", "black"}, {"Mercedes Benz", "C500", "red"}, {"BMW", "M5", "red"}, {"BMW", "X6", "white"}});
+    factory->ListFlyweights();
+
+    AddCarToPoliceDatabase(*factory,
+                           "CL234IR",
+                           "James Doe",
+                           "BMW",
+                           "M5",
+                           "red");
+
+    AddCarToPoliceDatabase(*factory,
+                           "CL234IR",
+                           "James Doe",
+                           "BMW",
+                           "X1",
+                           "red");
+
+    factory->ListFlyweights();
+    delete factory;
+
+    return 0;
+}
