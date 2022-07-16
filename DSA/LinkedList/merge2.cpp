e690a2dd0bd76124870448224528a4ba033c2388
mahsanghani <ghaniahsan@outlook.com>
2022-07-15 22:56:48 -0700

    mergeUtil

--- DSA/LinkedList/merge2.cpp
+++ DSA/LinkedList/merge2.cpp
@@ -27,3 +27,43 @@ void printList(struct Node* node)
     }
 }
 
+struct Node* mergeUtil(struct Node* h1, struct Node* h2)
+{
+    if(!h1->next)
+    {
+        h1->next = h2;
+        return h1;
+    }
+
+    struct Node *current1 = h1, *next1 = h1->next;
+    struct Node *current2 = h2, *next2 = h2->next;
+
+    while(next1 && current2)
+    {
+        if((current2->data) >= (current1->data) &&
+        (current2->data) <= (next1->data))
+        {
+            next2 = current2->next;
+            current1->next = current2;
+            current2->next = next1;
+
+            current1 = current2;
+            current2 = next2;
+        }
+        else
+        {
+            if(next1->next)
+            {
+                next1 = next1->next;
+                current1 = current1->next;
+            }
+            else
+            {
+                next1->next = current2;
+                return h1;
+            }
+        }
+    }
+    return h1;
+}
+
