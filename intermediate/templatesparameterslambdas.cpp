2baad85cf8268b35ab33303622a523c1c8a4b636
mahsanghani <ghaniahsan@outlook.com>
2022-07-15 23:30:51 -0700

    templates named parameters for lambdas

---
+++ intermediate/templatesparameterslambdas.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Ahsan Ghani on 2022-07-15.
+//
+#include <iostream>
+
+int main(){
+
+    auto func =  [] <typename T, typename P>(T a, P b){
+        return a + b;
+    };
+
+    char a {'c'};
+    int b{63};
+
+    auto result = func(a,b);
+    std::cout << "result : " << result << std::endl;
+    std::cout << "sizeof(result) : " << sizeof(result) << std::endl;
+
+    return 0;
+}
