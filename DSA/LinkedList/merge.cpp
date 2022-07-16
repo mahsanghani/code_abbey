308ef6eb4bbc05a657eaf56f30773e1823bfb60f
mahsanghani <ghaniahsan@outlook.com>
2022-07-15 22:31:37 -0700

    merge

--- DSA/LinkedList/merge.cpp
+++ DSA/LinkedList/merge.cpp
@@ -27,3 +27,26 @@ void printList(Node* node)
     }
 }
 
+Node* merge(Node* h1, Node* h2)
+{
+    if(!h1)
+    {
+        return h2;
+    }
+    if(!h2)
+    {
+        return h1;
+    }
+
+    if(h1->data < h2->data)
+    {
+        h1->next = merge(h1->next, h2);
+        return h1;
+    }
+    else
+    {
+        h2->next = merge(h1, h2->next);
+        return h2;
+    }
+}
+
