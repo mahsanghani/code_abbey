a3bb8b965359a9f6e119abe72f6527b15d574e3b
mahsanghani <ghaniahsan@outlook.com>
2022-07-16 21:22:54 -0700

    Node

--- DSA/LinkedList/reverse4.cpp
+++ DSA/LinkedList/reverse4.cpp
@@ -1,3 +1,20 @@
 //
 // Created by Ahsan Ghani on 2022-07-15.
 //
+
+// Head recursive C++ program
+// to reverse a linked list
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node* next;
+
+    Node(){};
+    Node(int val) : data(val), next(NULL) {};
+};
+
