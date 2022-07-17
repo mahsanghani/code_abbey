60647a5cf7315313ead78de1c43bdfe7f50e30ce
mahsanghani <ghaniahsan@outlook.com>
2022-07-16 20:28:30 -0700

    Node

--- DSA/LinkedList/reverse2.cpp
+++ DSA/LinkedList/reverse2.cpp
@@ -1,3 +1,24 @@
 //
 // Created by Ahsan Ghani on 2022-07-15.
 //
+
+// 1) Divide the list in two parts - first node and
+// rest of the linked list.
+// 2) Call reverse for the rest of the linked list.
+// 3) Link rest to first.
+// 4) Fix head pointer
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
