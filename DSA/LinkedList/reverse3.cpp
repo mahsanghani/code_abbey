1d6f7014e097881846e01c0d377967e32101fcf1
mahsanghani <ghaniahsan@outlook.com>
2022-07-16 21:19:30 -0700

    main

--- DSA/LinkedList/reverse3.cpp
+++ DSA/LinkedList/reverse3.cpp
@@ -56,3 +56,21 @@ void printlist(Node* head)
     }
     cout << endl;
 }
+
+int main()
+{
+    Node* head1 = newNode(1);
+    head1->next = newNode(2);
+    head1->next->next = newNode(3);
+    head1->next->next->next = newNode(4);
+    head1->next->next->next->next = newNode(5);
+    head1->next->next->next->next->next = newNode(6);
+    head1->next->next->next->next->next->next = newNode(7);
+    head1->next->next->next->next->next->next->next = newNode(8);
+    cout << "Given linked list\n";
+    printlist(head1);
+    reverse(&head1);
+    cout << "\nReversed linked list\n";
+    printlist(head1);
+    return 0;
+}
