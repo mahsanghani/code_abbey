32836abf388115eb39769b7e0113897487d7092c
mahsanghani <ghaniahsan@outlook.com>
2022-07-16 21:07:26 -0700

    Node

--- DSA/LinkedList/reverse3.cpp
+++ DSA/LinkedList/reverse3.cpp
@@ -1,3 +1,17 @@
 //
 // Created by Ahsan Ghani on 2022-07-15.
 //
+
+// A simple and tail recursive
+// C++ program to reverse
+// a linked list
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
