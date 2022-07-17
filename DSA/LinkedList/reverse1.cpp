6ec79fae3889ec4c5849cb554b6df9a393db7096
mahsanghani <ghaniahsan@outlook.com>
2022-07-16 20:15:32 -0700

    Node

--- DSA/LinkedList/reverse1.cpp
+++ DSA/LinkedList/reverse1.cpp
@@ -1,3 +1,16 @@
 //
 // Created by Ahsan Ghani on 2022-07-15.
 //
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
