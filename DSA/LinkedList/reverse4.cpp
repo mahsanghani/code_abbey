5430691eaee48d4074fde1458b59ed62a4b0c9ad
mahsanghani <ghaniahsan@outlook.com>
2022-07-16 21:39:03 -0700

    main

--- DSA/LinkedList/reverse4.cpp
+++ DSA/LinkedList/reverse4.cpp
@@ -67,3 +67,19 @@ void print(Node* headref)
     cout << endl;
 }
 
+int main()
+{
+    Node* head = NULL;
+    push(&head, 1);
+    push(&head, 2);
+    push(&head, 3);
+    push(&head, 4);
+    push(&head, 5);
+    push(&head, 6);
+    cout << "Given Linked List." << endl;
+    print(head);
+    reverse(&head);
+    cout << "Reversed Linked List." << endl;
+    print(head);
+    return 0;
+}
