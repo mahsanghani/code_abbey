d3368fbe0f0dde9e162216f8da00e40871d9e79b
mahsanghani <ghaniahsan@outlook.com>
2022-07-18 15:16:28 -0700

    reverse

--- DSA/LinkedList/reverse5.cpp
+++ DSA/LinkedList/reverse5.cpp
@@ -18,3 +18,23 @@ public:
     Node* next;
 };
 
+void reverseLL(Node** head)
+{
+    stack<Node*> s;
+    Node* temp = *head;
+    while(temp->next != NULL)
+    {
+        s.push(temp);
+        temp = temp->next;
+    }
+
+    *head = temp;
+    while(!s.empty())
+    {
+        temp->next = s.top();
+        s.pop();
+        temp = temp->next;
+    }
+    temp->next = NULL;
+}
+
