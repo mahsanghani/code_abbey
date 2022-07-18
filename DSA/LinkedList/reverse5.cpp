1e8fdca8fdcd56cbbd2d1de0b6c31c3ae08857dc
mahsanghani <ghaniahsan@outlook.com>
2022-07-18 15:14:41 -0700

    Node

--- DSA/LinkedList/reverse5.cpp
+++ DSA/LinkedList/reverse5.cpp
@@ -1,3 +1,20 @@
 //
 // Created by Ahsan Ghani on 2022-07-15.
 //
+
+// Store the nodes(values and address) in the stack until all the values are entered.
+// Once all entries are done, Update the Head pointer to the last location(i.e the last value).
+// Start popping the nodes(value and address) and store them in the same order until the stack is empty.
+// Update the next pointer of last Node in the stack by NULL.
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node* next;
+};
+
