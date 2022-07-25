78381807ad9a50ec157c85aab689d0517a360b7a
mahsanghani <ghaniahsan@outlook.com>
2022-07-25 11:24:31 -0700

    minDays

---
+++ DSA/search/minDays.cpp
@@ -0,0 +1,57 @@
+//
+// Created by Ahsan Ghani on 2022-07-25.
+//
+
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <valarray>
+#include <numeric>
+using namespace std;
+
+bool feasible(vector<int> &nums, int &m, int &k, int days)
+{
+    int bouquets = 0;
+    int flowers = 0;
+    for (auto num : nums)
+    {
+        if (num > days)
+        {
+            flowers = 0;
+        }
+        else
+        {
+            bouquets += (flowers + 1) / k;
+            flowers = (flowers + 1) % k;
+        }
+    }
+    return bouquets >= m;
+}
+
+
+int minDays(vector<int> nums, int m, int k)
+{
+    if(nums.size() < m * k)
+    {
+        return -1;
+    }
+
+    int left = *max_element(nums.begin(), nums.end());
+    int right = accumulate(nums.begin(), nums.end(), 0);
+
+    while (left < right)
+    {
+        int middle = left + (right - left) / 2;
+        if (feasible(nums, m, k,middle))
+        {
+            right = middle;
+        }
+        else
+        {
+            left = middle + 1;
+        }
+    }
+    return left;
+}
