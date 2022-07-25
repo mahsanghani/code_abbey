4a042ca51895c6fc923eca7fa4ba4f44660a3662
mahsanghani <ghaniahsan@outlook.com>
2022-07-24 21:15:07 -0700

    binary recursive

--- DSA/search/binary-recursive.cpp
+++ DSA/search/binary-recursive.cpp
@@ -27,3 +27,14 @@ int binarySearch(int arr[], int left, int right, int target)
     return -1;
 }
 
+int main()
+{
+    int arr[] = {1,2,3,4,10,40};
+    int target = 10;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = binarySearch(arr, 0, n-1, target);
+    (result == -1)
+    ? cout << "Element is not present in array"
+    : cout << "Element is present at index " << result << endl;
+    return 0;
+}
