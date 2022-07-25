a2ca946f9c437290f28f7b737714c79b063900a6
mahsanghani <ghaniahsan@outlook.com>
2022-07-24 21:10:10 -0700

    binary recursive

--- DSA/search/binary-recursive.cpp
+++ DSA/search/binary-recursive.cpp
@@ -1,3 +1,29 @@
 //
 // Created by Ahsan Ghani on 2022-07-24.
 //
+
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+using namespace std;
+
+int binarySearch(int arr[], int left, int right, int target)
+{
+    if(right >= left)
+    {
+        int middle = left + (right - left) / 2;
+        if(arr[middle] == target)
+        {
+            return middle;
+        }
+        if(arr[middle] > target)
+        {
+            return binarySearch(arr, left, middle-left, target);
+        }
+        return binarySearch(arr, middle+left, right, target);
+    }
+    return -1;
+}
+
