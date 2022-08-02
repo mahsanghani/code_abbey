7bafc8486da7dc09eeb0ce5f822da4655ce5cbca
mahsanghani <ghaniahsan@outlook.com>
2022-08-01 19:15:42 -0700

    recursive lowest common ancestor

---
+++ DSA/Trees/recLCA.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Ahsan Ghani on 2022-08-01.
+//
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (p->val < root->val && q->val < root->val)
+        {
+            return lowestCommonAncestor(root->left, p, q);
+        }
+        else if (p->val > root->val && q->val > root->val)
+        {
+            return lowestCommonAncestor(root->right, p, q);
+        }
+        else
+        {
+            return root;
+        }
+    }
+};
