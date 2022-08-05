47d548082ac4da1400f13f2290fec0de617150b4
mahsanghani <ghaniahsan@outlook.com>
2022-08-05 11:23:33 -0700

    minimum cost of climbing stairs 4

---
+++ leetcode/minCostClimbingStairs4.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Ahsan Ghani on 2022-08-05.
+//
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n + 1);
+        for (int i = 2; i <= n; i++) {
+            int jumpOneStep = dp[i - 1] + cost[i - 1];
+            int jumpTwoStep = dp[i - 2] + cost[i - 2];
+            dp[i] = min(jumpOneStep, jumpTwoStep);
+        }
+        return dp[n];
+    }
+};
