242434d4133f845a579997ca47cb7285cc251c84
mahsanghani <ghaniahsan@outlook.com>
2022-08-08 09:39:02 -0700

    main

---
+++ advanced/moveconstructors/main14.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Ahsan Ghani on 2022-08-08.
+//
+#include <iostream>
+#include "point.h"
+
+int main(){
+    //Point p1(10.4,15.6);
+    //p1.print_info();
+
+    //Create a copy from a temporary
+    Point p3(std::move(Point(20.5,5.8)));
+    p3.print_info();
+
+    return 0;
+}
