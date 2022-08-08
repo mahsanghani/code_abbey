705d82111e14298e73fa3f1199fdddb4499c5196
mahsanghani <ghaniahsan@outlook.com>
2022-08-07 22:59:27 -0700

    square

---
+++ advanced/explicitconstructors/square.h
@@ -0,0 +1,21 @@
+//
+// Created by Ahsan Ghani on 2022-08-07.
+//
+#ifndef SQUARE_H
+#define SQUARE_H
+
+#include <string>
+
+class Square
+{
+public:
+    explicit Square(double side_param , const std::string& color_param = "black");
+    ~Square();
+    double surface() const;
+
+private :
+    double m_side;
+    std::string m_color;
+};
+
+#endif // SQUARE_H
