37278be529959d4ab8aab4d4c0adbc17c067b265
mahsanghani <ghaniahsan@outlook.com>
2022-08-07 23:01:58 -0700

    square

---
+++ advanced/constructordelegation/square.h
@@ -0,0 +1,24 @@
+//
+// Created by Ahsan Ghani on 2022-08-07.
+//
+#ifndef SQUARE_H
+#define SQUARE_H
+
+#include <string>
+
+class Square
+{
+public:
+    explicit Square(double side_param);
+    Square( double side_param, const std::string& color_param, int shading_param);
+    ~Square();
+    double surface() const;
+
+private :
+    double m_side;
+    std::string m_color;
+    int m_shading;
+    double m_position;
+};
+
+#endif // SQUARE_H
