248dbd0a0c7bb79e490dda5e072101338eb38caa
mahsanghani <ghaniahsan@outlook.com>
2022-08-08 10:30:12 -0700

    integer

---
+++ advanced/membervariablesofothertypes/integer.h
@@ -0,0 +1,26 @@
+//
+// Created by Ahsan Ghani on 2022-08-08.
+//
+#ifndef INTEGER_H
+#define INTEGER_H
+
+class Integer
+{
+public:
+    explicit Integer(int value);
+    Integer()  = default;
+    ~Integer();
+
+    int get_value () const{
+        return inner_int;
+    }
+
+    void set_value(int new_val){
+        inner_int = new_val;
+    }
+
+private :
+    int inner_int{0};
+};
+
+#endif // INTEGER_H
