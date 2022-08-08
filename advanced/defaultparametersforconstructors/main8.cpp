813cac399e5bfe37652b48d681e06b6df6534922
mahsanghani <ghaniahsan@outlook.com>
2022-08-07 22:16:58 -0700

    main

---
+++ advanced/defaultparametersforconstructors/main8.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Ahsan Ghani on 2022-08-07.
+//
+#include <iostream>
+#include "cylinder.h"
+
+int main(){
+
+    Cylinder c1(4);
+    std::cout << "volume : " << c1.volume() << std::endl;
+
+    return 0;
+}
