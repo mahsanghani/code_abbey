386aade42e20a4aecafe1041cfa79ea775a35619
mahsanghani <ghaniahsan@outlook.com>
2022-08-08 10:14:05 -0700

    main

--- advanced/staticconstants/main22.cpp
+++ advanced/staticconstants/main22.cpp
@@ -1,3 +1,16 @@
 //
 // Created by Ahsan Ghani on 2022-08-08.
 //
+#include <iostream>
+#include "cylinder.h"
+
+
+
+int main(){
+    Cylinder cylinder1(3.3,10.0);
+    std::cout << "Volume of cylinder : " << cylinder1.volume() << std::endl;
+    std::cout << "Cylinder default color : " << Cylinder::default_color << std::endl;
+    std::cout << "Cylinder default color : " << cylinder1.default_color << std::endl;
+
+    return 0;
+}
