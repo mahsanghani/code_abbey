c81e61093e30fac3f27f7704052a75b640343308
mahsanghani <ghaniahsan@outlook.com>
2022-08-08 10:37:42 -0700

    integer

---
+++ advanced/inclassmembervariableinitialization/integer.h
@@ -0,0 +1,29 @@
+//
+// Created by Ahsan Ghani on 2022-08-08.
+//
+#ifndef INTEGER_H
+#define INTEGER_H
+
+class Integer
+{
+public:
+    explicit Integer(int value);
+    Integer(){
+        std::cout << "Default constructor for Integer called" << std::endl;
+    }
+    ~Integer();
+
+    int get_value () const{
+        return inner_int;
+    }
+
+    void set_value(int new_val){
+        inner_int = new_val;
+    }
+
+private :
+    int inner_int{};
+
+};
+
+#endif // INTEGER_H
