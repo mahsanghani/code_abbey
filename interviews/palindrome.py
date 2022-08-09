9f34f23abc7c6b466d2c94547d5fd1502b58ac9b
mahsanghani <ghaniahsan@outlook.com>
2022-08-09 09:05:06 -0700

    palidrome

---
+++ interviews/palindrome.py
@@ -0,0 +1,11 @@
+s = 'radkar'
+
+def solution(s):
+    for i in range(len(s)):
+        t = s[:i] + s[i+1:]
+        if t == t[::-1]:
+            return True
+
+    return s == s[::-1]
+
+solution(s)
