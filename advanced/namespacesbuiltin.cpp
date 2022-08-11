294018fcdb3bf16092df11444be6caa0f1a73a58
mahsanghani <ghaniahsan@outlook.com>
2022-08-11 12:34:55 -0700

    builtin namespaces

---
+++ advanced/namespacesbuiltin.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Ahsan Ghani on 2022-08-11.
+//
+#include <iostream>
+#include <string>
+
+int main(){
+
+    std::cout << "Hello World" << std::endl;
+    std::string message{"Hello World"};
+    std::cout << message << std::endl;
+    return 0;
+}
