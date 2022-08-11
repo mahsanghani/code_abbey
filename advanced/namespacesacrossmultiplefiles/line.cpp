7aa811a886c6483331deb8ad3f4588a69ccbf4b7
mahsanghani <ghaniahsan@outlook.com>
2022-08-11 12:30:53 -0700

    line

---
+++ advanced/namespacesacrossmultiplefiles/line.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Ahsan Ghani on 2022-08-11.
+//
+#include "line.h"
+
+namespace Geom{
+
+    Line::Line(const Point& start, const Point& end)
+            : m_start{start}, m_end{end}
+    {
+    }
+}
