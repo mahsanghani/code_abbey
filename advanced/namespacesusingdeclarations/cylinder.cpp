6b5374e1f62776e88f6061171c449aa617028444
mahsanghani <ghaniahsan@outlook.com>
2022-08-11 12:36:07 -0700

    cylinder

---
+++ advanced/namespacesusingdeclarations/cylinder.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Ahsan Ghani on 2022-08-11.
+//
+#include "cylinder.h"
+
+namespace Geom{
+
+    Cylinder::Cylinder(double base_rad, double height)
+            : m_base_rad{base_rad} , m_height{height}
+    {
+    }
+
+}
