566d0e68f3b6f7cf64536ed54d8bc8738c398376
mahsanghani <ghaniahsan@outlook.com>
2022-08-13 23:31:32 -0700

    utilities

---
+++ advanced/compilingandlinking/utilities.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Ahsan Ghani on 2022-08-13.
+//
+//Implementations
+/*
+double add( double a, double b){
+	return a + b;
+}
+double multiply(double a, double b){
+	return a * b;
+}
+*/
