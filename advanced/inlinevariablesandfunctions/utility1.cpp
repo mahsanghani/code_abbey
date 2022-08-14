5bf2dc876cfecde5b931bf80605e91079141aa99
mahsanghani <ghaniahsan@outlook.com>
2022-08-13 23:40:23 -0700

    utility

---
+++ advanced/inlinevariablesandfunctions/utility1.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Ahsan Ghani on 2022-08-13.
+//
+#include <iostream>
+#include "utility.h"
+
+inline int age{12};
+
+inline void some_function(){
+    std::cout << "age : " << age << std::endl;
+    std::cout << "&age : " << &age << std::endl;
+}
+
+void print_age_utility1(){
+    std::cout << "Printing from utility1 : " << std::endl;
+    some_function();
+    std::cout << std::endl;
+}
