410e14c3d87b0c9831a31348830b637792517f64
mahsanghani <ghaniahsan@outlook.com>
2022-08-14 13:12:16 -0700

    dog

---
+++ advanced/sharedpointersasfunctionparametersandreturntypes/dog.h
@@ -0,0 +1,33 @@
+//
+// Created by Ahsan Ghani on 2022-08-14.
+//
+#ifndef DOG_H
+#define DOG_H
+
+#include <string>
+#include <iostream>
+class Dog
+{
+public:
+    explicit Dog(std::string name_param);
+    Dog() = default;
+    ~Dog();
+
+    std::string get_name() const{
+        return dog_name;
+    }
+
+    void set_dog_name(const std::string & name){
+        dog_name = name;
+    }
+
+    void print_info() const{
+        std::cout << "Dog [ name : " << dog_name << " ]" <<  std::endl;
+    }
+
+private:
+    std::string dog_name {"Puffy"};
+};
+
+
+#endif // DOG_H
