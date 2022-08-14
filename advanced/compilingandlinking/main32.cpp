962ef3d4845ebba94ae539bc8b41df1074384e9d
mahsanghani <ghaniahsan@outlook.com>
2022-08-13 23:30:50 -0700

    main

---
+++ advanced/compilingandlinking/main32.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Ahsan Ghani on 2022-08-13.
+//
+#include <iostream>
+#include "utilities.h"
+
+int main(){
+
+    double result = add(10.5,20.8);
+    std::cout << "result : " << result << std::endl;
+
+    return 0;
+}
