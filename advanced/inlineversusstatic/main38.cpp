ec2a440ce745e91d545bb4c2c95c84dcf78080a5
mahsanghani <ghaniahsan@outlook.com>
2022-08-13 23:44:52 -0700

    main

---
+++ advanced/inlineversusstatic/main38.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Ahsan Ghani on 2022-08-13.
+//
+#include <iostream>
+
+void print_age_1();
+void print_age_2();
+
+void print_distance_1();
+void print_distance_2();
+
+int main(){
+
+    print_age_1();
+    print_age_2();
+
+    std::cout << "----" << std::endl;
+
+    print_distance_1();
+    print_distance_2();
+
+    return 0;
+}
