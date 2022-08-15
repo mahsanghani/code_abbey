dca909ed86da2823848520993508202e3e645341
mahsanghani <ghaniahsan@outlook.com>
2022-08-15 15:02:08 -0700

    car

---
+++ advanced/copyassignmentoperatorforothertypes/car.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Ahsan Ghani on 2022-08-15.
+//
+#include "car.h"
+
+Car::Car(const std::string& color, int speed) :
+        m_color(color),m_speed(speed)
+{
+}
+
+Car::~Car()
+{
+}
