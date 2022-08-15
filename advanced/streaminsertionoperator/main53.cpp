36cad3652d59f2e5940fb13b469471eebcd507d7
mahsanghani <ghaniahsan@outlook.com>
2022-08-15 14:23:56 -0700

    main

---
+++ advanced/streaminsertionoperator/main53.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Ahsan Ghani on 2022-08-15.
+//
+#include <iostream>
+#include "point.h"
+
+int main(){
+
+    Point p1(10,20);
+    Point p2(3,4);
+    //p1.print_info();
+
+    std::cout << p1 << p2 << std::endl;
+
+    return 0;
+}
