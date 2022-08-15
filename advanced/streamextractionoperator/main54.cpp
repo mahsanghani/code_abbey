2957b185fa1bfc82f846cfef88a5d2e42da79cb3
mahsanghani <ghaniahsan@outlook.com>
2022-08-15 14:26:31 -0700

    main

---
+++ advanced/streamextractionoperator/main54.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Ahsan Ghani on 2022-08-15.
+//
+#include <iostream>
+#include "point.h"
+
+int main(){
+    Point p2;
+    std::cin >> p2;//
+    std::cout << "p2 : " << p2 << std::endl;
+    return 0;
+}
