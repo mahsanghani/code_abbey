d462495ce42c5cee55067f2858bbc702eacae9f7
mahsanghani <ghaniahsan@outlook.com>
2022-08-23 13:27:37 -0400

    number

---
+++ advanced/logicaloperatorswithimplicitconversions/number.h
@@ -0,0 +1,51 @@
+//
+// Created by Ahsan Ghani on 2022-08-23.
+//
+#ifndef NUMBER_H
+#define NUMBER_H
+
+#include <iostream>
+class Number
+{
+    friend std::ostream& operator<<(std::ostream& out , const Number& number);
+
+    //Comparison operators
+    friend bool operator<(const Number& left_operand, const Number& right_operand);
+    friend bool operator<(int left_operand, const Number& right_operand);
+    friend bool operator<(const Number& left_operand, int right_operand);
+
+    friend bool operator==(const Number& left_operand, const Number& right_operand);
+    friend bool operator==(int left_operand, const Number& right_operand);
+    friend bool operator==(const Number& left_operand, int right_operand);
+
+    friend bool operator>(const Number& left_operand, const Number& right_operand);
+    friend bool operator>(int left_operand, const Number& right_operand);
+    friend bool operator>(const Number& left_operand, int right_operand);
+
+    friend bool operator>=(const Number& left_operand, const Number& right_operand);
+    friend bool operator>=(int left_operand, const Number& right_operand);
+    friend bool operator>=(const Number& left_operand, int right_operand);
+
+    friend bool operator<=(const Number& left_operand, const Number& right_operand);
+    friend bool operator<=(int left_operand, const Number& right_operand);
+    friend bool operator<=(const Number& left_operand, int right_operand);
+
+    friend bool operator!=(const Number& left_operand, const Number& right_operand);
+    friend bool operator!=(int left_operand, const Number& right_operand);
+    friend bool operator!=(const Number& left_operand, int right_operand);
+public:
+    Number() = default;
+    explicit Number(int value );
+
+    //getter
+    int get_wrapped_int() const{
+        return m_wrapped_int;
+    }
+
+    ~Number();
+private :
+    int m_wrapped_int{0};
+};
+
+
+#endif // NUMBER_H
