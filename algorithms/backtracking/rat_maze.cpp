3d52985bfe7379debf86b3d0e92872c3a7754864
mahsanghani <ghaniahsan@outlook.com>
2022-08-24 18:12:01 -0400

    printSolution

--- algorithms/backtracking/rat_maze.cpp
+++ algorithms/backtracking/rat_maze.cpp
@@ -12,3 +12,16 @@ bool solveMazeUtil(int maze[N][N],
                    int x,
                    int y,
                    int sol[N][N]);
+
+void pointSolution(int sol[N][N])
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << " " << sol[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
