4172b5a3ac1be30059394a4085abc8f62bf11df4
mahsanghani <ghaniahsan@outlook.com>
2022-08-24 17:16:33 -0400

    player

--- advanced/firsttryoninheritance/player.cpp
+++ advanced/firsttryoninheritance/player.cpp
@@ -1,3 +1,20 @@
 //
 // Created by Ahsan Ghani on 2022-08-23.
 //
+#include "person.h"
+#include "player.h"
+
+Player::Player(std::string_view game_param)
+        : m_game(game_param)
+{
+    //first_name = "John"; Compiler errors
+    //last_name = "Snow";
+}
+
+std::ostream& operator<<(std::ostream& out, const Player& player){
+
+    out << "Player : [ game : "  << player.m_game
+        << " names : " << player.get_first_name()
+        << " " << player.get_last_name() << "]";
+    return out;
+}
