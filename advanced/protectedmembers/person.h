5b49c2726acd0e7fc5a66184b1a5024724749343
mahsanghani <ghaniahsan@outlook.com>
2022-08-24 17:19:01 -0400

    person

--- advanced/protectedmembers/person.h
+++ advanced/protectedmembers/person.h
@@ -1,8 +1,40 @@
 //
 // Created by Ahsan Ghani on 2022-08-23.
 //
+#ifndef PERSON_H
+#define PERSON_H
 
-#ifndef CODE_ABBEY_PERSON_H
-#define CODE_ABBEY_PERSON_H
+#include <string>
+#include <iostream>
 
-#endif //CODE_ABBEY_PERSON_H
+class Person
+{
+    friend std::ostream& operator<<(std::ostream& out, const Person& person);
+public:
+    Person();
+    Person(std::string& first_name_param, std::string& last_name_param);
+    ~Person();
+
+    //Getters
+    std::string get_first_name() const{
+        return first_name;
+    }
+
+    std::string get_last_name() const{
+        return last_name;
+    }
+
+    //Setters
+    void set_first_name(std::string_view fn){
+        first_name = fn;
+    }
+    void set_last_name(std::string_view ln){
+        last_name = ln;
+    }
+protected :
+    std::string first_name{"Mysterious"};
+    std::string last_name{"Person"};
+};
+
+
+#endif // PERSON_H
