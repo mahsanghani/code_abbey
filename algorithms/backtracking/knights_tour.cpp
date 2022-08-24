5cc1b6a0ee7a9f195db76c9568ce24ccf91bf7b7
mahsanghani <ghaniahsan@outlook.com>
2022-08-24 17:35:16 -0400

    solveKTUtil

---
+++ algorithms/backtracking/knights_tour.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Ahsan Ghani on 2022-08-24.
+//
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+#define N 8
+
+int solveKTUtil(int x,
+                int y,
+                int movei,
+                int sol[N][N],
+                int xmove[],
+                int ymove[]);
