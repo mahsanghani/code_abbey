55875b58c1b500eb5ba9eca3105873cb3bc694ee
mahsanghani <ghaniahsan@outlook.com>
2022-08-24 17:18:19 -0400

    main

--- advanced/protectedmembers/main75.cpp
+++ advanced/protectedmembers/main75.cpp
@@ -1,3 +1,13 @@
 //
 // Created by Ahsan Ghani on 2022-08-23.
 //
+#include <iostream>
+#include "player.h"
+
+int main(){
+
+    Player p1("Basketball","John","Snow");
+    std::cout << "player : " << p1 << std::endl;
+
+    return 0;
+}
