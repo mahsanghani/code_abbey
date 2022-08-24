44419bc3c8c8fa053cacce1f6a147e15d68e6c6a
mahsanghani <ghaniahsan@outlook.com>
2022-08-24 17:15:56 -0400

    main

--- advanced/firsttryoninheritance/main74.cpp
+++ advanced/firsttryoninheritance/main74.cpp
@@ -1,3 +1,15 @@
 //
 // Created by Ahsan Ghani on 2022-08-23.
 //
+#include <iostream>
+#include "player.h"
+
+int main(){
+
+    Player p1("Basketball");
+    p1.set_first_name("John");
+    p1.set_last_name("Snow");
+    std::cout << "player : " << p1 << std::endl;
+
+    return 0;
+}
