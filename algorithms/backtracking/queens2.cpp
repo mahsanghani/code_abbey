35f17fd406922fb30f03a4c0b027046231571edd
mahsanghani <ghaniahsan@outlook.com>
2022-08-24 18:42:34 -0400

    print solution

---
+++ algorithms/backtracking/queens2.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Ahsan Ghani on 2022-08-24.
+//
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+#define N 4
+
+void printSolution(int board[N][N])
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << " " << board[i][j] << " ";
+        }
+        printf("\n");
+    }
+}
