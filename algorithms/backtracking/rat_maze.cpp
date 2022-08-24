69acb38f36d00f591f23ca6868345761212ef579
mahsanghani <ghaniahsan@outlook.com>
2022-08-24 18:11:53 -0400

    solveMazeUtil

---
+++ algorithms/backtracking/rat_maze.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Ahsan Ghani on 2022-08-24.
+//
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+#define N 4
+
+bool solveMazeUtil(int maze[N][N],
+                   int x,
+                   int y,
+                   int sol[N][N]);
