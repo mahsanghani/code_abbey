f2909872e1c46bb1a9c3776aedcec67564aef05b
mahsanghani <ghaniahsan@outlook.com>
2022-08-24 18:38:50 -0400

    solveNQtil

--- algorithms/backtracking/queens1.cpp
+++ algorithms/backtracking/queens1.cpp
@@ -50,3 +50,27 @@ bool isSafe(int board[N][N], int row, int col)
 
     return true;
 }
+
+bool solveNQUtil(int board[N][N], int col)
+{
+    if (col >= N)
+    {
+        return true;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        if (isSafe(board,i,col))
+        {
+            board[i][col]  = 1;
+        }
+        if (solveNQUtil(board, col+1))
+        {
+            return true;
+        }
+        board[i][col] = 0;
+    }
+
+    return false;
+}
+
