edb80dbd9dcb3491be1b51cc482fc6f8cb34c96a
mahsanghani <ghaniahsan@outlook.com>
2022-08-25 11:24:12 -0400

    engineer

---
+++ advanced/closinginonprivateinheritance/engineer.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Ahsan Ghani on 2022-08-25.
+//
+#include "person.h"
+#include "engineer.h"
+#include <iostream>
+
+Engineer::Engineer()
+{
+}
+
+std::ostream& operator<<(std::ostream& out , const Engineer& operand){
+    out << "Engineer [Full name : " << operand.get_full_name() <<
+        ",age : " << operand.get_age() <<
+        ",address : " << operand.get_address() <<
+        ",contract_count : " << operand.contract_count << "]";
+    return out;
+}
+
+
+Engineer::~Engineer()
+{
+}
