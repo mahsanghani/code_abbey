d001742b1bf87d07ce870fa46eb6bd664ed14a9d
mahsanghani <ghaniahsan@outlook.com>
2022-08-25 11:23:14 -0400

    main

---
+++ advanced/closinginonprivateinheritance/main77.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Ahsan Ghani on 2022-08-25.
+//
+#include <iostream>
+#include "person.h"
+#include "engineer.h"
+#include "civilengineer.h"
+
+int main(){
+
+    Engineer eng1;
+
+    CivilEngineer ce1;
+
+    std::cout << "Done" << std::endl;
+
+
+
+    return 0;
+}
