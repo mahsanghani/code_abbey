7d159a63061ab1ddf20978da6135caf7aed5a06b
mahsanghani <ghaniahsan@outlook.com>
2022-08-26 10:09:23 -0400

    main

---
+++ advanced/defaultargconstructorswithinheritance/main79.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Ahsan Ghani on 2022-08-26.
+//
+#include <iostream>
+#include "person.h"
+#include "engineer.h"
+#include "civilengineer.h"
+
+int main(){
+
+    CivilEngineer ce1;
+
+    return 0;
+}
