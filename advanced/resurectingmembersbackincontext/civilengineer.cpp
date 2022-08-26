33e2733f4041f4a3e9ca726e43ebc3f8ef96cff1
mahsanghani <ghaniahsan@outlook.com>
2022-08-26 10:07:18 -0400

    civil engineer

--- advanced/resurectingmembersbackincontext/civilengineer.cpp
+++ advanced/resurectingmembersbackincontext/civilengineer.cpp
@@ -1,3 +1,25 @@
 //
 // Created by Ahsan Ghani on 2022-08-25.
 //
+#include "person.h"
+#include "engineer.h"
+#include "civilengineer.h"
+#include <iostream>
+
+CivilEngineer::CivilEngineer()
+{
+}
+
+std::ostream& operator<<(std::ostream& out, const CivilEngineer& operand){
+    out << "CivilEngineer [Full name : " << operand.get_full_name() <<
+        ",age : " << operand.get_age() <<
+        ",address : " << operand.get_address() <<
+        ",contract_count : " << operand.get_contract_count() <<
+        ",speciality : " << operand.m_speciality << "]";
+
+    return out;
+}
+
+CivilEngineer::~CivilEngineer()
+{
+}
