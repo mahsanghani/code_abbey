a8618d898ecfd6fa7a79480f193f33cc712a7b66
mahsanghani <ghaniahsan@outlook.com>
2022-08-26 10:08:06 -0400

    engineer

--- advanced/resurectingmembersbackincontext/engineer.h
+++ advanced/resurectingmembersbackincontext/engineer.h
@@ -1,8 +1,40 @@
 //
 // Created by Ahsan Ghani on 2022-08-25.
 //
+#ifndef ENGINEER_H
+#define ENGINEER_H
+#include "person.h"
 
-#ifndef CODE_ABBEY_ENGINEER_H
-#define CODE_ABBEY_ENGINEER_H
+//Engineer is doing private inheritance
 
-#endif //CODE_ABBEY_ENGINEER_H
+class Engineer : private Person
+{
+    friend std::ostream& operator<<(std::ostream& out , const Engineer& operand);
+public:
+    Engineer();
+    ~Engineer();
+
+    void build_something(){
+        m_full_name = "John Snow"; // OK
+        m_age = 23; // OK
+        //m_address = "897-78-723"; Compiler error
+    }
+
+public :
+    using Person::add;
+
+protected :
+    using Person::get_full_name;
+    using Person::get_age;
+    using Person::get_address;
+    //using Person::m_address; // Compiler error
+
+    int get_contract_count() const{
+        return contract_count;
+    }
+
+private :
+    int contract_count{0};
+};
+
+#endif // ENGINEER_H
