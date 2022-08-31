ee23b4ca5ea42ba8d64ea5499e7e60a49a158dc2
mahsanghani <ghaniahsan@outlook.com>
2022-08-31 11:32:03 -0700

    main

---
+++ advanced/reusedsymbolsininheritance/main84.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Ahsan Ghani on 2022-08-31.
+//
+#include <iostream>
+#include "child.h"
+
+int main(){
+    Child child(33);
+    child.print_var();// Calls the method in Child
+    child.Parent::print_var(); // Calls the method in Parent,
+    // value in parent just contains junk or whatever
+    // in class initialization we did.
+
+    std::cout << "--------" << std::endl;
+    child.show_values();
+    return 0;
+}
