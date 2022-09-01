d4d84cc1a2435354e1fc7d6430372ba5a4d4f43d
mahsanghani <ghaniahsan@outlook.com>
2022-09-01 10:33:35 -0700

    book

---
+++ projects/LMS/lms_main.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Ahsan Ghani on 2022-09-01.
+//
+#include <iostream>
+#include <conio.h>
+#include <string>
+
+using namespace std;
+
+class Book
+{
+private:
+    // variables
+    string isbn;
+    string title;
+    string author;
+    string edition;
+    string publication;
+
+};
