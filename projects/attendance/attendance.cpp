61ecab9335e08eba7aabbc0bb8fab92aa6db964e
mahsanghani <ghaniahsan@outlook.com>
2022-09-11 14:05:18 -0700

    attend

---
+++ projects/attendance/attendance.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Ahsan Ghani on 2022-09-11.
+//
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+
+using namespace std;
+
+class attend
+{
+    float p=0, t=0, w;
+public:
+    int enter();
+    void calculate();
+};
