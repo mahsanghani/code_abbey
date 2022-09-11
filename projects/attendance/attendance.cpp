223c3c9f531bc09dc1834201d106f0ca59b8f17c
mahsanghani <ghaniahsan@outlook.com>
2022-09-11 14:10:15 -0700

    date

--- projects/attendance/attendance.cpp
+++ projects/attendance/attendance.cpp
@@ -14,3 +14,60 @@ public:
     int enter();
     void calculate();
 };
+
+int attend::enter()
+{
+    int date, month, year;
+    int i, n, k, a, c;
+    int z[20];
+    cout << ">>ENTER ANY KEY TO GIVE THE DATE..." << endl;
+    cin >> i;
+
+    while(i)
+    {
+        cout << "->Enter DATE/MONTH/YEAR: " << endl;
+        cin >> date >> month >> year;
+        cout << endl;
+        if(month== 1||month==3||month==5||month==7||month==8||month==10||month==12)
+        {
+            if(date>31||date==0)
+            {
+                c=0;
+            }
+            else
+            {
+                c=1;
+            }
+        }
+        if(month== 4||month==6||month==9||month==11)
+        {
+            if(date>30 ||date==0)
+            {
+                c=0;
+            }
+            else
+            {
+                c=1;
+            }
+        }
+        if(month==2)
+        {
+            if(date>29||date==0)
+            {
+                c=0;
+            }
+            else
+            {
+                c=1;
+            }
+        }
+        if(month>12)
+        {
+            c=0;
+        }
+        if(month==0||year==0)
+        {
+            c=0;
+        }
+    }
+}
