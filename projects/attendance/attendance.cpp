17ae7d5a424852e6cc30cea832c6968c3c03d924
mahsanghani <ghaniahsan@outlook.com>
2022-09-11 14:16:29 -0700

    enter attendance

--- projects/attendance/attendance.cpp
+++ projects/attendance/attendance.cpp
@@ -69,5 +69,75 @@ int attend::enter()
         {
             c=0;
         }
+
+        switch(c)//gives if the given date is correct or not and update
+        {
+            case 0:cout<<"Please enter::VALID DATE::"<<endl<<endl;
+                break;
+            case 1:
+                cout<<">>ENTER NUMBER OF PERIODS IN THE DAY...(max periods=7): ";
+                cin>>n;
+                cout<<endl;
+                for(k=1;k<n+1;k++)
+                {
+                    cout<<"->ENTER '1' IF PRESENT ELSE '0' TO UPDATE THE STATUS OF PERIOD "<<k<<" :";
+                    cin>>a;
+                    if(a==1)
+                    {
+                        p++;
+                        t++;
+                        z[k]=1;
+                        cout<<"   ......................................................................"<<endl<<endl;
+                        cout<<"                         :::UPDATED AS PRESENT:::"<<endl<<endl;
+                        cout<<"   ......................................................................"<<endl<<endl;
+                    }
+                    else {
+                        t++;
+                        z[k] = 0;
+                        cout << "   ......................................................................" << endl
+                             << endl;
+                        cout << "                          :::UPDATED AS ABSENT:::" << endl << endl;
+                        cout << "   ......................................................................" << endl
+                             << endl;
+                    }}
+                cout<<endl;
+                cout<<"                    ::::::ATTENDANCE OF THE DAY IS UPDATED AS:::::"<<endl;
+                for(k=1;k<n+1;k++)
+                {
+                    cout<<"__________";
+                }
+                cout<<endl<<endl;
+                cout<<"DD/MM/YY    ";
+                for(k=1;k<n+1;k++)
+                {
+                    cout<<"p"<<k<<"    ";
+                }
+                cout<<endl<<endl;
+                cout<<date<<"/"<<month<<"/"<<year<<"   ";
+                for(k=1;k<n+1;k++)
+                {
+                    if(z[k]==1)
+                    {
+                        cout<<"P"<<"     ";
+                    }
+                    else
+                    {
+                        cout<<"A"<<"     ";
+                    }
+                }
+                cout<<endl<<endl;
+                for(k=1;k<n+1;k++)
+                {
+                    cout<<"__________";
+                }
+                cout<<endl;
+                cout<<endl;
+                break;
+            default://To exit
+                exit(0);
+        }
+        cout<<">>ENTER 1 TO TO UPDATE ANOTHER DAY ELSE 0  :";
+        cin>>i;
+        cout<<endl;
     }
 }
