bb52ffda8e6f970e39bcaa78bf655faa1294e921
mahsanghani <ghaniahsan@outlook.com>
2022-09-12 16:39:06 -0700

    imports

---
+++ projects/LMS/reportcard.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Ahsan Ghani on 2022-09-12.
+//
+#include <iostream>
+#include <conio.h>
+#include <fstream>
+#include <string.h>
+#include <process.h>
+#include <windows.h>
+
+using namespace std;
+
