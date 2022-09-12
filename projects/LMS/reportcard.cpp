a0a0bbe3a029500babe94ebf446fb767dc451011
mahsanghani <ghaniahsan@outlook.com>
2022-09-12 16:55:20 -0700

    search student

--- projects/LMS/reportcard.cpp
+++ projects/LMS/reportcard.cpp
@@ -353,3 +353,41 @@ void search()
     }
     f.close();
 }
+
+void search_student()
+{
+    STUDENT s;
+    ifstream f("Record.dat");
+    int n,flag=0;
+    char a[5];
+    if(!f)
+    {
+        cout<<"\n\n\n\n\n\n\n\n\n\n\n\t\t\t\tFILE NOT FOUND!";
+        cout<<"\n\n\n\n\n\n\n\n";
+    }
+    else
+    {
+        cout<<"\n\n\n\n\n\n\n\n\n\t\t\t";
+        cout<<"ENTER YOUR ROLL NUMBER: ";
+        cin>>n;
+        cout<<"\n\t\t\t";
+        cout<<"ENTER YOUR SECURE CODE: ";
+        cin.ignore();
+        cin.getline(a,5);
+        system("cls");
+        while(f.read((char*)&s,sizeof(s)))
+        {
+            if((n==s.rno) && (strcmp(a,s.sec_code)==0))
+            {
+                flag=1;
+                s.display_report();
+            }
+        }
+        if(flag==0)
+        {
+            cout<<"\n\n\n\n\n\n\n\n\n\n\n\t\t\t\tRECORD NOT FOUND!";
+            cout<<"\n\n\n\n\n\n\n\n";
+        }
+    }
+    f.close();
+}
