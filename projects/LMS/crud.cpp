a30d368e3977b992402e98761b7f258625ed0e58
mahsanghani <ghaniahsan@outlook.com>
2022-09-12 17:29:53 -0700

    welcome

--- projects/LMS/crud.cpp
+++ projects/LMS/crud.cpp
@@ -40,3 +40,49 @@ void error()
     cout << " \n " ;
 }
 
+void  welcome ()        // Enter the password to enter the system, welcome interface
+{
+    char password [ 5 ];
+    char num [ 5 ];
+
+    strcpy (num, " 123 " );
+    loopback3: printf ( "  \n\n\n\n\n\n\n\n " );
+    system ( " color F1 " );
+    printf ( " \t\t\tWelcome to use the student performance management system! \n " );
+    printf ( " \t\t\t\tpassword : " );
+    scanf ( " %s " , &password);
+    if ( strcmp (password, num) == 0 )
+    {
+        system ( " cls " );
+        system ( " color F0 " );
+        printf ( "  \n\n\n\n\n\n\n\n\n\t\t\tWelcome to use the student performance management system! " );
+        Sleep ( 300 );
+        system ( " cls " );
+        system ( " color F2 " );
+        printf ( "  \n\n\n\n\n\n\n\n\n\t\t\tWelcome to use the student performance management system! " );
+        Sleep ( 300 );
+        system ( " cls " );
+        system ( " color F3 " );
+        printf ( "  \n\n\n\n\n\n\n\n\n\t\t\tWelcome to use the student performance management system! " );
+        Sleep ( 300 );
+        system ( " cls " );
+        system ( " color F4 " );
+        printf ( "  \n\n\n\n\n\n\n\n\n\t\t\tWelcome to use the student performance management system! " );
+        Sleep ( 300 );
+        system ( " cls " );
+        system ( " color F5 " );
+        printf ( "  \n\n\n\n\n\n\n\n\n\t\t\tWelcome to use the student performance management system! " );
+        Sleep ( 300 );
+        system ( " cls " );
+        system ( " color F0 " );
+        printf ( "  \n\n\n\n\n\n\n\n\n\t\t\tWelcome to use the student performance management system! " );
+        Sleep ( 300 );
+    }
+    else
+    {
+        printf ( " \t\t\t\t password error! " );            // re-output the password error
+        Sleep ( 2000 );
+        system ( " cls " );
+        goto loopback3;
+    }
+}
