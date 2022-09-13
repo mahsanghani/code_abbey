ac458c4d555ef1ee726a39fa381bff0bab1d5bbb
mahsanghani <ghaniahsan@outlook.com>
2022-09-13 14:14:07 -0700

    remove member

--- projects/LMS/university.cpp
+++ projects/LMS/university.cpp
@@ -23,3 +23,36 @@ public:
 
   int getId() const { return id; }
 };
+
+template <typename T>
+void removeMember(const string filePath, const string tempFilePath,
+                  const T schoolMember) {
+  system("cls");
+  int inputId = 0;
+  short flag = 0;
+  ifstream fileToRead(filePath, ios::binary);
+  ofstream fileToWrite(tempFilePath, ios::binary | ios::app);
+  fileToRead.seekg(0);
+  cout << "Enter ID of person whose data has to be removed: " << endl;
+  cin >> inputId;
+
+  while (fileToRead.read((char *)&schoolMember, sizeof(schoolMember))) {
+    if (inputId == schoolMember.getId()) {
+      flag++;
+    } else {
+      fileToWrite((char *)&schoolMember, sizeof(schoolMember));
+    }
+  }
+
+  if (flag == 0) {
+    cout << "Sorry, No Math Found." << endl;
+  } else {
+    cout << "Data of ID " << inputId << " has been removed from file.";
+  }
+
+  fileToRead.close();
+  fileToWrite.close();
+  remove(filePath.c_str());
+  rename(tempFilePath.c_str(), filePath.c_str());
+  basicNavigation();
+}
