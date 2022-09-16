1677b075994f127c46ef36120493e888e95653d2
mahsanghani <ghaniahsan@outlook.com>
2022-09-15 21:01:37 -0700

    include

---
+++ projects/video/webcam.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Ahsan Ghani on 2022-09-15.
+//
+#include <ctime>
+#include <iostream>
+#include <opencv/cv.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <sstream>
+#include <string>
+#include <unistd.h>
