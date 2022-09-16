075e27c18fe731fd3b7649c99f4c9148705630f7
mahsanghani <ghaniahsan@outlook.com>
2022-09-15 20:48:38 -0700

    include

---
+++ projects/audio/mp3.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Ahsan Ghani on 2022-09-15.
+//
+#include "resource.h"
+
+#include <cstdlib>
+#include <errno.h>
+#include <fstream>
+#include <iostream>
+#include <math.h>
+#include <mmsystem.h>
+
+using namespace std;
