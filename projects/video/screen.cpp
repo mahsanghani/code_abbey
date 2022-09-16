ca799902359763d4b6b93e784013b73d7b098a2a
mahsanghani <ghaniahsan@outlook.com>
2022-09-15 21:00:32 -0700

    include

---
+++ projects/video/screen.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Ahsan Ghani on 2022-09-15.
+//
+#include "opencv2/opencv.hpp"
+#include <cstdio>
+#include <ctime>
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+
+using namespace std;
+using namespace cv;
