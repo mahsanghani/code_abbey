9339f3d82019a14c3e1dfa44234d51a5c3ead12a
mahsanghani <ghaniahsan@outlook.com>
2022-09-15 20:59:19 -0700

    wav

--- projects/audio/mp3.cpp
+++ projects/audio/mp3.cpp
@@ -516,3 +516,61 @@ int readSample(int number, bool leftchannel) {
     return (int)0xefffffff;
   }
 }
+
+void Wav(char *c) {
+
+  filename = new char[strlen(c) + 1];
+  strcpy(filename, c);
+  // open filepointer readonly
+  stream = fopen(filename, "r");
+  if (stream == NULL) {
+    cout << "Could not open " << filename << endl;
+  } else {
+    // declare a char buff to store some values in
+    char *buff = new char[5];
+    buff[4] = '\0';
+    // read the first 4 bytes
+    fread((void *)buff, 1, 4, stream);
+    // the first four bytes should be 'RIFF'
+    if (strcmp((char *)buff, "RIFF") == 0) {
+
+      // read byte 8,9,10 and 11
+      fseek(stream, 4, SEEK_CUR);
+      fread((void *)buff, 1, 4, stream);
+      // this should read "WAVE"
+      if (strcmp((char *)buff, "WAVE") == 0) {
+        // read byte 12,13,14,15
+        fread((void *)buff, 1, 4, stream);
+        // this should read "fmt "
+        if (strcmp((char *)buff, "fmt ") == 0) {
+          fseek(stream, 20, SEEK_CUR);
+          // final one read byte 36,37,38,39
+          fread((void *)buff, 1, 4, stream);
+          if (strcmp((char *)buff, "data") == 0) {
+
+            // Now we know it is a wav file, rewind the stream
+            rewind(stream);
+            // now is it mono or stereo ?
+            fseek(stream, 22, SEEK_CUR);
+            fread((void *)buff, 1, 2, stream);
+            if (buff[0] == 0x02) {
+              mono = false;
+            } else {
+              mono = true;
+            }
+            // read the sample rate
+            fread((void *)&s_rate, 1, 4, stream);
+            fread((void *)&byte_sec, 1, 4, stream);
+            byte_samp = 0;
+            fread((void *)&byte_samp, 1, 2, stream);
+            bit_samp = 0;
+            fread((void *)&bit_samp, 1, 2, stream);
+            fseek(stream, 4, SEEK_CUR);
+            fread((void *)&length, 1, 4, stream);
+          }
+        }
+      }
+    }
+    delete buff;
+  }
+}
