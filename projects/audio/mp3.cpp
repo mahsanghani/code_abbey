68aa784214d4a560d1d7812932bf932aafc85194
mahsanghani <ghaniahsan@outlook.com>
2022-09-15 20:57:56 -0700

    windows procedures

--- projects/audio/mp3.cpp
+++ projects/audio/mp3.cpp
@@ -11,3 +11,19 @@
 #include <mmsystem.h>
 
 using namespace std;
+
+int number, length, byte_samp, byte_sec, bit_samp;
+bool mono = TRUE;
+bool PLAY = FALSE;
+// errno_t wavfile;
+char *filename;
+int s_rate = 11025;
+double limit = 5000.0;
+FILE *stream;
+/*  Declare Windows procedure  */
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+/* Declare procedures */
+int readSample(int number, bool leftchannel);
+void Wav(char *c);
+/*  Make the class name into a global variable  */
+char szAppName[] = "Recorder";
