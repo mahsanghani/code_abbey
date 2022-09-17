51e42d67d7aa7edd0198335a1ae55d3ce9dd1daf
mahsanghani <ghaniahsan@outlook.com>
2022-09-16 21:25:48 -0700

    include

---
+++ UdacityCpp/Intro_to_CPP/formatting.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Ahsan Ghani on 2022-09-16.
+//
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+enum class State { kEmpty, kObstacle };
