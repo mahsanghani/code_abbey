55d339f6baf701655af29a9b237585f88576b42e
mahsanghani <ghaniahsan@outlook.com>
2022-09-16 22:44:33 -0700

    ReadBoardFile

--- UdacityCpp/Intro_to_CPP/parse.cpp
+++ UdacityCpp/Intro_to_CPP/parse.cpp
@@ -20,3 +20,13 @@ vector<int> ParseLine(string line) {
   }
   return vec;
 }
+
+void ReadBoardFile(string path) {
+  ifstream myfile(path);
+  if (myfile) {
+    string line;
+    while (getline(myfile, line)) {
+      cout << line << endl;
+    }
+  }
+}
