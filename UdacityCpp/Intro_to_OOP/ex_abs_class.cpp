c9f7a60e1b320685f846d502e94b24a217a28b52
mahsanghani <ghaniahsan@outlook.com>
2022-09-19 16:03:11 -0700

    abstraction

---
+++ UdacityCpp/Intro_to_OOP/ex_abs_class.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Ahsan Ghani on 2022-09-19.
+//
+#include <iostream>
+
+class Abstraction {
+private:
+  int var1_;
+  double var2_;
+
+public:
+  Abstraction(int var1, double var2);
+  void printVal();
+};
