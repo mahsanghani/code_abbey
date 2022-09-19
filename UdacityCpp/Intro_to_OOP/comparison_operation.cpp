042e5f1d2ac4df5b405b45d3a76003e4ff106b65
mahsanghani <ghaniahsan@outlook.com>
2022-09-19 11:49:56 -0700

    comparison operation

---
+++ UdacityCpp/Intro_to_OOP/comparison_operation.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Ahsan Ghani on 2022-09-19.
+//
+#include <iostream>
+
+using namespace std;
+
+template <class T> T ReturnMax(T a, T b) {
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+int main() {
+  int num1 = 50;
+  int num2 = 10;
+
+  int res = ReturnMax(num1, num2);
+  cout << "Bigger one is: " << res << endl;
+
+  double n1 = 45.65;
+  double n2 = 100.45;
+
+  double res2 = ReturnMax(n1, n2);
+  cou t << "Bigger on is: " << res2 << endl;
+
+  char char1 = 'c';
+  char char2 = 'y';
+
+  char char3 = ReturnMax(char1, char2);
+  cout << "Bigger one is: " << res3 << endl;
+}
