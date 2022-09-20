a4f035119a88dff8af3371d70b2eb7c973ba22cd
mahsanghani <ghaniahsan@outlook.com>
2022-09-20 15:32:39 -0700

    main

--- UdacityCpp/Intro_to_OOP/ex_deduction.cpp
+++ UdacityCpp/Intro_to_OOP/ex_deduction.cpp
@@ -14,3 +14,19 @@ template <typename _Tf> void print(vector<_Tf> v) {
     cout << v[i] << endl;
   }
 }
+
+int main() {
+  std::vector<double> v = {9, 3, 2, 32.32, 10.323, 10.0003, 4.9789};
+  std::vector<char> v2 = {'a', 'Z', 'M', 'x', 'Y', 'c', 'U'};
+
+  print(v);
+
+  sort(v.begin(), v.end(), func<double>);
+
+  print(v);
+  print(v2);
+
+  sort(v2.begin(), v2.end(), func<char>);
+
+  print(v2);
+}
