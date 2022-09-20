ac1c119d7656f558e58f7b787fc9f11eaafbb183
mahsanghani <ghaniahsan@outlook.com>
2022-09-20 15:31:30 -0700

    print

--- UdacityCpp/Intro_to_OOP/ex_deduction.cpp
+++ UdacityCpp/Intro_to_OOP/ex_deduction.cpp
@@ -8,3 +8,9 @@
 using namespace std;
 
 template <typename _Tf> bool func(_Tf x, _Tf y) { return (x < y); }
+
+template <typename _Tf> void print(vector<_Tf> v) {
+  for (int i = 0; i < v.size(); i++) {
+    cout << v[i] << endl;
+  }
+}
