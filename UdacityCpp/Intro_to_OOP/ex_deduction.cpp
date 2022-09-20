67ad25337b5d7ea6dd4cbe22e012ad8b01637dc5
mahsanghani <ghaniahsan@outlook.com>
2022-09-20 15:29:28 -0700

    func

---
+++ UdacityCpp/Intro_to_OOP/ex_deduction.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Ahsan Ghani on 2022-09-20.
+//
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <typename _Tf> bool func(_Tf x, _Tf y) { return (x < y); }
