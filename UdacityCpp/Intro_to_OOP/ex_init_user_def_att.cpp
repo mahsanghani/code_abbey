e960261bb9198b479f1875f5094be251b4bb1274
mahsanghani <ghaniahsan@outlook.com>
2022-09-20 20:11:00 -0700

    A

---
+++ UdacityCpp/Intro_to_OOP/ex_init_user_def_att.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Ahsan Ghani on 2022-09-20.
+//
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class A {
+public:
+  A(int a, double b, char c) : attr1(a), attr2(b), attr3(c) {
+    std::cout << "A constructor"
+              << "\n";
+  }
+  void print() const {
+    std::cout << "A: " << attr1 << " " << attr2 << " " << attr3 << "\n";
+  }
+
+private:
+  int attr1;
+  double attr2;
+  char attr3;
+};
