3822a663a700b618efb565b16618d8c3c101ffdf
mahsanghani <ghaniahsan@outlook.com>
2022-09-21 13:57:32 -0700

    circle

--- UdacityCpp/Intro_to_OOP/ex_line_class.cpp
+++ UdacityCpp/Intro_to_OOP/ex_line_class.cpp
@@ -10,3 +10,15 @@ class Line {
 protected:
   double length_;
 };
+
+class Circle : public Line {
+public:
+  double getArea() const;
+  void setRadius(double radius);
+};
+
+const double PI = 3.14159;
+
+double Circle::getArea() const { return this->length_ * this->length_ * PI; }
+
+void Circle::setRadius(double radius) { this->length_ = radius; }
