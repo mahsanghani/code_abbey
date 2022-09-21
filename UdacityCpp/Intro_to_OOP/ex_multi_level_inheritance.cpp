5e283556ca1ae45331b9af494924e8bd80dca03a
mahsanghani <ghaniahsan@outlook.com>
2022-09-21 14:01:54 -0700

    B

--- UdacityCpp/Intro_to_OOP/ex_multi_level_inheritance.cpp
+++ UdacityCpp/Intro_to_OOP/ex_multi_level_inheritance.cpp
@@ -17,3 +17,13 @@ public:
 void A::SetA(int val_a) { A::a_attr = val_a; }
 
 int A::GetA(void) { return A::a_attr; }
+
+class B : public A {
+private:
+  int b_attr;
+
+public:
+  void SetAB(int val_a, int val_b);
+  void PrintAB(void);
+  int GetB(void);
+};
