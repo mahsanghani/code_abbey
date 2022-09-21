10ad2f97f435e967f93d7c03ec46b53175e1f015
mahsanghani <ghaniahsan@outlook.com>
2022-09-20 19:55:09 -0700

    average

---
+++ UdacityCpp/Intro_to_OOP/ex_func_template.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Ahsan Ghani on 2022-09-20.
+//
+#include <iostream>
+
+using namespace std;
+
+template <typename _Tf> _Tf Average(const _Tf &num1, const _Tf &num2) {
+  return (num1 + num2) / 2;
+}
