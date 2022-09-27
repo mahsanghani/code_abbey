c60c2a209aa4a7c538058af6bc712b461a7f78a9
mahsanghani <ghaniahsan@outlook.com>
2022-09-27 13:23:31 -0600

    point

---
+++ UdacityCpp/Intro_to_OOP/multi_operator_overload.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Ahsan Ghani on 2022-09-27.
+//
+#include <iostream>
+
+using namespace std;
+
+class Point {
+private:
+  double x_;
+  double y_;
+
+public:
+  Point(double x = 0, double y = 0) : x_(x), y_(y) {}
+
+  Point operator+(Point const &pt);
+
+  void Print();
+};
