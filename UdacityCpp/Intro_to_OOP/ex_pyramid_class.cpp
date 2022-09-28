8bc371e765c88baf308e5a773d63768d0f381973
mahsanghani <ghaniahsan@outlook.com>
2022-09-27 21:54:38 -0600

    pyramid

--- UdacityCpp/Intro_to_OOP/ex_pyramid_class.cpp
+++ UdacityCpp/Intro_to_OOP/ex_pyramid_class.cpp
@@ -1,3 +1,30 @@
 //
 // Created by Ahsan Ghani on 2022-09-27.
 //
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+class Pyramid {
+private:
+  double base_length_;
+  double base_width_;
+  double height_;
+
+public:
+  Pyramid(double base_length, double base_width, double height);
+  Pyramid();
+
+  double getBaseLength() const;
+  double getBaseWidth() const;
+  double getHeight() const;
+
+  void setBaseLength(double base_length);
+  void setBaseWidth(double base_width);
+  void setHeight(double height);
+
+  double Volume() const;
+  double SurfaceArea() const;
+  bool isAllPositive() const;
+};
