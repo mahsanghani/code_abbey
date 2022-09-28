bd78875878592c2e8271c3284080cb250fe68cae
mahsanghani <ghaniahsan@outlook.com>
2022-09-27 22:57:36 -0600

    sphere

---
+++ UdacityCpp/Intro_to_OOP/ex_sphere_class.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Ahsan Ghani on 2022-09-27.
+//
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+class Sphere {
+public:
+  Sphere(double radius);
+  double getVolume();
+
+private:
+  double radius_;
+};
