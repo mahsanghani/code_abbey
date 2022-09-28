5cbd64c76d87b5b3ee7ec113fbac499c07b73fa8
mahsanghani <ghaniahsan@outlook.com>
2022-09-27 22:58:08 -0600

    main

--- UdacityCpp/Intro_to_OOP/ex_sphere_class.cpp
+++ UdacityCpp/Intro_to_OOP/ex_sphere_class.cpp
@@ -18,3 +18,8 @@ private:
 Sphere::Sphere(double radius) : radius_(radius) {}
 
 double Sphere::getVolume() { return M_PI * 4 / 3 * pow(radius_, 3); }
+
+int main() {
+  Sphere s(5);
+  cout << s.getVolume() << endl;
+}
