88cf32f75666bbd409dc509214daee73437b6d49
mahsanghani <ghaniahsan@outlook.com>
2022-09-28 22:15:08 -0600

    sudoku 2

---
+++ leetcode/sudoku2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # [row[i] for row in matrix]
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        cubes = collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                if (board[r][c] in rows[r] or
+                        board[r][c] in cols[c] or
+                        board[r][c] in cubes[(r // 3, c // 3)]):
+                    return False
+
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                cubes[r // 3, c // 3].add(board[r][c])
+        return True
