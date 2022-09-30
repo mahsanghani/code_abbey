f91c01e78a9bb63d62013dace689f318ade6a507
mahsanghani <ghaniahsan@outlook.com>
2022-09-30 13:09:09 -0600

    shape

--- UdacityCpp/Intro_to_OOP/ex_virtual_functions.cpp
+++ UdacityCpp/Intro_to_OOP/ex_virtual_functions.cpp
@@ -1,3 +1,13 @@
 //
 // Created by Ahsan Ghani on 2022-09-29.
 //
+#include <iostream>
+using namespace std;
+const double PI = 3.14159;
+
+class Shape {
+public:
+  Shape() {}
+  virtual double Area() const = 0;
+  virtual double PermiterLength() const = 0;
+};
