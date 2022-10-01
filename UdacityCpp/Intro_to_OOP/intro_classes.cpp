990e2e9073589aa5481da020bb467817c34a0824
mahsanghani <ghaniahsan@outlook.com>
2022-10-01 12:38:07 -0600

    Date

---
+++ UdacityCpp/Intro_to_OOP/intro_classes.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Ahsan Ghani on 2022-10-01.
+//
+#include <iostream>
+
+using namespace std;
+
+class Date {
+public:
+  int day;
+  int month;
+  int year;
+
+  void SetDate(int day, int month, int year);
+};
