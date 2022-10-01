9329e2e7bfd185e18a6d9ef14afa57d27812a842
mahsanghani <ghaniahsan@outlook.com>
2022-10-01 12:41:36 -0600

    main

--- UdacityCpp/Intro_to_OOP/intro_to_inheritance.cpp
+++ UdacityCpp/Intro_to_OOP/intro_to_inheritance.cpp
@@ -22,3 +22,11 @@ class ChildClass_1 : public ParentClass {};
 class ChildClass_2 : protected ParentClass {};
 
 class ChildClass_3 : private ParentClass {};
+
+int main() {
+  ChildClass_1 c1;
+  ChildClass_2 c2;
+  ChildClass_3 c3;
+  c1.var1 = 4;
+  std::cout << c1.var1 << endl;
+}
