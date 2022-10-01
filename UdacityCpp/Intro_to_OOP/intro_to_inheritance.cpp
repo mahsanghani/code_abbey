31d33dddb51a2aafa0cfb6528dc6041e1f8f28d0
mahsanghani <ghaniahsan@outlook.com>
2022-10-01 12:40:45 -0600

    ParentClass

---
+++ UdacityCpp/Intro_to_OOP/intro_to_inheritance.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Ahsan Ghani on 2022-10-01.
+//
+#include <iostream>
+
+using namespace std;
+
+class ParentClass {
+
+public:
+  int var1;
+
+protected:
+  int var2;
+
+private:
+  int var3;
+};
