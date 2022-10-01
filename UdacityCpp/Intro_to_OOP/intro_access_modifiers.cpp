259d6ce030938c12250afa0c45428a6193dc79eb
mahsanghani <ghaniahsan@outlook.com>
2022-10-01 12:30:54 -0600

    SetDate

--- UdacityCpp/Intro_to_OOP/intro_access_modifiers.cpp
+++ UdacityCpp/Intro_to_OOP/intro_access_modifiers.cpp
@@ -3,6 +3,8 @@
 //
 #include <iostream>
 
+using namespace std;
+
 class Date {
 private:
   int day;
@@ -15,3 +17,21 @@ public:
   int GetMonth() const;
   int GetYear() const;
 };
+
+void Date::SetDate(int day, int month, int year) {
+  int day_numbers[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  ;
+
+  if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+    day_numbers[1]++;
+  }
+
+  if (year < 1 || day < 1 || month < 1 || month > 12 ||
+      day > day_numbers[month - 1]) {
+    throw domain_error("invalid date!");
+  }
+
+  Date::day = day;
+  Date::month = month;
+  Date::year = year;
+}
