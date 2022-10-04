f28f2c4ccda3c2370e308b69143a15ba6e6245d8
mahsanghani <ghaniahsan@outlook.com>
2022-10-04 12:10:51 -0600

    malloc main

---
+++ UdacityCpp/Intro_to_Memory_Mgmt/malloc.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Ahsan Ghani on 2022-10-04.
+//
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int *ptr = NULL;
+
+  ptr = new (nothrow) int;
+
+  ptr = malloc(4 * sizeof(int));
+  for (int i = 0; i < 4; i++) {
+    cout << &ptr[i] << ptr[i] << endl;
+  }
+
+  for (int i = 0; i < 4; i++) {
+    memset(&ptr[i], ('A' + i), sizeof(int));
+  }
+
+  for (int i = 0; i < 4; i++) {
+    cout << &ptr[i] << ptr[i] << endl;
+  }
+
+  return 0;
+}
