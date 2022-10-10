4871e73d4739a2e31384e0d494327f19165e90e5
mahsanghani <ghaniahsan@outlook.com>
2022-10-09 22:23:29 -0600

    iterator

---
+++ leetcode/iteratorBST.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.stack = []
+        while root:
+            self.stack.append(root)
+            root = root.left
+
+    def next(self) -> int:
+        result = self.stack.pop()
+        current = result.right
+        while current:
+            self.stack.append(current)
+            current = current.left
+        return result.val
+
+    def hasNext(self) -> bool:
+        return self.stack != []
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
