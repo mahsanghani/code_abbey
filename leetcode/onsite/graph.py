8edc68e5bf8ace9038319414788ec528ab19f14e
mahsanghani <ghaniahsan@outlook.com>
2022-10-17 13:30:14 -0600

    graph

---
+++ leetcode/onsite/graph.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.graph = defaultdict(list)
+
+    def addEdge(self, source, destination):
+        self.graph[source].append(destination)
+
+    def findEdges(self, source):
+        return self.graph[source]
+
+    def isReachable(self, source, destination):
+        visit = [False] * (self.vertices)
+        queue = []
+        queue.append(source)
+        visit[source] = True
+
+        while queue:
+            n = queue.pop(0)
+            if n == destination:
+                return True
+
+            for i in self.graph[n]:
+                if not visit[i]:
+                    queue.append(i)
+                    visit[i] = True
+
+        return False
