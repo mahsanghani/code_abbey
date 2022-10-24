97c14bf6ca45385c8bfc0a1a8c69336e896a1d16
mahsanghani <ghaniahsan@outlook.com>
2022-10-23 20:34:13 -0600

    ParseLine

---
+++ UdacityCpp/A_star/heuristic.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Ahsan Ghani on 2022-10-23.
+//
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+enum class State { kEmpty, kObstacle };
+
+vector<State> ParseLine(string line) {
+  istringstream sline(line);
+  int n;
+  char c;
+  vector<State> row;
+  while (sline >> n >> c && c == ',') {
+    if (n == 0) {
+      row.push_back(State::kEmpty);
+    } else {
+      row.push_back(State::kObstacle);
+    }
+  }
+  return row;
+}
