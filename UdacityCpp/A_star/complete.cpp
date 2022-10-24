07aae5b7980d82fc36a3fc027e0c510a4f3665dd
mahsanghani <ghaniahsan@outlook.com>
2022-10-23 20:30:20 -0600

    ExpandNeighbors

--- UdacityCpp/A_star/complete.cpp
+++ UdacityCpp/A_star/complete.cpp
@@ -66,3 +66,27 @@ void AddToOpen(int x, int y, int g, int h, vector<vector<int>> &openlist,
   openlist.push_back(vector<int>{x, y, g, h});
   grid[x][y] = State::kClosed;
 }
+
+void ExpandNeighbors(const vector<int> &current, int goal[2],
+                     vector<vector<int>> &openlist,
+                     vector<vector<State>> &grid) {
+  // Get current node's data.
+  int x = current[0];
+  int y = current[1];
+  int g = current[2];
+
+  // Loop through current node's potential neighbors.
+  for (int i = 0; i < 4; i++) {
+    int x2 = x + delta[i][0];
+    int y2 = y + delta[i][1];
+
+    // Check that the potential neighbor's x2 and y2 values are on the grid and
+    // not closed.
+    if (CheckValidCell(x2, y2, grid)) {
+      // Increment g value and add neighbor to open list.
+      int g2 = g + 1;
+      int h2 = Heuristic(x2, y2, goal[0], goal[1]);
+      AddToOpen(x2, y2, g2, h2, openlist, grid);
+    }
+  }
+}
