7402c0223ee8c291400467a3dacf1e1ba25582af
mahsanghani <ghaniahsan@outlook.com>
2022-10-23 20:30:33 -0600

    Search

--- UdacityCpp/A_star/complete.cpp
+++ UdacityCpp/A_star/complete.cpp
@@ -90,3 +90,43 @@ void ExpandNeighbors(const vector<int> &current, int goal[2],
     }
   }
 }
+
+vector<vector<State>> Search(vector<vector<State>> grid, int init[2],
+                             int goal[2]) {
+  // Create the vector of open nodes.
+  vector<vector<int>> open{};
+
+  // Initialize the starting node.
+  int x = init[0];
+  int y = init[1];
+  int g = 0;
+  int h = Heuristic(x, y, goal[0], goal[1]);
+  AddToOpen(x, y, g, h, open, grid);
+
+  while (open.size() > 0) {
+    // Get the next node
+    CellSort(&open);
+    auto current = open.back();
+    open.pop_back();
+    x = current[0];
+    y = current[1];
+    grid[x][y] = State::kPath;
+
+    // Check if we're done.
+    if (x == goal[0] && y == goal[1]) {
+      // Set the init grid cell to kStart, and
+      // set the goal grid cell to kFinish before returning the grid.
+      grid[init[0]][init[1]] = State::kStart;
+      grid[goal[0]][goal[1]] = State::kFinish;
+      return grid;
+    }
+
+    // If we're not done, expand search to current node's neighbors.
+    ExpandNeighbors(current, goal, open, grid);
+  }
+
+  // We've run out of new nodes to explore and haven't found a path.
+  cout << "No path found!"
+       << "\n";
+  return std::vector<vector<State>>{};
+}
