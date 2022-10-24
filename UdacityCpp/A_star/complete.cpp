a42e3a521ba86744bcdc658393e7c92d25590491
mahsanghani <ghaniahsan@outlook.com>
2022-10-23 20:31:04 -0600

    main

--- UdacityCpp/A_star/complete.cpp
+++ UdacityCpp/A_star/complete.cpp
@@ -154,3 +154,18 @@ void PrintBoard(const vector<vector<State>> board) {
     cout << "\n";
   }
 }
+
+int main() {
+  int init[2]{0, 0};
+  int goal[2]{4, 5};
+  auto board = ReadBoardFile("1.board");
+  auto solution = Search(board, init, goal);
+  PrintBoard(solution);
+  // Tests
+  TestHeuristic();
+  TestAddToOpen();
+  TestCompare();
+  TestSearch();
+  TestCheckValidCell();
+  TestExpandNeighbors();
+}
