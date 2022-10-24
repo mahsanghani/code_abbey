d0d5e678f049a357b32238eaba3787e243abe309
mahsanghani <ghaniahsan@outlook.com>
2022-10-23 20:41:17 -0600

    ParseLine

--- UdacityCpp/A_star/expand.cpp
+++ UdacityCpp/A_star/expand.cpp
@@ -12,3 +12,18 @@ using namespace std;
 enum class State { kEmpty, kObstacle, kClosed, kPath };
 
 const int delta[4][2]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+vector<State> ParseLine(string line) {
+  istringstream sline(line);
+  int n;
+  char c;
+  vector<State> row;
+  while (sline >> n >> c && c == ',') {
+    if (n == 0) {
+      row.push_back(State::kEmpty);
+    } else {
+      row.push_back(State::kObstacle);
+    }
+  }
+  return row;
+}
