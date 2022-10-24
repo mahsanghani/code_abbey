a900d84afc35ed4496ef999ea54c07b6ea149043
mahsanghani <ghaniahsan@outlook.com>
2022-10-23 20:34:57 -0600

    Search

--- UdacityCpp/A_star/heuristic.cpp
+++ UdacityCpp/A_star/heuristic.cpp
@@ -38,3 +38,14 @@ vector<vector<State>> ReadBoardFile(string path) {
   }
   return board;
 }
+
+int Heuristic(int x1, int y1, int x2, int y2) {
+  return abs(x2 - x1) + abs(y2 - y1);
+}
+
+vector<vector<State>> Search(vector<vector<State>> grid, int init[2],
+                             int goal[2]) {
+  cout << "No path found!"
+       << "\n";
+  return std::vector<vector<State>>{};
+}
