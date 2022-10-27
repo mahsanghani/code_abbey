3e79360776d0ad9a61e5cddc5b0308d76e685bb5
mahsanghani <ghaniahsan@outlook.com>
2022-10-26 19:35:19 -0600

    tester

--- UdacityCpp/Smart_Pointer/LeakTester.h
+++ UdacityCpp/Smart_Pointer/LeakTester.h
@@ -16,3 +16,19 @@
 
 #define FILE_OUTPUT(name) __Tester__::redirect_output(#name)
 #define SCREEN_OUTPUT __Tester__::redirect_output(0)
+
+namespace __Tester__ {
+typedef unsigned long ULong;
+struct Info {
+  void *address;
+  long line;
+  std::size_t _size;
+  bool isArray;
+  Info *link;
+} * alloc_map(0);
+int alloc_count(0), dealloc_count(0);
+long alloc_total(0), dealloc_total(0), alloc_current(0), alloc_max(0);
+bool notifications(false);
+char previous_name[1000] = "";
+FILE *output(stdout);
+} // namespace __Tester__
