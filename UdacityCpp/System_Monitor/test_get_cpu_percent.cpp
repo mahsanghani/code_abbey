81785902b3568cc9668c9ba429592cfad4ac16fa
mahsanghani <ghaniahsan@outlook.com>
2022-10-29 10:42:49 -0600

    test get cpu percent

---
+++ UdacityCpp/System_Monitor/test_get_cpu_percent.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Ahsan Ghani on 2022-10-29.
+//
+#include "ProcessParser.h"
+
+int main(int argc, char **argv) {
+  std::cout << ProcessParser::getCpuPercent("1651") << std::endl;
+  return 0;
+}
