ac7966f7b0d04238f52e9782218fc3b0169f63da
mahsanghani <ghaniahsan@outlook.com>
2022-10-30 11:31:01 -0600

    Vehicle

---
+++ UdacityCpp/Concurrent_Traffic/src/TrafficLight.h
@@ -0,0 +1,15 @@
+//
+// Created by Ahsan Ghani on 2022-10-30.
+//
+
+#ifndef TRAFFICLIGHT_H
+#define TRAFFICLIGHT_H
+
+#include "TrafficObject.h"
+#include <condition_variable>
+#include <deque>
+#include <mutex>
+
+using namespace std;
+
+class Vehicle;
