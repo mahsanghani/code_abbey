919f5a24c709868134d46f29a5ce5c4ce8a866ce
mahsanghani <ghaniahsan@outlook.com>
2022-10-30 11:42:48 -0600

    MessageQueue

--- UdacityCpp/Concurrent_Traffic/src/TrafficLight.cpp
+++ UdacityCpp/Concurrent_Traffic/src/TrafficLight.cpp
@@ -10,3 +10,12 @@
 #include "TrafficLight.h"
 
 using namespace std;
+
+template <typename T> T MessageQueue<T>::receive() {
+  unique_lock<mutex> u_lock(mutex_);
+  cond_.wait(u_lock, [this] { return !queue.empty(); });
+
+  T msg = move(queue_.back());
+  queue_.pop_back();
+  return msg;
+}
