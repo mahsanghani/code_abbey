a96f25135780ff14c0fdd206c788c728b27d2efa
mahsanghani <ghaniahsan@outlook.com>
2022-10-31 19:51:18 -0600

    TrafficObject

--- UdacityCpp/Concurrent_Traffic/src/TrafficObject.h
+++ UdacityCpp/Concurrent_Traffic/src/TrafficObject.h
@@ -16,3 +16,28 @@ enum ObjectType {
   objectIntersection,
   objectStreet,
 };
+
+class TrafficObject {
+public:
+  TrafficObject();
+  ~TrafficObject();
+
+  int getID() { return id_; }
+  void setPosition(double x, double y);
+  void getPosition(double &x, double &y);
+  ObjectType getType() { return type_; }
+
+  virtual void simulate(){};
+
+protected:
+  ObjectType type_;
+  int id_;
+  double posX_, posY_;
+  std::vector<std::thread> threads_;
+  static std::mutex mtx_;
+
+private:
+  static int id_cnt_;
+};
+
+#endif
