d5b0b626e623e8441a9cfae306d3b0381c004c93
mahsanghani <ghaniahsan@outlook.com>
2022-10-31 19:59:16 -0600

    entered intersection

--- UdacityCpp/Concurrent_Traffic/src/Vehicle.cpp
+++ UdacityCpp/Concurrent_Traffic/src/Vehicle.cpp
@@ -44,6 +44,39 @@ void Vehicle::drive() {
     if (timeSinceLastUpdate >= cycleDuration) {
       _posStreet += _speed * timeSinceLastUpdate / 1000;
       double completion = _posStreet / _currStreet->getLength();
+
+      // compute current pixel position on street based on driving direction
+      std::shared_ptr<Intersection> i1, i2;
+      i2 = _currDestination;
+      i1 = i2->getID() == _currStreet->getInIntersection()->getID()
+               ? _currStreet->getOutIntersection()
+               : _currStreet->getInIntersection();
+
+      double x1, y1, x2, y2, xv, yv, dx, dy, l;
+      i1->getPosition(x1, y1);
+      i2->getPosition(x2, y2);
+      dx = x2 - x1;
+      dy = y2 - y1;
+      l = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (x1 - x2));
+      xv = x1 + completion *
+                    dx; // new position based on line equation in parameter form
+      yv = y1 + completion * dy;
+      this->setPosition(xv, yv);
+
+      /* Task L2.1 */
+      // check wether halting position in front of destination has been reached
+      if (completion >= 0.9 && !hasEnteredIntersection) {
+        // request entry to the current intersection (using async)
+        auto ftrEntryGranted = std::async(&Intersection::addVehicleToQueue,
+                                          _currDestination, get_shared_this());
+
+        // wait until entry has been granted
+        ftrEntryGranted.get();
+
+        // slow down and set intersection flag
+        _speed /= 10.0;
+        hasEnteredIntersection = true;
+      }
     }
   }
 }
