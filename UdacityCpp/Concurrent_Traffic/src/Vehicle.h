2e4689c50812482a11b002904485b96474ce8e45
mahsanghani <ghaniahsan@outlook.com>
2022-10-31 19:55:08 -0600

    private

--- UdacityCpp/Concurrent_Traffic/src/Vehicle.h
+++ UdacityCpp/Concurrent_Traffic/src/Vehicle.h
@@ -14,19 +14,23 @@ class Intersection;
 class Vehicle : public TrafficObject,
                 public std::enable_shared_from_this<Vehicle> {
 public:
-  // constructor / desctructor
   Vehicle();
 
-  // getters / setters
   void setCurrentStreet(std::shared_ptr<Street> street) {
     _currStreet = street;
   };
   void setCurrentDestination(std::shared_ptr<Intersection> destination);
 
-  // typical behaviour methods
   void simulate();
 
-  // miscellaneous
   std::shared_ptr<Vehicle> get_shared_this() { return shared_from_this(); }
+
+private:
+  void drive();
+
+  std::shared_ptr<Street> _currStreet;
+  std::shared_ptr<Intersection> _currDestination;
+  double _posStreet;
+  double _speed;
 };
 #endif
