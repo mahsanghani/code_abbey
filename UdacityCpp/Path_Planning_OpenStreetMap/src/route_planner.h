f4a2120c71adc46e5744fce22ffbace4f4b10fd0
mahsanghani <ghaniahsan@outlook.com>
2022-11-03 20:45:47 -0600

    private

--- UdacityCpp/Path_Planning_OpenStreetMap/src/route_planner.h
+++ UdacityCpp/Path_Planning_OpenStreetMap/src/route_planner.h
@@ -16,4 +16,18 @@ public:
                float end_y);
   float GetDistance() { return distance; }
   void AStarSearch();
+
+private:
+  RouteModel &m_Model;
+  RouteModel::Node *start_node;
+  RouteModel::Node *end_node;
+  float distance = 0.0f;
+  vector<RouteModel::Node *> open_list;
+
+  vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *);
+  float CalculateHValue(const RouteModel::Node *node);
+  RouteModel::Node *NextNode();
+  static bool compareFValue(const RouteModel::Node *node_a,
+                            const RouteModel::Node *node_b);
+  void AddNeighbors(RouteModel::Node *);
 };
