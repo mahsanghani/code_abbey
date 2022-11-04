06e8c47618a3d0deceb1f9398869e010cc340858
mahsanghani <ghaniahsan@outlook.com>
2022-11-03 21:15:46 -0600

    private

--- UdacityCpp/Path_Planning_OpenStreetMap/src/render.h
+++ UdacityCpp/Path_Planning_OpenStreetMap/src/render.h
@@ -13,4 +13,21 @@ class Render {
 public:
   Render(RouteModel &model);
   void Display(io2d::output_surface &surface);
+
+private:
+  void BuildRoadReps();
+  void BuildLanduseBrushes();
+
+  void DrawBuildings(io2d::output_surface &surface) const;
+  void DrawHighways(io2d::output_surface &surface) const;
+  void DrawRailways(io2d::output_surface &surface) const;
+  void DrawLeisure(io2d::output_surface &surface) const;
+  void DrawWater(io2d::output_surface &surface) const;
+  void DrawLanduses(io2d::output_surface &surface) const;
+  void DrawStartPosition(io2d::output_surface &surface) const;
+  void DrawEndPosition(io2d::output_surface &surface) const;
+  void DrawPath(io2d::output_surface &surface) const;
+  io2d::interpreted_path PathFromWay(const Model::Way &way) const;
+  io2d::interpreted_path PathFromMP(const Model::Multipolygon &mp) const;
+  io2d::interpreted_path PathLine() const;
 };
