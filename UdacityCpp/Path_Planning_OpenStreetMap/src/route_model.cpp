9a86a7b6265dea8cddcab7c0c0ded405b9429e96
mahsanghani <ghaniahsan@outlook.com>
2022-11-04 11:38:55 -0600

    RouteModel

--- UdacityCpp/Path_Planning_OpenStreetMap/src/route_model.cpp
+++ UdacityCpp/Path_Planning_OpenStreetMap/src/route_model.cpp
@@ -1,3 +1,16 @@
 //
 // Created by Ahsan Ghani on 2022-11-03.
 //
+#include "route_model.h"
+#include <iostream>
+
+using namespace std;
+
+RouteModel::RouteModel(const vector<byte> &xml) : Model(xml) {
+  int counter = 0;
+  for (auto node : this->Nodes()) {
+    m_Nodes.push_back(Node(counter, this, node));
+    counter++;
+  }
+  CreateNodeToRoadHashmap();
+}
