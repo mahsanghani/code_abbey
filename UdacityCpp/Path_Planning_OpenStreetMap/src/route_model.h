ac3350876fd18819dd42dcefef341c36c24752f6
mahsanghani <ghaniahsan@outlook.com>
2022-11-03 20:57:27 -0600

    private

--- UdacityCpp/Path_Planning_OpenStreetMap/src/route_model.h
+++ UdacityCpp/Path_Planning_OpenStreetMap/src/route_model.h
@@ -1,8 +1,25 @@
 //
 // Created by Ahsan Ghani on 2022-11-03.
 //
+#pragma once
 
-#ifndef CODE_ABBEY_ROUTE_MODEL_H
-#define CODE_ABBEY_ROUTE_MODEL_H
+#include "model.h"
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <unordered_map>
 
-#endif // CODE_ABBEY_ROUTE_MODEL_H
+using namespace std;
+
+class RouteModel : public Model {
+public:
+  class Node : public Model::Node {
+  public:
+  };
+
+private:
+  vector<Node> m_Nodes;
+  unordered_map<int, vector<const Model::Road *>> node_to_road;
+
+  void CreateNodeToRoadHashmap();
+}
