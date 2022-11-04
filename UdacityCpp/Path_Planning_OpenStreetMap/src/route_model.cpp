74f7b7dbca4e675db91b30463bdfe761fbe23242
mahsanghani <ghaniahsan@outlook.com>
2022-11-04 11:42:36 -0600

    CreateNodeToRoadHashmap

--- UdacityCpp/Path_Planning_OpenStreetMap/src/route_model.cpp
+++ UdacityCpp/Path_Planning_OpenStreetMap/src/route_model.cpp
@@ -19,3 +19,16 @@ float RouteModel::Node::distance(RouteModel::Node anotherNode) const {
   return sqrt(pow(this->x - anotherNode.x, 2) +
               pow(this->y - anotherNode.y, 2));
 }
+
+void RouteModel::CreateNodeToRoadHashmap() {
+  for (const Model::Road &road : Roads()) {
+    if (road.type != Model::Road::Type::Footway) {
+      for (auto node_idx : Ways()[road.way].nodes) {
+        if (node_to_road.find(node_idx) == node_to_road.end()) {
+          node_to_road[node_idx] = vector<const Model::Road *>{};
+        }
+        node_to_road[node_idx].push_back(&road);
+      }
+    }
+  }
+}
