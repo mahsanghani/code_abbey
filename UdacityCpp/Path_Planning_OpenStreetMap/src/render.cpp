173ba76734239f8f55ebd5d6ddd36d7aecc1f204
mahsanghani <ghaniahsan@outlook.com>
2022-11-04 21:54:15 -0600

    static

--- UdacityCpp/Path_Planning_OpenStreetMap/src/render.cpp
+++ UdacityCpp/Path_Planning_OpenStreetMap/src/render.cpp
@@ -1,3 +1,12 @@
 //
 // Created by Ahsan Ghani on 2022-11-03.
 //
+#include "render.h"
+#include <iostream>
+
+using namespace std;
+
+static float RoadMetricWidth(Model::Road::Type type);
+static io2d::rgba_color RoadColor(Model::Road::Type type);
+static io2d::dashes RoadDashes(Model::Road::Type type);
+static io2d::point_2d ToPoint2D(const Model::Node &node) noexcept;
