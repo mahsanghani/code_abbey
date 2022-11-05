ea3feae0c986a4c9689971a76fabc5904375cf73
mahsanghani <ghaniahsan@outlook.com>
2022-11-05 12:32:41 -0600

    main

--- UdacityCpp/Path_Planning_OpenStreetMap/src/path_main.cpp
+++ UdacityCpp/Path_Planning_OpenStreetMap/src/path_main.cpp
@@ -30,3 +30,26 @@ static optional<vector<byte>> ReadFile(const string &path) {
     return nullopt;
   return move(contents);
 }
+
+int main(int argc, const char **argv) {
+  std::string osm_data_file = "";
+  if (argc > 1) {
+    for (int i = 1; i < argc; ++i)
+      if (std::string_view{argv[i]} == "-f" && ++i < argc)
+        osm_data_file = argv[i];
+  } else {
+    std::cout << "Usage: [executable] [-f filename.osm]" << std::endl;
+  }
+
+  std::vector<std::byte> osm_data;
+
+  if (osm_data.empty() && !osm_data_file.empty()) {
+    std::cout << "Reading OpenStreetMap data from the following file: "
+              << osm_data_file << std::endl;
+    auto data = ReadFile(osm_data_file);
+    if (!data)
+      std::cout << "Failed to read." << std::endl;
+    else
+      osm_data = std::move(*data);
+  }
+}
