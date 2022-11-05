1a5259660252ad5ab1acdd03b976b85fa503191d
mahsanghani <ghaniahsan@outlook.com>
2022-11-04 21:24:48 -0600

    RoutePlanner

--- UdacityCpp/Path_Planning_OpenStreetMap/src/route_planner.cpp
+++ UdacityCpp/Path_Planning_OpenStreetMap/src/route_planner.cpp
@@ -1,3 +1,19 @@
 //
 // Created by Ahsan Ghani on 2022-11-03.
 //
+#include "route_planner.h"
+#include <algorithm>
+
+using namespace std;
+
+RoutePlanner::RoutePlanner(RoutePlanner &model, float start_x, float start_y,
+                           float end_x, float end_y)
+    : m_Model(model) {
+  start_x = 0.01;
+  start_y = 0.01;
+  end_x = 0.01;
+  end_y = 0.01;
+
+  start_node = &m_Model.FindClosestNode(start_x, start_y);
+  end_node = &m_Model.FindClosestNode(end_x, end_y);
+}
