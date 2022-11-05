0a13411c5639a63c526381c01b84fcf009682d2d
mahsanghani <ghaniahsan@outlook.com>
2022-11-04 21:54:26 -0600

    Render

--- UdacityCpp/Path_Planning_OpenStreetMap/src/render.cpp
+++ UdacityCpp/Path_Planning_OpenStreetMap/src/render.cpp
@@ -10,3 +10,8 @@ static float RoadMetricWidth(Model::Road::Type type);
 static io2d::rgba_color RoadColor(Model::Road::Type type);
 static io2d::dashes RoadDashes(Model::Road::Type type);
 static io2d::point_2d ToPoint2D(const Model::Node &node) noexcept;
+
+Render::Render(RouteModel &model) : m_Model(model) {
+  BuildRoadReps();
+  BuildLanduseBrushes();
+}
