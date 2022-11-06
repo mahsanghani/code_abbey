48d2ecf0856474e2510de72de1699a42ac7e9940
mahsanghani <ghaniahsan@outlook.com>
2022-11-06 13:58:47 -0700

    ReadFile

--- UdacityCpp/Path_Planning_OpenStreetMap/test/utest_rm_find_closest_node.cpp
+++ UdacityCpp/Path_Planning_OpenStreetMap/test/utest_rm_find_closest_node.cpp
@@ -1,3 +1,30 @@
 //
 // Created by Ahsan Ghani on 2022-11-03.
 //
+#include "../src/route_model.h"
+#include "../src/route_planner.h"
+#include "gtest/gtest.h"
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <vector>
+
+using namespace std;
+
+static optional<vector<byte>> ReadFile(const string &path) {
+  ifstream is{path, ios::binary | ios::ate};
+  if (!is) {
+    return nullopt;
+  }
+
+  auto size = is.tellg();
+  vector<byte> contents(size);
+
+  is.seekg(0);
+  is.read((char *)contents.data(), size);
+
+  if (contents.empty()) {
+    return nullopt;
+  }
+  return move(contents);
+}
