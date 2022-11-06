fc9fead31f4aad64cc6b82192f19b4f38e6fc0b8
mahsanghani <ghaniahsan@outlook.com>
2022-11-06 14:19:38 -0700

    clean

--- UdacityCpp/Path_Planning_OpenStreetMap/test/utest_rm_find_closest_node.cpp
+++ UdacityCpp/Path_Planning_OpenStreetMap/test/utest_rm_find_closest_node.cpp
@@ -57,7 +57,7 @@ TEST_F(RouteModelTest, RouteModelNode) {
   EXPECT_FLOAT_EQ(test_node.x, 1.2646476);
   EXPECT_FLOAT_EQ(test_node.y, 0.23506972);
   EXPECT_EQ(test_node.parent, nullptr);
-  EXPECT_FLOAT_EQ(test_node.h_value, std::numeric_limits<float>::max());
+  EXPECT_FLOAT_EQ(test_node.h_value, numeric_limits<float>::max());
   EXPECT_FLOAT_EQ(test_node.g_value, 0.0);
   EXPECT_FLOAT_EQ(test_node.visited, false);
   EXPECT_EQ(test_node.neighbors.size(), 0);
