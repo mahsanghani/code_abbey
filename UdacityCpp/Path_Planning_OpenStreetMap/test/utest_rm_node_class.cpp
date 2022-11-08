a866af7f7badd234d21b14b0fa71d3b44821d6a7
mahsanghani <ghaniahsan@outlook.com>
2022-11-07 21:04:36 -0700

    RouteModelNode

--- UdacityCpp/Path_Planning_OpenStreetMap/test/utest_rm_node_class.cpp
+++ UdacityCpp/Path_Planning_OpenStreetMap/test/utest_rm_node_class.cpp
@@ -50,3 +50,16 @@ TEST_F(RouteModelTest, RouteModelData) {
   EXPECT_EQ(model.Nodes().size(), 10754);
   ASSERT_EQ(model.SNodes().size(), 0);
 }
+
+TEST_F(RouteModelTest, RouteModelNode) {
+  RouteModel *p_model = &model;
+  RouteModel::Node test_node = RouteModel::Node(1, p_model, model.Nodes()[1]);
+  EXPECT_FLOAT_EQ(test_node.x, 1.2646476);
+  EXPECT_FLOAT_EQ(test_node.y, 0.23506972);
+  EXPECT_EQ(test_node.parent, nullptr);
+  EXPECT_FLOAT_EQ(test_node.h_value, std::numeric_limits<float>::max());
+  EXPECT_FLOAT_EQ(test_node.g_value, 0.0);
+  EXPECT_FLOAT_EQ(test_node.visited, false);
+  EXPECT_EQ(test_node.neighbors.size(), 0);
+  RouteModel::Node test_node_2 = RouteModel::Node();
+}
