b10bcce32976bbba181c6241fa8a19659a228616
mahsanghani <ghaniahsan@outlook.com>
2022-11-07 21:10:37 -0700

    ReadFile

--- UdacityCpp/Path_Planning_OpenStreetMap/test/utest_route_model_class.cpp
+++ UdacityCpp/Path_Planning_OpenStreetMap/test/utest_route_model_class.cpp
@@ -1,3 +1,28 @@
 //
 // Created by Ahsan Ghani on 2022-11-03.
 //
+#include "../src/route_model.h"
+#include "../src/route_planner.h"
+#include "gtest/gtest.h"
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <vector>
+
+using namespace std;
+
+static std::optional<std::vector<std::byte>> ReadFile(const std::string &path) {
+  std::ifstream is{path, std::ios::binary | std::ios::ate};
+  if (!is)
+    return std::nullopt;
+
+  auto size = is.tellg();
+  std::vector<std::byte> contents(size);
+
+  is.seekg(0);
+  is.read((char *)contents.data(), size);
+
+  if (contents.empty())
+    return std::nullopt;
+  return std::move(contents);
+}
