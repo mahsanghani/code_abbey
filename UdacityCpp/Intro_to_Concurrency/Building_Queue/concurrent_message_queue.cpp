1cbd84200c4bd70d2a6e6f5c261e09f6da6b3c6e
mahsanghani <ghaniahsan@outlook.com>
2022-11-13 17:53:31 -0800

    concurrent

---
+++ UdacityCpp/Intro_to_Concurrency/Building_Queue/concurrent_message_queue.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Ahsan Ghani on 2022-11-13.
+//
+#include <algorithm>
+#include <future>
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <thread>
+
+using namespace std;
+
+template <class T> class MessageQueue {};
