b1f51c392acdadca4bd0c225ded2e1ed2e9da768
mahsanghani <ghaniahsan@outlook.com>
2022-11-14 10:07:25 -0800

    Vehicle

---
+++ UdacityCpp/Intro_to_Concurrency/Monitor_Object_Pattern/waiting_vehicle_counter.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Ahsan Ghani on 2022-11-13.
+//
+#include <algorithm>
+#include <future>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+class Vehicle {
+public:
+  Vehicle(int id) : id_(id) {}
+  int getID() { return id_; }
+
+private:
+  int id_;
+};
