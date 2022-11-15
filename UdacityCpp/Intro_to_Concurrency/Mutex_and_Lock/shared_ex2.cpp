0a78f58e59b382a2f88825592e2133e9761c15a0
mahsanghani <ghaniahsan@outlook.com>
2022-11-15 10:30:38 -0800

    Vehicle

---
+++ UdacityCpp/Intro_to_Concurrency/Mutex_and_Lock/shared_ex2.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Ahsan Ghani on 2022-11-15.
+//
+#include <algorithm>
+#include <future>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+class Vehicle {
+public:
+  Vehicle(int id) : id_(id) {}
+
+private:
+  int id_;
+};
