346feb411122a366e802ab9f85e28b6d713b1914
mahsanghani <ghaniahsan@outlook.com>
2022-11-15 11:38:32 -0800

    divideByNumber

--- UdacityCpp/Intro_to_Concurrency/Mutex_and_Lock/ex1.cpp
+++ UdacityCpp/Intro_to_Concurrency/Mutex_and_Lock/ex1.cpp
@@ -15,3 +15,18 @@ void printResult(int denom) {
   std::cout << "for denom = " << denom << ", the result is " << result
             << std::endl;
 }
+
+void divideByNumber(double num, double denom) {
+  try {
+    if (denom != 0) {
+      result = num / denom;
+      this_thread::sleep_for(chrono::milliseconds(1));
+      printResult(denom);
+    } else {
+      throw invalid_argument("Exception from thread: Division by zero!");
+    }
+  } catch (const invalid_argument &e) {
+    cout << e.what() << endl;
+    return;
+  }
+}
