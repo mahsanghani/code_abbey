b91fd1483d62b4c14ff9c3e53f5def0beea2cd06
mahsanghani <ghaniahsan@outlook.com>
2022-11-15 18:19:03 -0800

    ThreadA

--- UdacityCpp/Intro_to_Concurrency/Mutex_and_Lock/ex4.cpp
+++ UdacityCpp/Intro_to_Concurrency/Mutex_and_Lock/ex4.cpp
@@ -10,3 +10,11 @@ using namespace std;
 mutex mutex_1;
 mutex mutex_2;
 mutex mutex_3;
+
+void ThreadA() {
+  mutex_1.lock();
+  cout << "Thread A" << endl;
+  mutex_2.lock();
+  mutex_2.unlock();
+  mutex_1.unlock();
+}
