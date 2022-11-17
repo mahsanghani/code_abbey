8f13b0976ca9a53379d7c4763241c5e44becaa1e
mahsanghani <ghaniahsan@outlook.com>
2022-11-17 10:44:33 -0800

    fork

--- UdacityCpp/Intro_to_Concurrency/Running_Multiple_Threads/fork.cpp
+++ UdacityCpp/Intro_to_Concurrency/Running_Multiple_Threads/fork.cpp
@@ -10,3 +10,19 @@ using namespace std;
 void printHello() {
   cout << "Hello from the Worker thread #" << this_thread::get_id();
 }
+
+int main() {
+  vector<thread> threads;
+
+  for (size_t i = 0; i < 5; i++) {
+    threads.emplace_back(thread(printHello));
+  }
+
+  cout << "Hello from Main thread #" << this_thread::get_id() << endl;
+
+  for (auto &t : threads) {
+    t.join();
+  }
+
+  return 0;
+}
