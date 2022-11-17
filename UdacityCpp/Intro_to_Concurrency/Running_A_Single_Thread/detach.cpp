be2a41d0c0b89b64bf0e141c893479ae20e03255
mahsanghani <ghaniahsan@outlook.com>
2022-11-16 19:27:09 -0800

    main

--- UdacityCpp/Intro_to_Concurrency/Running_A_Single_Thread/detach.cpp
+++ UdacityCpp/Intro_to_Concurrency/Running_A_Single_Thread/detach.cpp
@@ -10,3 +10,11 @@ void threadFunction() {
   this_thread::sleep_for(chrono::microseconds(50)); // simulate work
   cout << "Finish work in thread\n";
 }
+
+int main() {
+  thread t(threadFunction);
+  t.detach();
+  this_thread::sleep_for(chrono::milliseconds(50));
+  cout << "Finished work in main!" << endl;
+  return 0;
+}
