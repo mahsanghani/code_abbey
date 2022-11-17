e292a3a96222bfca3b3dd524a8cce4ecfa17b23b
mahsanghani <ghaniahsan@outlook.com>
2022-11-16 19:26:12 -0800

    threadFunction

---
+++ UdacityCpp/Intro_to_Concurrency/Running_A_Single_Thread/detach.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Ahsan Ghani on 2022-11-16.
+//
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void threadFunction() {
+  this_thread::sleep_for(chrono::microseconds(50)); // simulate work
+  cout << "Finish work in thread\n";
+}
