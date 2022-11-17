e2ac144662e487e175e847a3e113d35aec1a53b0
mahsanghani <ghaniahsan@outlook.com>
2022-11-16 19:21:40 -0800

    main

--- UdacityCpp/Intro_to_Concurrency/Promises_and_Futures/ex1.cpp
+++ UdacityCpp/Intro_to_Concurrency/Promises_and_Futures/ex1.cpp
@@ -10,3 +10,17 @@ void printMessage(string message) {
   this_thread::sleep_for(chrono::milliseconds(10)); // simulate work
   cout << "Thread 1: " << message << endl;
 }
+
+int main() {
+  string message = "My message";
+  thread t1(printMessage, message);
+
+  thread t2([message] {
+    this_thread::sleep_for(chrono::milliseconds(10));
+    cout << "Thread 2: " << message << endl;
+  });
+
+  t1.join();
+  t2.join();
+  return 0;
+}
