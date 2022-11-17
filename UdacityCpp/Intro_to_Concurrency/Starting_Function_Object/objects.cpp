30283a80a0b9f0a8da32acafc1bb606d9fbda110
mahsanghani <ghaniahsan@outlook.com>
2022-11-17 10:49:17 -0800

    main

--- UdacityCpp/Intro_to_Concurrency/Starting_Function_Object/objects.cpp
+++ UdacityCpp/Intro_to_Concurrency/Starting_Function_Object/objects.cpp
@@ -10,3 +10,17 @@ class Vehicle {
 public:
   void operator()() { cout << "Vehicle object has been created. \n" << endl; }
 };
+
+int main() {
+  thread t1((Vehicle()));
+  thread t2 = thread(Vehicle());
+  thread t3 { Vehicle(); };
+
+  cout << "Finished work in main function!" << endl;
+
+  t1.join();
+  t2.join();
+  t3.join();
+
+  return 0;
+}
