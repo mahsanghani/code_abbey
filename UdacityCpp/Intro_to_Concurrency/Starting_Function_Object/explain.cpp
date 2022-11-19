9778eddd544f63d294249860966f491cb3c4e090
mahsanghani <ghaniahsan@outlook.com>
2022-11-18 19:25:43 -0800

    explain

--- UdacityCpp/Intro_to_Concurrency/Starting_Function_Object/explain.cpp
+++ UdacityCpp/Intro_to_Concurrency/Starting_Function_Object/explain.cpp
@@ -13,4 +13,17 @@ int main() {
     cout << "ID in Thread (call-by-reference) " << id << endl;
   };
   thread t1(f0);
+
+  thread t2([id]() mutable {
+    this_thread::sleep_for(chrono::milliseconds(50));
+    cout << "ID in thread (call-by-value) = " << id << endl;
+  });
+
+  ++id;
+  cout << "ID in Main (call-by-value) = " << id << endl;
+
+  t1.join();
+  t2.join();
+
+  return 0;
 }
