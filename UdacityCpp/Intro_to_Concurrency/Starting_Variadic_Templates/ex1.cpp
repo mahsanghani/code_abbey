9507b411941227c34c26d26ca9f0de8b1190e2f4
mahsanghani <ghaniahsan@outlook.com>
2022-11-18 19:43:57 -0800

    printIDAndName

--- UdacityCpp/Intro_to_Concurrency/Starting_Variadic_Templates/ex1.cpp
+++ UdacityCpp/Intro_to_Concurrency/Starting_Variadic_Templates/ex1.cpp
@@ -11,3 +11,8 @@ void printID(int id) {
   this_thread::sleep_for(chrono::milliseconds(50));
   cout << "ID = " << id << endl;
 }
+
+void printIDAndName(int id, string name) {
+  this_thread::sleep_for(chrono::milliseconds(100));
+  cout << "ID = " << id << ", name = " << name << endl;
+}
