ad78a43d18b9202730dec7658d4d911da961e22c
mahsanghani <ghaniahsan@outlook.com>
2022-11-18 20:50:32 -0800

    main

--- UdacityCpp/Intro_to_Concurrency/Starting_Variadic_Templates/ex4.cpp
+++ UdacityCpp/Intro_to_Concurrency/Starting_Variadic_Templates/ex4.cpp
@@ -15,3 +15,18 @@ public:
 private:
   int id_;
 };
+
+int main() {
+  Vehicle v1, v2;
+  thread t1 = thread(&Vehicle::addID, v1,
+                     1); // pass by value, doesn't affect original thread
+  thread t2 = thread(
+      &Vehicle::addID, &v2,
+      2); // pass by reference, change will also be visible in the main thread
+
+  t1.join();
+  t2.join();
+  v1.printID();
+  v2.printID();
+  return 0;
+}
