c7828317e7c42b6b70bec767d5ef340c0452d326
mahsanghani <ghaniahsan@outlook.com>
2022-11-19 18:44:15 -0800

    main

--- UdacityCpp/Intro_to_Concurrency/Threads_vs_Tasks/paralllel.cpp
+++ UdacityCpp/Intro_to_Concurrency/Threads_vs_Tasks/paralllel.cpp
@@ -18,3 +18,26 @@ void workerFunction(int n) {
   for (int i = 0; i < n; i++)
     sqrt(12345.6789);
 }
+
+int main() {
+  cout << "Main thread id = " << this_thread::get_id() << endl;
+  chrono::high_resolution_clock::time_point t1 =
+      chrono::high_resolution_clock::now();
+  vector<future<void>> futures;
+  int n_loops = 10 int n_threads = 5;
+
+  for (int i = 0; i < n_threads; i++) {
+    futures.emplace_back(async(launch::async, workerFunction, n_loops));
+  }
+
+  for (const future<void> &ftr : futures) {
+    ftr.wait();
+  }
+
+  chrono::high_resolution_clock::time_point t2 =
+      chrono::high_resolution_clock::now();
+  auto duration = chrono::duration_cast<chrono::microseconds>(t2 - t1).count();
+  cout << "Execution finished after " << duration << " microseconds" << endl;
+
+  return 0;
+}
