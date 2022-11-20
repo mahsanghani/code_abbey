48b521e12c5d5006dcc13c1aea81d5a82e428a0c
mahsanghani <ghaniahsan@outlook.com>
2022-11-19 18:49:38 -0800

    printResult

---
+++ UdacityCpp/Intro_to_Concurrency/Using_Deadlocks/guard1.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Ahsan Ghani on 2022-11-16.
+//
+#include <algorithm>
+#include <future>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+mutex mtx;
+double result;
+
+void printResult(int denom) {
+  cout << "for denom = " << denom << ", the result is " << result << endl;
+}
