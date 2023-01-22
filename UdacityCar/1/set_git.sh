3bc4928fc13c9c17209d1c1c6432980e3d84858e
mghaniahsan2 <mghani1@ualberta.ca>
2023-01-22 09:32:26 -0500

    set git shell script

---
+++ UdacityCar/1/set_git.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo
+git pull
+echo
+
+read -p 'Git repository url: ' upstreamVar
+read -p 'Git Username: ' userVar
+read -p 'Git email: ' emailVar
+
+echo
+echo Thank you $userVar!, we now have your credentials
+echo for upstream $upstreamVar. You must supply your password for each push.
+echo
+
+echo setting up git
+
+git config --global user.name $userVar
+git config --global user.email $emailVar
+git remote set-url origin $upstreamVar
+echo
+
+echo Please verify remote:
+git remote -v
+echo
+
+echo Please verify your credentials:
+echo username: `git config user.name`
+echo email: `git config user.email`
