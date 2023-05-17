25ed639489584d50c87f2aa28efbeee690d31c36
mahsanghani <ghaniahsan@outlook.com>
2023-05-17 16:43:46 -0400

    concatenation of array

---
+++ leetcode/vsc/1929.concatenation-of-array.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=1929 lang=python3
+#
+# [1929] Concatenation of Array
+#
+
+# @lc code=start
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = nums
+        ans.extend(nums)
+        return ans
+# @lc code=end
+
