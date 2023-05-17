f30776037598216dac781cc7338e4807135edbca
mahsanghani <ghaniahsan@outlook.com>
2023-05-17 19:20:33 -0400

    group anagrams

--- leetcode/vsc/49.group-anagrams.py
+++ leetcode/vsc/49.group-anagrams.py
@@ -7,16 +7,9 @@
 # @lc code=start
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res=[]
-        # strs=sorted(strs)
         d=defaultdict(list)
-        
         for s in strs:
             d["".join(sorted(s))].append(s)
-        
-        for k,v in d.items():
-            res.append(v)
-        
         return sorted(d.values())
 # @lc code=end
 
