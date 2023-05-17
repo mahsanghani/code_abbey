fd235904f2570b16eb48c111b0e2e85b809eb761
mahsanghani <ghaniahsan@outlook.com>
2023-05-17 18:40:50 -0400

    length of last word

--- leetcode/vsc/58.length-of-last-word.py
+++ leetcode/vsc/58.length-of-last-word.py
@@ -8,8 +8,8 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         l=0
-        for i in range(len(s)-1,-1,-1):
-            if s[i]==" ":
+        for c in s[::-1]:
+            if c==" ":
                 if l>0:
                     return l
             else:
