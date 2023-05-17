8b0611af4670759d9f8b4ccbc963d091062a44ab
mahsanghani <ghaniahsan@outlook.com>
2023-05-17 19:43:25 -0400

    isomorphic strings

--- leetcode/vsc/205.isomorphic-strings.py
+++ leetcode/vsc/205.isomorphic-strings.py
@@ -7,28 +7,6 @@
 # @lc code=start
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        ds=Counter(s)
-        dt=Counter(t)
-
-        if ds==dt:
-            return True
-
-        ds2=defaultdict(list)
-        dt2=defaultdict(list)
-
-        for k,v in ds.items():
-            if v>0:
-                ds2[v].append(k)
-
-        for k,v in dt.items():
-            if v>0:
-                dt2[v].append(k)
-        
-        for k,v in dt2.items():
-            if len(v)!=len(ds2[k]):
-                return False
-        
-        return True
-
+        return len(set(s))==len(set(t))==len(set(zip(s,t)))
 # @lc code=end
 
