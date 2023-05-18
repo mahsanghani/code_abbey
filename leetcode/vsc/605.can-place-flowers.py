f71ff988d7fc45ee7d37ef0d58b81db75bff9e23
mahsanghani <ghaniahsan@outlook.com>
2023-05-17 23:05:42 -0400

    can place flowers

--- leetcode/vsc/605.can-place-flowers.py
+++ leetcode/vsc/605.can-place-flowers.py
@@ -7,11 +7,15 @@
 # @lc code=start
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        f = [0]+flowerbed+[0]
-        for i in range(1, len(f)-1):
-            if f[i-1]==0 and f[i]==0 and f[i+1]==0:
-                f[i]=1
-                n-=1
+        e=0 if flowerbed[0] else 1
+        for f in flowerbed:
+            if f:
+                n-=int((e-1)/2)
+                e=0
+            else:
+                e+=1
+        
+        n-=(e)//2
         return n<=0
         
 # @lc code=end
