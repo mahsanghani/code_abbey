7a3283c6c77ee2e6994f876916274c8be5394495
mahsanghani <ghaniahsan@outlook.com>
2023-05-17 22:00:20 -0400

    isomorphic strings

--- leetcode/vsc/205.isomorphic-strings.py
+++ leetcode/vsc/205.isomorphic-strings.py
@@ -7,22 +7,21 @@
 # @lc code=start
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        ST = {}
-        TS = {}
+        ST, TS = {}, {}
 
-        if len(s)!=len(t):
+        if len(s) != len(t):
             return False
-
+        
         for i in range(len(s)):
             c1 = s[i]
             c2 = t[i]
 
             if ((c1 in ST and ST[c1]!=c2) or (c2 in TS and TS[c2]!=c1)):
                 return False
-
+            
             ST[c1] = c2
             TS[c2] = c1
-
+        
         return True
 # @lc code=end
 
