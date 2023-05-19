da8c0d04bc5bb20c3f01a18a5dbd303f32afe62d
mahsanghani <ghaniahsan@outlook.com>
2023-05-18 23:27:15 -0400

    design hashmap

--- leetcode/vsc/706.design-hash-map.py
+++ leetcode/vsc/706.design-hash-map.py
@@ -20,10 +20,13 @@ class MyHashMap:
         return key % len(self.map)
 
     def put(self, key: int, value: int) -> None:
+        cur = self.map[self.hash(key)]
 
     def get(self, key: int) -> int:
+        cur = self.map[self.hash(key)]
 
     def remove(self, key: int) -> None:
+        cur = self.map[self.hash(key)]
         
 
 
