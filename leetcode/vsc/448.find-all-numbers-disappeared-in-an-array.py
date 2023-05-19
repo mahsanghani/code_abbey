a19d50f590a0a178dfe9fba154e78efe4daacee6
mahsanghani <ghaniahsan@outlook.com>
2023-05-18 22:40:33 -0400

    find all numbers disappeared in an array

--- leetcode/vsc/448.find-all-numbers-disappeared-in-an-array.py
+++ leetcode/vsc/448.find-all-numbers-disappeared-in-an-array.py
@@ -8,6 +8,6 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         check = set(range(1,len(nums)+1))
-        return list(check.difference(set(nums)))
+        return list(set(range(1,len(nums)+1)).difference(set(nums)))
 # @lc code=end
 
