efcfee41d8d09e4f7845584c5f3cba4bb03a3f36
mahsanghani <ghaniahsan@outlook.com>
2023-05-18 23:47:02 -0400

    sort an array

--- leetcode/vsc/912.sort-an-array.py
+++ leetcode/vsc/912.sort-an-array.py
@@ -5,9 +5,12 @@
 #
 
 # @lc code=start
+from queue import PriorityQueue as pq
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        nums.sort()
+        q = pq()
+        for i in nums: q.put(i)
+        for i in range(len(nums)): nums[i] = q.get()
         return nums
 # @lc code=end
 
