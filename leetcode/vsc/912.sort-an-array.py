7b57825ece41688b18ef746cf09d5e22ed0edb94
mahsanghani <ghaniahsan@outlook.com>
2023-05-18 23:45:39 -0400

    sort an array

--- leetcode/vsc/912.sort-an-array.py
+++ leetcode/vsc/912.sort-an-array.py
@@ -7,9 +7,7 @@
 # @lc code=start
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        ctr  = Counter(nums)
-        return list(chain(*([i]*ctr[i]
-                    for i in range(min(ctr),
-                    max(ctr)+1) if i in ctr)))  
+        nums.sort()
+        return nums
 # @lc code=end
 
