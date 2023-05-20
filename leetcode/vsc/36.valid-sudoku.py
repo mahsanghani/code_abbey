60d21ee016327ca72f5d5cd0983264adf0bac816
mahsanghani <ghaniahsan@outlook.com>
2023-05-20 10:06:09 -0400

    valid sudoku

---
+++ leetcode/vsc/36.valid-sudoku.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=36 lang=python3
+#
+# [36] Valid Sudoku
+#
+
+# @lc code=start
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+
+        for r in rows:
+            for c in cols:
+                if board[r][c]==".":
+                    continue
+                if (board[r][c] in rows[r] or
+                    board[r][c] in cols[c] or
+                    board[r][c] in squares[(r//3,c//3)]):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r//3,c//3)].add(board[r][c])
+        return True
+
+# @lc code=end
+
