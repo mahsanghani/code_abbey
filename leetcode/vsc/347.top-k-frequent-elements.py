d2e6a5e122f5e08784c3d3d5d9bd8f82e9229cf4
mahsanghani <ghaniahsan@outlook.com>
2023-05-19 22:57:03 -0400

    top k frequent elements

--- leetcode/vsc/347.top-k-frequent-elements.py
+++ leetcode/vsc/347.top-k-frequent-elements.py
@@ -7,7 +7,6 @@
 # @lc code=start
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        d=dict(sorted(Counter(nums).items(), key=lambda t:t[1], reverse=True))
-        return list(d.keys())[:k]
+        return list(dict(sorted(Counter(nums).items(), key=lambda t:t[1], reverse=True)).keys())[:k]
 # @lc code=end
 
