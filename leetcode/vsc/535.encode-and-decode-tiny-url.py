cafd0b545ef927b01badb1b53611d1bd32f4fa3c
mahsanghani <ghaniahsan@outlook.com>
2023-05-20 22:14:08 -0400

    decode

--- leetcode/vsc/535.encode-and-decode-tiny-url.py
+++ leetcode/vsc/535.encode-and-decode-tiny-url.py
@@ -19,6 +19,17 @@ class Codec:
     def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL.
         """
+        i=0
+        results=[]
+
+        while i<len(s):
+            j=i
+            while s[j]!="#":
+                j+=1
+            length=int(s[i:j])
+            results.append(s[j+1:j+1+length])
+            i=j+1+length
+        return results
         
 
 # Your Codec object will be instantiated and called as such:
