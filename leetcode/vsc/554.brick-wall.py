7527a004bdb6c31225ae0d951637e3273c9a1455
mahsanghani <ghaniahsan@outlook.com>
2023-05-24 17:50:28 -0400

    brick wall|

--- leetcode/vsc/554.brick-wall.py
+++ leetcode/vsc/554.brick-wall.py
@@ -8,14 +8,11 @@
 class Solution:
     def leastBricks(self, wall: List[List[int]]) -> int:
         gap={0:0}
-
         for r in wall:
             t=0
             for b in r[:-1]:
                 t+=b
                 gap[t]=1+gap.get(t,0)
-
         return len(wall) - max(gap.values())
-        
 # @lc code=end
 
