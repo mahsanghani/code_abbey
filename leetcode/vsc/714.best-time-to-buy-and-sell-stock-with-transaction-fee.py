2aed2ab027d03bd04aa008d1046f264be15c4307
mahsanghani <ghaniahsan@outlook.com>
2023-05-28 17:53:59 -0400

    max profit transaction fee

--- leetcode/vsc/714.best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ leetcode/vsc/714.best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -7,11 +7,10 @@
 # @lc code=start
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        length = len(prices)
-        hold, free = [0]*length, [0]*length
+        hold, free = [0]*len(prices), [0]*len(prices)
         hold[0] = -prices[0]
         
-        for i in range(1, length):
+        for i in range(1, len(prices)):
             hold[i] = max(hold[i-1], free[i-1] - prices[i])
             free[i] = max(free[i-1], hold[i-1] + prices[i] - fee)
 
