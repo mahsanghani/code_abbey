7f900607506f61dbf85f7bb553d61026a0047aae
mahsanghani <ghaniahsan@outlook.com>
2023-05-28 17:54:35 -0400

    max profit cooldown

--- leetcode/vsc/714.best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ leetcode/vsc/714.best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -7,7 +7,8 @@
 # @lc code=start
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        hold, free = [0]*len(prices), [0]*len(prices)
+        hold = [0]*len(prices)
+        free = [0]*len(prices)
         hold[0] = -prices[0]
         
         for i in range(1, len(prices)):
