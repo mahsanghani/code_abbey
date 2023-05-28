122d441efbf2f50208e8b7e3cd337f1082292c45
mahsanghani <ghaniahsan@outlook.com>
2023-05-28 17:23:40 -0400

    max profit cooldown

---
+++ leetcode/vsc/309.best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=309 lang=python3
+#
+# [309] Best Time to Buy and Sell Stock with Cooldown
+#
+
+# @lc code=start
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = {} # key=(i, buying)
+
+        def dfs(i, buying):
+            if i>=len(prices):
+                return 0
+            if (i, buying) in dp:
+                return dp[(i, buying)]
+        
+            if buying:
+                buy = dfs(i+1, not buying) - prices[i]
+                cool = dfs(i+1, buying)
+                dp[(i, buying)] = max(buy, cool) # cache
+            else:
+                sell = dfs(i+2, not buying) + prices[i]
+                cool = dfs(i+1, buying)
+                dp[(i, buying)] = max(sell, cool) # cache
+            return dp[(i, buying)]
+        
+        return dfs(0, True)
+# @lc code=end
