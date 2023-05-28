9af120ff48d50cd08c2f713f057efac65b243e57
mahsanghani <ghaniahsan@outlook.com>
2023-05-28 16:38:34 -0400

    max profit 3

--- leetcode/vsc/123.best-time-to-buy-and-sell-stock-iii.py
+++ leetcode/vsc/123.best-time-to-buy-and-sell-stock-iii.py
@@ -7,26 +7,25 @@
 # @lc code=start
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        length = len(prices)
         left = prices[0]
         right = prices[-1]
+        profit_left = [0] * (length)
+        profit_right = [0] * (length+1)
 
-        length = len(prices)
-        pl = [0]*length
-        pr = [0]*(length+1)
-
-        for l in range(1,length):
-            pl[l] = max(pl[l-1], prices[l]-left)
-            left = min(left,prices[l])
-
+        for l in range(1, length):
             r = length - l - 1
 
-            pr[r] = max(pr[r+1], right-prices[r])
+            profit_left[l] = max(profit_left[l-1], prices[l]-left)
+            left = min(left, prices[l])
+
+            profit_right[r] = max(profit_right[r+1], right-prices[r])
             right = max(right, prices[r])
 
         profit = 0
 
         for i in range(0, length):
-            profit = max(profit, pl[i]+pr[i+1])
-        
+            profit = max(profit, profit_left[i] + profit_right[i+1])
+
         return profit
 # @lc code=end
