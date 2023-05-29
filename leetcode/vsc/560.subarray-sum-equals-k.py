63edc30ec1664089cc47f32f788db783adf599fd
mahsanghani <ghaniahsan@outlook.com>
2023-05-29 16:12:03 -0400

    sub array sum

--- leetcode/vsc/560.subarray-sum-equals-k.py
+++ leetcode/vsc/560.subarray-sum-equals-k.py
@@ -7,14 +7,13 @@
 # @lc code=start
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        res,cur,pre=0,0,{0:1}
+        res, cur, pre = 0, 0, {0:1}
 
         for num in nums:
             cur += num
-            diff = cur-k
-
-            res += pre.get(diff,0)
-            pre[cur] = 1 + pre.get(cur,0)
+            diff = cur - k
+            res += pre.get(diff, 0)
+            pre[cur] = 1 + pre.get(cur, 0)
 
         return res
 # @lc code=end
