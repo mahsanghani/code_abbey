456c7e9a5d8ba2ae883efcf0f66c297d2ba2214e
mahsanghani <ghaniahsan@outlook.com>
2023-06-01 22:05:54 -0400

    grid game

--- leetcode/vsc/2017.grid-game.py
+++ leetcode/vsc/2017.grid-game.py
@@ -7,21 +7,14 @@
 # @lc code=start
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
-        n = len(grid[0])
-        pre1 = grid[0].copy()
-        pre2 = grid[1].copy()
-
-        for i in range(1, n):
-            pre1[i] += pre1[i-1]
-            pre2[i] += pre2[i-1]
-
+        left = 0
+        right = sum(grid[0])
         results = float("inf")
-        for i in range(n):
-            top = pre1[-1] - pre1[i]
-            bottom = pre2[i-1]
-            second = max(top, bottom)
-            results = min(results, second)
 
+        for x, y in zip(grid[0], grid[1]):
+            right -= x
+            results = min(results, max(left, right))
+            left += y
         return results
 # @lc code=end
 
