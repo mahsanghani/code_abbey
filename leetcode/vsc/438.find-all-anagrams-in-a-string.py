c75cc8198ed246446a692370598f9d54b64e85b2
mahsanghani <ghaniahsan@outlook.com>
2023-06-01 22:32:12 -0400

    find anagrams

--- leetcode/vsc/438.find-all-anagrams-in-a-string.py
+++ leetcode/vsc/438.find-all-anagrams-in-a-string.py
@@ -7,27 +7,25 @@
 # @lc code=start
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        if len(p)>len(s):
-            return []
-        
-        cp = {}
-        cs = {}
+        start = 0
+        results = []
+        cp, cs = {}, {}
 
-        for i in range(len(p)):
-            cp[p[i]] = 1 + cp.get(p[i], 0)
+        for c in p:
+            cp[c] = 1 + cp.get(c, 0)
+
+        for i in range(len(s)):
             cs[s[i]] = 1 + cs.get(s[i], 0)
-        
-        results = [0] if cs == cp else []
-        l = 0
-        for j in range(len(p), len(s)):
-            cs[s[j]] = 1 + cs.get(s[j], 0)
-            cs[s[l]] -= 1
 
-            if cs[s[l]] == 0:
-                cs.pop(s[l])
-            l += 1
-            if cs == cp:
-                results.append(l)
+            if i >= len(p) - 1:
+                if cs == cp:
+                    results.append(start)
+            
+                if s[start] in cs:
+                    cs[s[start]] -= 1
+                    if cs[s[start]] == 0:
+                        del cs[s[start]]
+                start += 1
 
         return results
 # @lc code=end
