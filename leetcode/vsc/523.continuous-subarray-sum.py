b2394196c525d9144baa1b743943a135d3f4b9f5
mahsanghani <ghaniahsan@outlook.com>
2023-06-10 14:14:04 -0400

    check subarray sum almost finished

--- leetcode/vsc/523.continuous-subarray-sum.py
+++ leetcode/vsc/523.continuous-subarray-sum.py
@@ -7,21 +7,22 @@
 # @lc code=start
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        d={0:[-1]}
+        d={0:-1}
         t=0
 
         for i,j in enumerate(nums):
             t+=j
             r=t%k
             if r not in d:
-                d[r]=[i]
-            elif i-d[r][0]>1:
-                d[r].append(i)
-        
-        for k,v in d.items():
-            if len(v)>1:
+                d[r]=i
+            elif i-d[r]>1:
                 return True
-            else:
-                return False
+            
+        return False
+        # for k,v in d.items():
+        #     if len(v)>1:
+        #         return True
+        #     else:
+        #         return False
 # @lc code=end
 
