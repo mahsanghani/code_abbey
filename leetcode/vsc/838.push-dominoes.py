4021d098b67e04cdf6269e774b8a579e3acecf15
mahsanghani <ghaniahsan@outlook.com>
2023-06-10 15:52:54 -0400

    push dominoes

---
+++ leetcode/vsc/838.push-dominoes.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=838 lang=python3
+#
+# [838] Push Dominoes
+#
+
+# @lc code=start
+from collections import deque
+class Solution:
+    def pushDominoes(self, dominoes: str) -> str:
+        d = list(dominoes)
+        q = deque()
+
+        for i,j in enumerate(d):
+            if j != ".":
+                q.append((i,j))
+
+        while q:
+            i,j = q.popleft()
+
+            if j=="L" and i>0 and d[i-1]==".":
+                q.append((i-1, "L"))
+                d[i-1] = "L"
+            elif j=="R":
+                if i+1<len(d) and d[i+1]==".":
+                    if i+2<len(d) and d[i+2]=="L":
+                        q.popleft()
+                    else:
+                        q.append((i+1,"R"))
+                        d[i+1] = "R"
+
+        return "".join(d)
+# @lc code=end
+
