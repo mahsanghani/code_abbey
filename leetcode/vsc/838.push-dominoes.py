7c1b788c29f7222516d2687fde80135a37de2a6b
mahsanghani <ghaniahsan@outlook.com>
2023-06-12 08:58:07 -0400

    push dominoes

--- leetcode/vsc/838.push-dominoes.py
+++ leetcode/vsc/838.push-dominoes.py
@@ -12,14 +12,14 @@ class Solution:
         q = deque()
 
         for i,j in enumerate(d):
-            if j != ".":
+            if j!=".":
                 q.append((i,j))
 
         while q:
             i,j = q.popleft()
 
             if j=="L" and i>0 and d[i-1]==".":
-                q.append((i-1, "L"))
+                q.append((i-1,"L"))
                 d[i-1] = "L"
             elif j=="R":
                 if i+1<len(d) and d[i+1]==".":
