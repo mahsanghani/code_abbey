85263ad25a62082855de3fabf6b6f4654b2c2a86
mahsanghani <ghaniahsan@outlook.com>
2023-06-13 07:17:29 -0400

    check possibility

--- leetcode/vsc/665.non-decreasing-array.py
+++ leetcode/vsc/665.non-decreasing-array.py
@@ -7,18 +7,19 @@
 # @lc code=start
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
-        d=-1
+        changed = False
         for i in range(len(nums)-1):
             if nums[i]<=nums[i+1]:
                 continue
-            if d==0:
+            if changed:
                 return False
-            if i==0 or nums[i+1] >= nums[i-1]:
+            
+            if i==0 or nums[i+1]>=nums[i-1]:
                 nums[i] = nums[i+1]
             else:
                 nums[i+1] = nums[i]
-            d+=1
+            changed = True
         return True
-
+    
 # @lc code=end
 
