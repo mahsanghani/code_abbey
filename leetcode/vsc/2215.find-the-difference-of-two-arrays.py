ed4ad027da83d8222c816298df98d0dcd354e913
mahsanghani <ghaniahsan@outlook.com>
2023-06-14 08:42:42 -0400

    find the difference of two arrays

--- leetcode/vsc/2215.find-the-difference-of-two-arrays.py
+++ leetcode/vsc/2215.find-the-difference-of-two-arrays.py
@@ -7,8 +7,9 @@
 # @lc code=start
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        set1,set2 = set(nums1),set(nums2)
-        return [set1-set2,set2-set1]
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return [set1-set2, set2-set1]
         
 # @lc code=end
 
