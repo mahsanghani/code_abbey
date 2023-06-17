afb3002a824199a0bd548af1dd8a9102008c0ec7
mahsanghani <ghaniahsan@outlook.com>
2023-06-17 13:13:01 -0400

    merge sorted array

--- leetcode/vsc/88.merge-sorted-array.py
+++ leetcode/vsc/88.merge-sorted-array.py
@@ -10,7 +10,21 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        nums1[m:] = nums2
-        return nums1.sort()
+        last = m + n - 1
+
+        while m>0 and n>0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[last] = nums1[m-1]
+                m-=1
+            else:
+                nums1[last] = nums2[n-1]
+                n-=1
+            last-=1
+        
+        while n>0:
+            nums1[last] = nums2[n-1]
+            n -= 1
+            last -= 1
+
 # @lc code=end
 
