bb8e7758245ea9d994e7f06ea61d434656d40629
mahsanghani <ghaniahsan@outlook.com>
2023-06-17 08:09:32 -0400

    valid palindrome 2

---
+++ leetcode/vsc/680.valid-palindrome-ii.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=680 lang=python3
+#
+# [680] Valid Palindrome II
+#
+
+# @lc code=start
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s)-1
+
+        while l<r:
+            if s[l]!=s[r]:
+                skipL = s[l+1:r+1]
+                skipR = s[l:r]
+                return (skipL == skipL[::-1] or skipR == skipR[::-1])
+            l+=1
+            r-=1
+        return True
+          
+    def isPalindrome(self, s: str) -> bool:
+        return [''.join(c.lower()) for c in s if c.isalnum()][::-1] == [''.join(c.lower()) for c in s if c.isalnum()]
+# @lc code=end
+
