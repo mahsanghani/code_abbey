c4d51d6ec677949ae1fe4534959dcb878293a693
mahsanghani <ghaniahsan@outlook.com>
2023-06-17 08:32:34 -0400

    merge strings alternately

--- leetcode/vsc/1768.merge-strings-alternately.py
+++ leetcode/vsc/1768.merge-strings-alternately.py
@@ -7,18 +7,15 @@
 # @lc code=start
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        i,j,k = 0,0,0
-        word3 = []
-
+        i,j = 0,0
+        res = []
         while i<len(word1) and j<len(word2):
-            word3.append(word1[i])
-            word3.append(word2[j])
+            res.append(word1[i])
+            res.append(word2[j])
             i+=1
             j+=1
-        word3.append(word1[i:])
-        word3.append(word2[j:])
-
-        return "".join(word3)
-
+        res.append(word1[i:])
+        res.append(word2[j:])
+        return ''.join(res)
 # @lc code=end
 
