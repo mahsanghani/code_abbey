4ac65db97d2e4f08941ed5aadabf7cbad957215e
mahsanghani <ghaniahsan@outlook.com>
2023-06-17 13:32:34 -0400

    remove duplicates

---
+++ leetcode/vsc/26.remove-duplicates-from-sorted-array.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=26 lang=python3
+#
+# [26] Remove Duplicates from Sorted Array
+#
+
+# @lc code=start
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i,l = 0, len(set(nums))
+        newset = set()
+
+        while i<l:
+            if nums[i] in newset:
+                nums.pop(i)
+                nums.append('_')
+                i-=1
+            else:
+                newset.add(nums[i])
+            i+=1
+        return len(newset)
+# @lc code=end
+
