88ca1cb65b21123bd18f051109ace8c57022d3d1
mahsanghani <ghaniahsan@outlook.com>
2023-06-17 13:10:00 -0400

    merge sorted array

--- leetcode/vsc/88.merge-sorted-array.py
+++ leetcode/vsc/88.merge-sorted-array.py
@@ -10,17 +10,7 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        p1 = m - 1
-        p2 = n - 1
-
-        for p in range(n+m-1, -1, -1):
-            if p2 < 0:
-                break
-            if p1 >= 0 and nums1[p1] > nums2[p2]:
-                nums1[p]= nums1[p1]
-                p1 -= 1
-            else:
-                nums1[p] = nums2[p2]
-                p2 -= 1
+        nums1[m:] = nums2
+        return nums1.sort()
 # @lc code=end
 
