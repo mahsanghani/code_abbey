8adbd8db739b20695b89a18c298815714b8b4628
mahsanghani <ghaniahsan@outlook.com>
2023-06-17 08:00:54 -0400

    valid palindrome

--- leetcode/vsc/125.valid-palindrome.py
+++ leetcode/vsc/125.valid-palindrome.py
@@ -7,6 +7,21 @@
 # @lc code=start
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        return [''.join(w.lower()) for w in s if w.isalnum()][::-1] == [''.join(w.lower()) for w in s if w.isalnum()]
+        l,r = 0, len(s)-1
+        while l<r:
+            while l<r and not self.isalphaNum(s[l]):
+                l+=1
+            while r>l and not self.isalphaNum(s[r]):
+                r-=1
+            if s[l].lower()!=s[r].lower():
+                return False
+            l+=1
+            r-=1
+        return True
+
+    def isalphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or 
+                ord('a') <= ord(c) <= ord('z') or 
+                ord('0') <= ord(c) <= ord('9'))
 # @lc code=end
 
