8b96e9d52b88d992401f869033daf2025ebcdf25
mahsanghani <ghaniahsan@outlook.com>
2023-06-17 10:09:10 -0400

    merge sorted array

---
+++ leetcode/vsc/88.merge-sorted-array.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=88 lang=python3
+#
+# [88] Merge Sorted Array
+#
+
+# @lc code=start
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1 = m - 1
+        p2 = n - 1
+
+        for p in range(n+m-1, -1, -1):
+            if p2 < 0:
+                break
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p]= nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+# @lc code=end
+
