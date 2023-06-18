bedebfef1b5d2740e9097ba333ec2a980941026e
mahsanghani <ghaniahsan@outlook.com>
2023-06-18 13:44:51 -0400

    3 sum

--- leetcode/vsc/15.3-sum.py
+++ leetcode/vsc/15.3-sum.py
@@ -7,21 +7,32 @@
 # @lc code=start
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        comp = 0
-        d, ss = {}, []
-        n = len(nums) - 1
-        
         nums.sort()
-        nums = list(set(nums))
+        n = len(nums)
+        ss = []
 
-        for i,j in enumerate(nums):
-            while n!=i and -1>j<n:
-                comp = nums[i]+nums[n]
-                n -= 1
-            if comp in d:
-                ss.append([comp, nums[i], nums[n]])
-            else:
-                d[i] = j
+        for i in range(n-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            left = i + 1
+            right = n - 1
+
+            while left < right:
+                curr_sum = nums[i] + nums[left] + nums[right]
+
+                if curr_sum == 0:
+                    ss.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elif curr_sum < 0:
+                    left += 1
+                else:
+                    right -= 1
         return ss
 # @lc code=end
 
