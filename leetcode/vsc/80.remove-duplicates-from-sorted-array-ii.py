4497af1cf40c65d45fd3f3f7623937b52ae28bf7
mahsanghani <ghaniahsan@outlook.com>
2023-06-18 10:31:28 -0400

    remove duplicates 2

--- leetcode/vsc/80.remove-duplicates-from-sorted-array-ii.py
+++ leetcode/vsc/80.remove-duplicates-from-sorted-array-ii.py
@@ -5,8 +5,21 @@
 #
 
 # @lc code=start
+from collections import Counter
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        
+        i,l = 0,len(nums)
+        d = Counter(nums)
+
+        while i<l:
+            if d[nums[i]]>2:
+                nums.pop(i)
+                nums.append('_')
+                d[nums[i]]-=1
+                l-=1
+            else:
+                i+=1
+        return l
+
 # @lc code=end
 
