340e2e9be2cd33af1841918b526d1eef58b95487
mahsanghani <ghaniahsan@outlook.com>
2023-06-18 14:14:33 -0400

    four sum

--- leetcode/vsc/18.4-sum.py
+++ leetcode/vsc/18.4-sum.py
@@ -7,6 +7,14 @@
 # @lc code=start
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        res = []
+        quad= []
+
+        def helper():
+            return
         
+        helper(4,0,target)
+        return res
 # @lc code=end
 
