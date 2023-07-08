efbc0705dfdc4f3ca75c904a40ac357c84a9984b
mahsanghani <ghaniahsan@outlook.com>
2023-07-08 15:08:30 -0400

    boats to save people

--- leetcode/vsc/881.boats-to-save-people.py
+++ leetcode/vsc/881.boats-to-save-people.py
@@ -7,6 +7,20 @@
 # @lc code=start
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        results = 0
+        l,r = 0,len(people)-1
+
+        while l<=r:
+            if people[l]+people[r]<=limit:
+                results+=1
+                l+=1
+                r-=1
+            elif people[r]<=limit:
+                results+=1
+                r-=1
         
+        return results
+
 # @lc code=end
 
