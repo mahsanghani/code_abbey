193439fb92cdee978a32bfc722d5be012708944b
mahsanghani <ghaniahsan@outlook.com>
2023-07-08 14:58:53 -0400

    array with elements not equal to average of neighbours

--- leetcode/vsc/1968.array-with-elements-not-equal-to-average-of-neighbors.py
+++ leetcode/vsc/1968.array-with-elements-not-equal-to-average-of-neighbors.py
@@ -7,6 +7,18 @@
 # @lc code=start
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        l,r = 0, len(nums)-1
+        results = []
+
+        while len(results)!=len(nums):
+            results.append(nums[l])
+            l+=1
+
+            if l<=r:
+                results.append(nums[r])
+                r-=1
         
+        return results
 # @lc code=end
 
