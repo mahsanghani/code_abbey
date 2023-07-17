21800376ccd49ab88a78d1b98a3f6f8152a49c76
mahsanghani <ghaniahsan@outlook.com>
2023-07-17 15:54:52 -0700

    contains duplicate 2

---
+++ leetcode/vsc/1343.number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1343 lang=python3
+#
+# [1343] Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold
+#
+
+# @lc code=start
+class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        
+# @lc code=end
+
--- leetcode/vsc/219.contains-duplicate-ii.py
+++ leetcode/vsc/219.contains-duplicate-ii.py
@@ -8,12 +8,10 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         hashdict = {}
-
         for idx, num in enumerate(nums):
-            if num in hashdict and abs(idx - hashdict[num]) <= k:
+            if num in hashdict and abs(idx - hashdict[num])<=k:
                 return True
-            hashdict[num] = idx
-        
+            hashdict[num]=idx
         return False
 # @lc code=end
 
