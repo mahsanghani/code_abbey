8d61a80e315732bf7c3c932fdc104673cb27baff
mahsanghani <ghaniahsan@outlook.com>
2023-07-17 14:43:39 -0700

    best time to buy and sell stocks

--- leetcode/vsc/121.best-time-to-buy-and-sell-stock.py
+++ leetcode/vsc/121.best-time-to-buy-and-sell-stock.py
@@ -7,9 +7,7 @@
 # @lc code=start
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        p=0
-        l=0
-        r=1
+        p,l,r = 0,0,1
 
         while r<len(prices):
             p=max(p,prices[r]-prices[l])
