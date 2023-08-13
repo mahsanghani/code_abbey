cf619abc9b3e4a6921ee4f899fa737e4d468bd59
mahsanghani <ghaniahsan@outlook.com>
2023-08-13 14:50:14 -0700

    SetColor() ClearColor()

--- projects/calendar/calendar.c
+++ projects/calendar/calendar.c
@@ -23,3 +23,19 @@ void gotoxy(int x, int y) {
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), xy);
 }
 
+void SetColor(int ForgC) {
+	WORD wColor;
+	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+	if (GetConsoleScreenBufferInfo(hStdOut, &csbi)) {
+		wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
+		SetConsoleTextAttribute(hStdOut, wColor);
+	}
+	return;
+}
+
+void ClearColor() {
+	SetColor(15);
+}
+
