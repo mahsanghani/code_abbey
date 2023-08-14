8c940713e0d3af54dedf9dac77601e083101b561
mahsanghani <ghaniahsan@outlook.com>
2023-08-13 17:20:24 -0700

    SetColorAndBackground

--- projects/calendar/calendar.c
+++ projects/calendar/calendar.c
@@ -54,3 +54,10 @@ void ClearConsoleToColors(int ForgC, int BackC) {
     }
     return;
 }
+
+void SetColorAndBackground(int ForgC, int BackC) {
+	WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), wColor);
+	return;
+}
+
