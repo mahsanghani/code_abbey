01e531b607768814d800733a52d791fcf2ba5c1c
mahsanghani <ghaniahsan@outlook.com>
2023-08-13 17:28:34 -0700

    getName

--- projects/calendar/calendar.c
+++ projects/calendar/calendar.c
@@ -109,3 +109,16 @@ int getNumberOfDays(int month, int year) { //returns the number of days in given
 	}
 }
 
+char* getName(int day) { //returns the name of the day
+	switch (day) {
+	case 0:return("Sunday");
+	case 1:return("Monday");
+	case 2:return("Tuesday");
+	case 3:return("Wednesday");
+	case 4:return("Thursday");
+	case 5:return("Friday");
+	case 6:return("Saturday");
+	default:return("Error in getName() module.Invalid argument passed");
+	}
+}
+
