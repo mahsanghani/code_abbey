00dd3736f34f6216d324f303e6a844a53d7a87f5
mahsanghani <ghaniahsan@outlook.com>
2023-08-13 17:29:01 -0700

    print_date

--- projects/calendar/calendar.c
+++ projects/calendar/calendar.c
@@ -122,3 +122,25 @@ char* getName(int day) { //returns the name of the day
 	}
 }
 
+void print_date(int mm, int yy) { //prints the name of month and year
+	printf("---------------------------\n");
+	gotoxy(25, 6);
+	switch (mm) {
+	case 1: printf("January"); break;
+	case 2: printf("February"); break;
+	case 3: printf("March"); break;
+	case 4: printf("April"); break;
+	case 5: printf("May"); break;
+	case 6: printf("June"); break;
+	case 7: printf("July"); break;
+	case 8: printf("August"); break;
+	case 9: printf("September"); break;
+	case 10: printf("October"); break;
+	case 11: printf("November"); break;
+	case 12: printf("December"); break;
+	}
+	printf(" , %d", yy);
+	gotoxy(20, 7);
+	printf("---------------------------");
+}
+
