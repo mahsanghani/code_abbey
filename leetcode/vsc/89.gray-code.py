68f09c3870abb6181fca9603fb789df0f28a1a1d
mahsanghani <ghaniahsan@outlook.com>
2023-08-14 09:21:34 -0700

    gray codes recursive

--- leetcode/vsc/89.gray-code.py
+++ leetcode/vsc/89.gray-code.py
@@ -7,34 +7,34 @@
 # @lc code=start
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        # def calculate(n):
-        #     if n==1:
-        #         return ["0","1"]
-        #     temp = calculate(n-1)
-        #     results = []
+        def calculate(n):
+            if n==1:
+                return ["0","1"]
+            temp = calculate(n-1)
+            results = []
             
-        #     for i in range(len(temp)):
-        #         results.append("0" + temp[i])
+            for i in range(len(temp)):
+                results.append("0" + temp[i])
 
-        #     for i in range(len(temp)-1,-1,-1):
-        #         results.append("1" + temp[i])
+            for i in range(len(temp)-1,-1,-1):
+                results.append("1" + temp[i])
 
-        #     return results
+            return results
         
-        # combinations = calculate(n)
+        combinations = calculate(n)
 
-        # results = []
-        # for comb in combinations:
-        #     results.append(int(comb,2))
+        results = []
+        for comb in combinations:
+            results.append(int(comb,2))
 
-        # return results
+        return results
 
-        codes = [0]
-        offset = 1
-        for i in range(n):
-            for j in range(len(codes)-1,-1,-1):
-                codes.append(codes[j]+offset)
-            offset = offset << 1
-        return codes
+        # codes = [0]
+        # offset = 1
+        # for i in range(n):
+        #     for j in range(len(codes)-1,-1,-1):
+        #         codes.append(codes[j]+offset)
+        #     offset = offset << 1
+        # return codes
 # @lc code=end
 
