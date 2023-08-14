1a6cccf0daef34b7b3ee6e0d04012d4055947d52
mahsanghani <ghaniahsan@outlook.com>
2023-08-13 17:19:38 -0700

    ClearConsoleToColors

--- projects/calendar/calendar.c
+++ projects/calendar/calendar.c
@@ -39,3 +39,18 @@ void ClearColor() {
 	SetColor(15);
 }
 
+void ClearConsoleToColors(int ForgC, int BackC) {
+    WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);
+    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD coord = { 0, 0 };
+    DWORD count;
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+	SetConsoleTextAttribute(hStdOut, wColor);
+    if (GetConsoleScreenBufferInfo(hStdOut, &csbi)) {
+        FillConsoleOutputCharacter(hStdOut, (TCHAR)32, csbi.dwSize.X * csbi.dwSize.Y, coord, &count);
+        FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, csbi.dwSize.X * csbi.dwSize.Y, coord, &count);
+        SetConsoleCursorPosition(hStdOut, coord);
+    }
+    return;
+}
