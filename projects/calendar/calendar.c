e7b779ed3f0a8af97adcc1629c5dbc8b923affb9
mahsanghani <ghaniahsan@outlook.com>
2023-08-13 17:30:21 -0700

    checkNote

--- projects/calendar/calendar.c
+++ projects/calendar/calendar.c
@@ -190,3 +190,19 @@ char* getDay(int dd, int mm, int yy) {
 	}
 }
 
+int checkNote(int dd, int mm) {
+	FILE* fp;
+	fp = fopen("note.dat", "rb");
+	if (fp == NULL) {
+		printf("Error in Opening the file");
+	}
+	while (fread(&R, sizeof(R), 1, fp) == 1) {
+		if (R.dd == dd && R.mm == mm) {
+			fclose(fp);
+			return 1;
+		}
+	}
+	fclose(fp);
+	return 0;
+}
+
