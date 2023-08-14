0498a4f1290736d7efb38ee1c7bebedceca590cf
mahsanghani <ghaniahsan@outlook.com>
2023-08-13 17:31:44 -0700

    main

--- projects/calendar/calendar.c
+++ projects/calendar/calendar.c
@@ -351,3 +351,58 @@ void showNote(int mm) {
 
 }
 
+int main() {
+	ClearConsoleToColors(15, 1);
+	SetConsoleTitle("Calender Project - Programming-technique.blogspot.com");
+	int choice;
+	char ch = 'a';
+	while (1) {
+		system("cls");
+		printf("1. Find Out the Day\n");
+		printf("2. Print all the day of month\n");
+		printf("3. Add Note\n");
+		printf("4. EXIT\n");
+		printf("ENTER YOUR CHOICE : ");
+		scanf("%d", &choice);
+		system("cls");
+		switch (choice) {
+		case 1:
+			printf("Enter date (DD MM YYYY) : ");
+			scanf("%d %d %d", &date.dd, &date.mm, &date.yy);
+			printf("Day is : %s", getDay(date.dd, date.mm, date.yy));
+			printf("\nPress any key to continue......");
+			getch();
+			break;
+		case 2:
+			printf("Enter month and year (MM YYYY) : ");
+			scanf("%d %d", &date.mm, &date.yy);
+			system("cls");
+			while (ch != 'q') {
+				printMonth(date.mm, date.yy, 20, 5);
+				ch = getch();
+				if (ch == 'n') {
+					increase_month(&date.mm, &date.yy);
+					system("cls");
+					printMonth(date.mm, date.yy, 20, 5);
+				}
+				else if (ch == 'p') {
+					decrease_month(&date.mm, &date.yy);
+					system("cls");
+					printMonth(date.mm, date.yy, 20, 5);
+				}
+				else if (ch == 's') {
+					showNote(date.mm);
+					system("cls");
+				}
+			}
+			break;
+		case 3:
+			AddNote();
+			break;
+		case 4:
+			exit(0);
+		}
+	}
+	return 0;
+}
+
