1a6852fb63874afa63e7f8aed9945f4e9052ad88
mahsanghani <ghaniahsan@outlook.com>
2023-08-13 17:28:14 -0700

    getNumberOfDays

--- projects/calendar/calendar.c
+++ projects/calendar/calendar.c
@@ -88,3 +88,24 @@ void decrease_month(int* mm, int* yy) {
 	}
 }
 
+int getNumberOfDays(int month, int year) { //returns the number of days in given month
+	switch (month) {                          //and year
+	case 1: return(31);
+	case 2: if (check_leapYear(year) == 1)
+		return(29);
+		  else
+		return(28);
+	case 3: return(31);
+	case 4: return(30);
+	case 5: return(31);
+	case 6: return(30);
+	case 7: return(31);
+	case 8: return(31);
+	case 9: return(30);
+	case 10: return(31);
+	case 11: return(30);
+	case 12: return(31);
+	default: return(-1);
+	}
+}
+
