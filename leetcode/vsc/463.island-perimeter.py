204d2cab8e24bd73cf2150f11138f1b8f05ee6b5
mahsanghani <ghaniahsan@outlook.com>
2023-08-31 17:44:29 -0400

    island perimeter

--- leetcode/vsc/463.island-perimeter.py
+++ leetcode/vsc/463.island-perimeter.py
@@ -7,27 +7,26 @@
 # @lc code=start
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        # perimeter = 0
         visit = set()
-        # directions = [[1,0],[0,1],[-1,0],[0,-1]]
 
         def dfs(i,j):
-            if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j]==0:
-                return 1
             if (i,j) in visit:
                 return 0
+            if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j]==0:
+                return 1
             
             visit.add((i,j))
 
             perimeter = dfs(i,j+1)
-            perimeter += dfs(i+1,j)
             perimeter += dfs(i,j-1)
             perimeter += dfs(i-1,j)
-            return perimeter
+            perimeter += dfs(i+1,j)
 
+            return perimeter
+        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j]:
+                if grid[i][j]==1:
                     return dfs(i,j)
 
 # @lc code=end
