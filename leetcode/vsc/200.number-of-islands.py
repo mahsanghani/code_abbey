f17572fb7b02c4aabb7a54fe020506f1bfbfa258
mahsanghani <ghaniahsan@outlook.com>
2023-09-01 12:27:08 -0400

    number of islands

--- leetcode/vsc/200.number-of-islands.py
+++ leetcode/vsc/200.number-of-islands.py
@@ -12,36 +12,37 @@ class Solution:
         if not grid:
             return 0
         
-        rows = len(grid)
-        cols = len(grid[0])
-        visit = set()
-        islands = 0
-
+        rows,cols,visit,islands = len(grid),len(grid[0]),set(),0
+        
         def bfs(r,c):
-            q=collections.deque()
+            q = collections.deque()
             visit.add((r,c))
             q.append((r,c))
 
             while q:
                 row,col = q.popleft()
-                directions = [[0,1],[1,0],[0,-1],[-1,0]]
+                directions = [[0,1],[1,0],[-1,0],[0,-1]]
 
-                for dr, dc in directions:
-                    r,c = row+dr,col+dc
-                    if (r in range(rows) and 
-                        c in range(cols) and 
-                        grid[r][c] == "1" and
+                for dr,dc in directions:
+                    r=row+dr
+                    c=col+dc
+                
+                    if (r in range(rows) and
+                        c in range(cols) and
+                        grid[r][c]=="1" and 
                         (r,c) not in visit):
                         visit.add((r,c))
                         q.append((r,c))
 
+
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r,c) not in visit:
+                if grid[r][c]=="1" and (r,c) not in visit:
                     bfs(r,c)
                     islands+=1
 
         return islands
+
 
 # @lc code=end
 
