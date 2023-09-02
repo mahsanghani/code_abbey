137451db591f8c4e538bfdf2065eb8854c4bbb89
mahsanghani <ghaniahsan@outlook.com>
2023-09-02 12:04:19 -0400

    rotting oranges

---
+++ leetcode/vsc/1553.minimum-number-of-days-to-eat-n-oranges.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1553 lang=python3
+#
+# [1553] Minimum Number of Days to Eat N Oranges
+#
+
+# @lc code=start
+class Solution:
+    def minDays(self, n: int) -> int:
+        
+# @lc code=end
+
---
+++ leetcode/vsc/994.rotting-oranges.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=994 lang=python3
+#
+# [994] Rotting Oranges
+#
+
+# @lc code=start
+import collections
+from collections import deque
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        time,fresh,q,rows,cols = 0,0,deque(),len(grid),len(grid[0])
+        directions = [[0,1],[1,0],[-1,0],[0,-1]]
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]==1:
+                    fresh+=1
+                if grid[r][c]==2:
+                    q.append([r,c])
+
+        while q and fresh>0:
+            for i in range(len(q)):
+                row,col = q.popleft()
+                for dr,dc in directions:
+                    r,c = row+dr, col+dc
+                    if (r<0 or c<0 or r==rows or c==cols or grid[r][c]!=1):
+                        continue
+                    grid[r][c]=2
+                    q.append([r,c])
+                    fresh-=1
+            time+=1
+        return time if fresh==0 else -1
+# @lc code=end
+
