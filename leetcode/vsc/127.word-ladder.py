aa5cbceffd32a69203438098fd61f4a0fd5fcb54
mahsanghani <ghaniahsan@outlook.com>
2023-09-03 09:43:28 -0400

    word ladder

--- leetcode/vsc/127.word-ladder.py
+++ leetcode/vsc/127.word-ladder.py
@@ -6,19 +6,38 @@
 
 # @lc code=start
 import collections
+from collections import deque
 from collections import defaultdict
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         adj = collections.defaultdict(list)
         wordList.append(beginWord)
 
-        if endWord in wordList:
+        results = 1
+        visit = set([beginWord])
+        q = collections.deque([beginWord])
+
+        if endWord not in wordList:
             return 0
         
         for word in wordList:
             for j in range(len(word)):
                 pattern = word[:j] + "*" + word[j+1:]
                 adj[pattern].append(word)
-                
+
+        while q:
+            for i in range(len(q)):
+                word = q.popleft()
+                if word == endWord:
+                    return results
+                for j in range(len(word)):
+                    pattern = word[:j] + "*" + word[j+1:]
+                    for neighbour in adj[pattern]:
+                        if neighbour not in visit:
+                            visit.add(neighbour)
+                            q.append(neighbour)
+            results+=1
+        return 0
+
 # @lc code=end
 
