50b92cb0ac8d36b00334762024839638dca9d5b8
mahsanghani <ghaniahsan@outlook.com>
2023-09-03 10:02:28 -0400

    reconstruct itinerary

---
+++ leetcode/vsc/332.reconstruct-itinerary.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode id=332 lang=python3
+#
+# [332] Reconstruct Itinerary
+#
+
+# @lc code=start
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj = {s:[] for s,d in tickets}
+        
+        tickets.sort()
+        results = ["JFK"]
+        
+        for s,d in tickets:
+            adj[s].append(d)
+
+        def dfs(c):
+            if len(results)==len(tickets)+1:
+                return True
+            if c not in adj:
+                return False
+            
+            temp = list(adj[c])
+
+            for k,v in enumerate(temp):
+                adj[c].pop(k)
+                results.append(v)
+                if dfs(v):
+                    return True
+                adj[c].append(k,v)
+                results.pop()
+
+            return False
+
+        dfs("JFK")
+        return results
+
+# @lc code=end
+
