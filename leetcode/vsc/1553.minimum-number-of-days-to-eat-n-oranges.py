98f3a1c26c29931d4bced0a3ffa7544e2e87a0d9
mahsanghani <ghaniahsan@outlook.com>
2023-09-03 09:16:01 -0400

    minimum number of days to eat n oranges

--- leetcode/vsc/1553.minimum-number-of-days-to-eat-n-oranges.py
+++ leetcode/vsc/1553.minimum-number-of-days-to-eat-n-oranges.py
@@ -8,18 +8,13 @@
 class Solution:
     def minDays(self, n: int) -> int:
         dp={0:0,1:1}
-
         def dfs(n):
             if n in dp:
                 return dp[n]
-            
-            one = 1+(n%2)+dfs(n//2)
-            two = 1+(n%3)+dfs(n//3)
-
-            dp[n] = min(one,two)
-
+            one=1+(n%2)+dfs(n//2)
+            two=1+(n%3)+dfs(n//3)
+            dp[n]=min(one,two)
             return dp[n]
-        
         return dfs(n)
 # @lc code=end
 
