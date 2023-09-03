2ec81099f96f3c4d3ff75e874c360bf137d98992
mahsanghani <ghaniahsan@outlook.com>
2023-09-03 10:58:28 -0400

    alien dictionary

--- leetcode/vsc/269.alien-dictionary.py
+++ leetcode/vsc/269.alien-dictionary.py
@@ -7,7 +7,43 @@
 # @lc code=start
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        return []
+        adj = {i: set() for w in words for i in w}
+
+        visit = {}
+        results = []
+
+        for i in range(len(words)-1):
+            w1,w2 = words[i],words[i+1]
+            minlen= min(len(w1),len(w2))
+
+            if len(w1)>len(w2) and w1[:minlen]==w2[:minlen]:
+                return ""
+            for j in range(minlen):
+                if w1[j]!=w2[j]:
+                    adj[w1[j]].add(w2[j])
+                    break
+
+        def dfs(c):
+            if c in visit:
+                return visit[c]
+            if c not in adj:
+                return False
+            
+            visit[c] = True
+
+            for j in adj[c]:
+                if dfs(j):
+                    return True
+                
+            visit[c] = False
+            results.append(c)
+
+
+        for c in adj:
+            if dfs(c):
+                return ""
+            
+        return "".join(results[::-1])
 
 # @lc code=end
 
