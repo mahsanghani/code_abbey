2d5585d0aa981b45c9a205f5bd3f0d4172b86758
mahsanghani <ghaniahsan@outlook.com>
2023-09-03 09:36:55 -0400

    word ladder

--- leetcode/vsc/127.word-ladder.py
+++ leetcode/vsc/127.word-ladder.py
@@ -5,8 +5,20 @@
 #
 
 # @lc code=start
+import collections
+from collections import defaultdict
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        adj = collections.defaultdict(list)
+        wordList.append(beginWord)
+
+        if endWord in wordList:
+            return 0
         
+        for word in wordList:
+            for j in range(len(word)):
+                pattern = word[:j] + "*" + word[j+1:]
+                adj[pattern].append(word)
+                
 # @lc code=end
 
