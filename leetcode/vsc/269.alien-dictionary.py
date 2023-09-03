ce098aa9f2924c985675ae426b46c89aa5a4de85
mahsanghani <ghaniahsan@outlook.com>
2023-09-03 10:54:43 -0400

    alien dictionary

---
+++ leetcode/vsc/269.alien-dictionary.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=269 lang=python3
+#
+# [269] Alien Dictionary
+#
+
+# @lc code=start
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        return []
+
+# @lc code=end
+
