0fc427118ce71a4fd12a92dcd93c9dc084152e3c
mahsanghani <ghaniahsan@outlook.com>
2023-09-04 12:29:10 -0400

    find critical and pseudo critical edges in minimum spanning tree

--- leetcode/vsc/1489.find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.py
+++ leetcode/vsc/1489.find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.py
@@ -31,6 +31,42 @@ class UnionFind:
 
 class Solution:
     def findCriticalAndPseudoCriticalEdges(self, n: int, edges: List[List[int]]) -> List[List[int]]:
-        
+        for i, j in enumerate(edges):
+            j.append(i)
+            # [v1, v2, weight, original_index]
+
+        edges.sort(key=lambda e: e[2])
+
+        pseudo = []
+        critical = []
+        most_weight = 0
+        uf = UnionFind(n)
+
+        for v1, v2, w, i in edges:
+            if uf.union(v1, v2):
+                most_weight += w
+
+        for n1, n2, e_weight, i in edges:
+            weight = 0
+            uf = UnionFind(n)
+            for v1, v2, w, j in edges:
+                if i != j and uf.union(v1, v2):
+                    weight += w
+            if max(uf.rank) != n or weight > most_weight:
+                critical.append(i)
+                continue
+
+            uf = UnionFind(n)
+            uf.union(n1, n2)
+            weight = e_weight
+            for v1, v2, w, j in edges:
+                if uf.union(v1, v2):
+                    weight += w
+
+            if weight == most_weight:
+                pseudo.append(i)
+
+        return [critical, pseudo]
+
 # @lc code=end
 
