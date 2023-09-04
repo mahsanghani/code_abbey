6c029a687a733a8d8f4cc5fdc5782c04f86f7569
mahsanghani <ghaniahsan@outlook.com>
2023-09-04 14:24:32 -0400

    alien dictionary

--- leetcode/vsc/269.alien-dictionary.py
+++ leetcode/vsc/269.alien-dictionary.py
@@ -13,28 +13,28 @@ class Solution:
         results = []
 
         for i in range(len(words)-1):
-            w1,w2 = words[i],words[i+1]
+            w1 = words[i]
+            w2 = words[i+1]
             minlen = min(len(w1),len(w2))
-
-            if len(w1)>len(w2) and w1[:minlen] == w2[:minlen]:
+            if len(w1)>len(w2) and w1[:minlen]==w2[:minlen]:
                 return ""
             for j in range(minlen):
-                if w1[j] != w2[j]:
+                if w1[j]!=w2[j]:
                     adj[w1[j]].add(w2[j])
                     break
-            
+
         def dfs(c):
             if c in visit:
                 return visit[c]
             if c not in adj:
-                return False
+                return ""
             
             visit[c] = True
 
             for j in adj[c]:
                 if dfs(j):
                     return True
-                
+            
             visit[c] = False
             results.append(c)
 
