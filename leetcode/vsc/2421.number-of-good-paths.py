158ed9b6aab589bbf7bedb8f234ba4eff6634f94
mahsanghani <ghaniahsan@outlook.com>
2023-09-04 10:21:08 -0400

    union find

--- leetcode/vsc/2421.number-of-good-paths.py
+++ leetcode/vsc/2421.number-of-good-paths.py
@@ -5,6 +5,31 @@
 #
 
 # @lc code=start
+class UnionFind:
+    def __init__(self, n):
+        self.par = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, i):
+        while i != self.par[i]:
+            self.par[i] = self.par[self.par[i]]
+            i = self.par[i]
+        return i
+    
+    def union(self, a, b):
+        aRoot = self.find(a)
+        bRoot = self.find(b)
+
+        if aRoot == bRoot:
+            return False
+        if self.rank[aRoot] < self.rank[bRoot]:
+            self.par[aRoot] = bRoot
+            self.rank[bRoot] += self.rank[aRoot]
+        else:
+            self.par[bRoot] = aRoot
+            self.rank[aRoot] += self.rank[bRoot]
+        return True
+
 class Solution:
     def numberOfGoodPaths(self, vals: List[int], edges: List[List[int]]) -> int:
         
