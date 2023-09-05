bb8d396159f9ea4268fb94658a4cdea21e12f60e
mahsanghani <ghaniahsan@outlook.com>
2023-09-05 19:34:44 -0400

    range sum query immutable

--- leetcode/vsc/303.range-sum-query-immutable.py
+++ leetcode/vsc/303.range-sum-query-immutable.py
@@ -11,7 +11,7 @@ class NumArray:
         self.nums = nums
 
     def sumRange(self, left: int, right: int) -> int:
-        return sum(self.nums[left:right+1])
+        return sum(self.nums[left:right + 1])
 
 
 # Your NumArray object will be instantiated and called as such:
