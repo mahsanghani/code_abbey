bb9e5f5bd87471e29faf6aabab39e0113c52b122
mahsanghani <ghaniahsan@outlook.com>
2023-09-05 19:32:09 -0400

    implement queue using stacks

---
+++ leetcode/vsc/232.implement-queue-using-stacks.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode id=232 lang=python3
+#
+# [232] Implement Queue using Stacks
+#
+
+# @lc code=start
+class MyQueue:
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+
+        self.stack1.append(x)
+
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+
+    def pop(self) -> int:
+        return self.stack1.pop()
+
+    def peek(self) -> int:
+        return self.stack1[-1]
+
+    def empty(self) -> bool:
+        return len(self.stack1) == 0
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
+# @lc code=end
+
