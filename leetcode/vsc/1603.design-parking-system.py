b4c505fd562ba34b425add2fb5864346129dfe0d
mahsanghani <ghaniahsan@outlook.com>
2023-09-06 17:28:04 -0400

    design parking system

--- leetcode/vsc/1603.design-parking-system.py
+++ leetcode/vsc/1603.design-parking-system.py
@@ -8,11 +8,11 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        self.spaces = [big,medium,small]
+        self.spaces = {1: big, 2: medium, 3: small}
 
     def addCar(self, carType: int) -> bool:
-        if self.spaces[carType-1] > 0:
-            self.spaces[carType-1] -= 1
+        if self.spaces[carType] > 0:
+            self.spaces[carType] -= 1
             return True
         else:
             return False
