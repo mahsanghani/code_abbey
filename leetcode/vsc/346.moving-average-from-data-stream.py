07ffacdf9121ccc9497fefc96cf33777f38cff30
mahsanghani <ghaniahsan@outlook.com>
2023-09-05 20:51:35 -0400

    moving average from data stream

--- leetcode/vsc/346.moving-average-from-data-stream.py
+++ leetcode/vsc/346.moving-average-from-data-stream.py
@@ -5,19 +5,22 @@
 #
 
 # @lc code=start
+import collections
+from collections import deque
 class MovingAverage:
 
     def __init__(self, size: int):
         self.size = size
-        self.queue = []
+        self.queue = deque()
+        self.win_sum = 0
+        self.count = 0
 
     def next(self, val: int) -> float:
-        size = self.size
-        queue = self.queue
-        queue.append(val)
-        win_sum = sum(queue[-size:])
-        return win_sum/min(len(queue),size)
-
+        self.count += 1
+        self.queue.append(val)
+        tail = self.queue.popleft() if self.count > self.size else 0
+        self.win_sum = self.win_sum - tail + val
+        return self.win_sum/min(self.size,self.count)
 
 # Your MovingAverage object will be instantiated and called as such:
 # obj = MovingAverage(size)
