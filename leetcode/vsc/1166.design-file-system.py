f00558af44a82513052665a005c17b40071234bd
mahsanghani <ghaniahsan@outlook.com>
2023-09-07 19:29:12 -0400

    design file system

--- leetcode/vsc/1166.design-file-system.py
+++ leetcode/vsc/1166.design-file-system.py
@@ -13,11 +13,15 @@ class FileSystem:
         self.paths = collections.defaultdict()
 
     def createPath(self, path: str, value: int) -> bool:
-        if path in self.paths or '/'.join(path.split('/')[:-2]) not in self.paths:
+        if path in self.paths or len(path) == 0 or path == "/":
             return False
-        else:
-            self.paths[path] = value
-            return True
+        
+        parent = path[:path.rfind('/')]
+        if len(parent)>1 and parent not in self.paths:
+            return False
+
+        self.paths[path] = value
+        return True
 
     def get(self, path: str) -> int:
         return self.paths.get(path, -1)
