44e4e6f8e35b2c8e90e1d17bca65cc86bc9e8d28
mahsanghani <ghaniahsan@outlook.com>
2023-09-07 15:28:00 -0400

    design browser history

--- leetcode/vsc/1472.design-browser-history_DLL.py
+++ leetcode/vsc/1472.design-browser-history_DLL.py
@@ -5,32 +5,19 @@
 #
 
 # @lc code=start
-import heapq
+class DLLNode:
+    def __init__(self, url: str):
+        self.prev = None
+        self.next = None
+        self.data = url
 class BrowserHistory:
-
     def __init__(self, homepage: str):
-        self.history = []
-        self.future = []
-        self.current = homepage
-
+        
     def visit(self, url: str) -> None:
-        self.history.append(self.current)
-        self.current = url
-        self.future = []
 
     def back(self, steps: int) -> str:
-        while steps>0 and self.history:
-            self.future.append(self.current)
-            self.current = self.history.pop()
-            steps -= 1
-        return self.current
 
     def forward(self, steps: int) -> str:
-        while steps>0 and self.future:
-            self.history.append(self.current)
-            self.current = self.future.pop()
-            steps -= 1
-        return self.current
 
 
 # Your BrowserHistory object will be instantiated and called as such:
