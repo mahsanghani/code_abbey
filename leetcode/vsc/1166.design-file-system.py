140f389e66286d082d58bf56e49a30e880bc1c04
mahsanghani <ghaniahsan@outlook.com>
2023-09-07 17:20:28 -0400

    design file system

---
+++ leetcode/vsc/1166.design-file-system.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=1166 lang=python3
+#
+# [1166] Design File System
+#
+
+# @lc code=start
+class FileSystem:
+
+    def __init__(self):
+        
+
+    def createPath(self, path: str, value: int) -> bool:
+        
+
+    def get(self, path: str) -> int:
+        
+
+
+# Your FileSystem object will be instantiated and called as such:
+# obj = FileSystem()
+# param_1 = obj.createPath(path,value)
+# param_2 = obj.get(path)
+# @lc code=end
+
---
+++ leetcode/vsc/588.design-in-memory-file-system.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=588 lang=python3
+#
+# [588] Design In-Memory File System
+#
+
+# @lc code=start
+class FileSystem:
+
+    def __init__(self):
+        
+
+    def ls(self, path: str) -> List[str]:
+        
+
+    def mkdir(self, path: str) -> None:
+        
+
+    def addContentToFile(self, filePath: str, content: str) -> None:
+        
+
+    def readContentFromFile(self, filePath: str) -> str:
+        
+
+
+# Your FileSystem object will be instantiated and called as such:
+# obj = FileSystem()
+# param_1 = obj.ls(path)
+# obj.mkdir(path)
+# obj.addContentToFile(filePath,content)
+# param_4 = obj.readContentFromFile(filePath)
+# @lc code=end
+
