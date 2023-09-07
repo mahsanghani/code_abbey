8c63ad586e9f5a13ff200dfffdbd54173f1bf71c
mahsanghani <ghaniahsan@outlook.com>
2023-09-07 19:39:33 -0400

    design in memory file system

--- leetcode/vsc/588.design-in-memory-file-system.py
+++ leetcode/vsc/588.design-in-memory-file-system.py
@@ -5,22 +5,52 @@
 #
 
 # @lc code=start
-class FileSystem:
+import sortedcontainers
+from sortedcontainers import SortedDict
 
+class Node:
     def __init__(self):
-        
+        self.content = ""
+        self.children = SortedDict()
+class FileSystem:
+    def __init__(self):
+        self.root = Node()
 
     def ls(self, path: str) -> List[str]:
-        
+        path = ['/'] + [p for p in path.split('/') if p]
+        node = self.root
+        for p in path:
+            if p not in node.children:
+                node.children[p] = Node()
+            node = node.children[p]
+        return [path[-1]] if node.content else node.children.keys()
 
     def mkdir(self, path: str) -> None:
-        
+        path = ['/'] + [p for p in path.split('/') if p]
+        node = self.root
+        for p in path:
+            if p not in node.children:
+                node.children[p] = Node()
+            node = node.children[p]
+        return node
 
-    def addContentToFile(self, filePath: str, content: str) -> None:
-        
+    def addContentToFile(self, path: str, content: str) -> None:
+        path = ['/'] + [p for p in path.split('/') if p]
+        node = self.root
+        for p in path:
+            if p not in node.children:
+                node.children[p] = Node()
+            node = node.children[p]
+        node.content += content
 
-    def readContentFromFile(self, filePath: str) -> str:
-        
+    def readContentFromFile(self, path: str) -> str:
+        path = ['/'] + [p for p in path.split('/') if p]
+        node = self.root
+        for p in path:
+            if p not in node.children:
+                return ""
+            node = node.children[p]
+        return node.content
 
 
 # Your FileSystem object will be instantiated and called as such:
