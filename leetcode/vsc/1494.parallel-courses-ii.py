52c7dcf060e86918398dbcbdc9ae6e180f3c96b7
mahsanghani <ghaniahsan@outlook.com>
2023-09-10 19:15:07 -0400

    parallel courses 2

--- leetcode/vsc/1494.parallel-courses-ii.py
+++ leetcode/vsc/1494.parallel-courses-ii.py
@@ -5,8 +5,29 @@
 #
 
 # @lc code=start
+from itertools import combinations
 class Solution:
+    @lru_cache(None)
+    def recurse(self, mask, indeg):
+        if not mask:
+            return 0
+        nodes = [i for i in range(self.n) if mask & 1 << i and indeg[i] == 0]
+        results = float("inf")
+
+        for k in combinations(nodes, min(self.k, len(nodes))):
+            new_mask = mask
+            new_indeg = list(indeg)
+
+            for c in k:
+                new_mask ^= 1<<c
+                for child in self.adj[c]:
+                    new_indeg -= 1
+
+            results = min(results, 1+self.recurse(new_mask, tuple(new_indeg)))
+        return results
+
     def minNumberOfSemesters(self, n: int, relations: List[List[int]], k: int) -> int:
-        
+
+
 # @lc code=end
 
