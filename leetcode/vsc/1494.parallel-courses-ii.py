6d19c41e4b657cadc9e0cd899acbbd8fdbf9052e
mahsanghani <ghaniahsan@outlook.com>
2023-09-10 18:50:51 -0400

    breadth first search parallel courses

---
+++ leetcode/vsc/1494.parallel-courses-ii.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1494 lang=python3
+#
+# [1494] Parallel Courses II
+#
+
+# @lc code=start
+class Solution:
+    def minNumberOfSemesters(self, n: int, relations: List[List[int]], k: int) -> int:
+        
+# @lc code=end
+
