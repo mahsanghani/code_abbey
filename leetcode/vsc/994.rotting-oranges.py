aab6464b6b987203fb224408cb21e0605a9e6c41
mahsanghani <ghaniahsan@outlook.com>
2023-09-10 22:23:56 -0400

    rotting oranges

--- leetcode/vsc/994.rotting-oranges.py
+++ leetcode/vsc/994.rotting-oranges.py
@@ -9,7 +9,11 @@ import collections
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        time,fresh,q,rows,cols = 0,0,deque(),len(grid),len(grid[0])
+        time = 0
+        fresh = 0
+        q = deque()
+        rows = len(grid)
+        cols = len(grid[0])
         directions = [[0,1],[1,0],[0,-1],[-1,0]]
 
         for r in range(rows):
@@ -23,8 +27,8 @@ class Solution:
             for i in range(len(q)):
                 r,c = q.popleft()
                 for dr,dc in directions:
-                    row = r+dr
-                    col = c+dc
+                    row=r+dr
+                    col=c+dc
                     if row<0 or col<0 or row==rows or col==cols or grid[row][col]!=1:
                         continue
                     grid[row][col]=2
