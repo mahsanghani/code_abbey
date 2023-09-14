58f8fadea11228a18c10afdf53b7f0b218b9947c
mahsanghani <ghaniahsan@outlook.com>
2023-09-14 07:57:00 -0400

    minimum window substring

--- leetcode/vsc/76.minimum-window-substring.py
+++ leetcode/vsc/76.minimum-window-substring.py
@@ -8,33 +8,40 @@
 from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if t=="":
+        if t == "":
             return ""
         
-        countT,window = {},{}
+        l = 0
+        countT = {}
+        window = {}
 
         for c in t:
-            countT[c] = 1 + countT.get(c,0)
+            countT[c] = countT.get(c,0) + 1
 
-        res, resLen, l, have, need = [-1,-1], float("inf"), 0, 0, len(countT)
+        have = 0
+        need = len(countT)
+        res = [-1,-1]
+        resLen = float("infinity")
 
         for r in range(len(s)):
             c = s[r]
-            window[c] = 1 + window.get(c,0)
-            
+            window[c] = window.get(c, 0) + 1
+        
             if c in countT and window[c] == countT[c]:
                 have += 1
             
             while have == need:
                 if (r-l+1) < resLen:
                     res = [l,r]
-                    resLen = (r-l+1)
+                    resLen = r-l+1
                 window[s[l]] -= 1
                 if s[l] in countT and window[s[l]] < countT[s[l]]:
                     have -= 1
                 l += 1
-        
+
         l,r = res
-        return s[l:r+1] if resLen!=float("inf") else ""
+        print(resLen)
+        return s[l:r+1] if resLen != float("infinity") else ""
+
 # @lc code=end
 
