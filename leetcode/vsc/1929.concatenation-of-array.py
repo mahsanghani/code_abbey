d280edad0ac853b2e1b2f24a4a150001d70f295f
mahsanghani <ghaniahsan@outlook.com>
2023-09-15 07:59:54 -0400

    concatenation of an array

--- leetcode/vsc/1929.concatenation-of-array.py
+++ leetcode/vsc/1929.concatenation-of-array.py
@@ -7,8 +7,6 @@
 # @lc code=start
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        ans = nums
-        ans.extend(nums)
-        return ans
+        return nums+nums
 # @lc code=end
 
