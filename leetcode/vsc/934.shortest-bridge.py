99fcf7e38bd36564b1a6f0e1a3fdb9a704125bcc
mahsanghani <ghaniahsan@outlook.com>
2023-09-17 13:13:07 -0400

    bfs shortest bridge

--- leetcode/vsc/934.shortest-bridge.py
+++ leetcode/vsc/934.shortest-bridge.py
@@ -17,31 +17,35 @@ class Solution:
                 if grid[i][j] == 1:
                     x,y = i,j
                     break
-
-        def dfs(x,y):
-            grid[x][y] = 2
-            bfs.append((x,y))
-            for x,y in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
-                if 0 <= x < n and 0 <= y < n and grid[x][y] == 1:
-                    dfs(x,y)
         
-        bfs = []
-        dist = 0
-        dfs(x,y)
-
-        while bfs:
+        bfs1 = [(x,y)]
+        bfs2 = [(x,y)]
+        grid[x][y] = 2
+        distance = 0
+        
+        while bfs1:
             new_bfs = []
-            for x,y in bfs:
+            for x,y in bfs1:
+                for i,j in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
+                    if 0 <= i < n and 0 <= j < n and grid[i][j] == 1:
+                        new_bfs.append((i,j))
+                        bfs2.append((i,j))
+                        grid[i][j] = 2
+            bfs1 = new_bfs
+
+        while bfs2:
+            new_bfs = []
+            for x,y in bfs2:
                 for i,j in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
                     if 0 <= i < n and 0 <= j < n:
                         if grid[i][j] == 1:
-                            return dist
+                            return distance
                         elif grid[i][j] == 0:
                             new_bfs.append((i,j))
                             grid[i][j] = -1
-            
-            bfs = new_bfs
-            dist += 1
+
+            bfs2 = new_bfs
+            distance += 1
                 
 # @lc code=end
 
