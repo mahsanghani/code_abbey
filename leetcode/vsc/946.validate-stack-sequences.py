d3411d8a7123d9347a81767d18d2245ec556cb37
mahsanghani <ghaniahsan@outlook.com>
2023-09-18 19:15:39 -0400

    validate stack sequences

---
+++ leetcode/vsc/946.validate-stack-sequences.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=946 lang=python3
+#
+# [946] Validate Stack Sequences
+#
+
+# @lc code=start
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        i = 0
+        stack = []
+        for n in pushed:
+            stack.append(n)
+            while stack and i < len(popped) and popped[i] == stack[-1]:
+                i += 1
+                stack.pop()
+        return not stack
+
+# @lc code=end
+
