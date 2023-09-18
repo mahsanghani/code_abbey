a9e288cf07d9a17572755eb7c4bfae2c4ec22359
mahsanghani <ghaniahsan@outlook.com>
2023-09-18 19:41:23 -0400

    generate parenthesis

---
+++ leetcode/vsc/22.generate-parentheses.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=22 lang=python3
+#
+# [22] Generate Parentheses
+#
+
+# @lc code=start
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        results = []
+
+        def bt(o,c):
+            if o==c==n:
+                results.append(''.join(stack))
+                return
+            
+            if o<n:
+                stack.append('(')
+                bt(o+1,c)
+                stack.pop()
+
+            if c<o:
+                stack.append(')')
+                bt(o,c+1)
+                stack.pop()
+
+        bt(0,0)
+        return results
+# @lc code=end
+
