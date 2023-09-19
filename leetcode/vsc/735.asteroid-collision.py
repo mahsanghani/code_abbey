6a8bf691445ca46625b466aabc0ecb1a39b739af
mahsanghani <ghaniahsan@outlook.com>
2023-09-18 20:30:56 -0400

    asteroid collision

---
+++ leetcode/vsc/735.asteroid-collision.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=735 lang=python3
+#
+# [735] Asteroid Collision
+#
+
+# @lc code=start
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+
+        for a in asteroids:
+            while stack and a<0 and stack[-1]>0:
+                delta = a+stack[-1]
+                if delta<0:
+                    stack.pop()
+                elif delta>0:
+                    a=0
+                else:
+                    a=0
+                    stack.pop()
+            if a:
+                stack.append(a)
+
+        return stack
+
+# @lc code=end
+
