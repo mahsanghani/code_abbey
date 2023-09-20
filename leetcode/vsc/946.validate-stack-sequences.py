6a62b5ff6aec6968663f0bd2ecf60d4b42ba036b
mahsanghani <ghaniahsan@outlook.com>
2023-09-19 20:32:19 -0400

    validate stack sequences

--- leetcode/vsc/946.validate-stack-sequences.py
+++ leetcode/vsc/946.validate-stack-sequences.py
@@ -7,13 +7,13 @@
 # @lc code=start
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
-        i = 0
-        stack = []
+        i=0
+        stack=[]
         for n in pushed:
             stack.append(n)
-            while stack and i < len(popped) and popped[i] == stack[-1]:
+            while stack and i<len(popped) and popped[i]==stack[-1]:
+                i+=1
                 stack.pop()
-                i += 1
         return not stack
 
 # @lc code=end
