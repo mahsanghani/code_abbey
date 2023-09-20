e65c364c5f1b184bce85573b1fa930fad963d063
mahsanghani <ghaniahsan@outlook.com>
2023-09-20 19:25:19 -0400

    car fleet

---
+++ leetcode/vsc/853.car-fleet.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=853 lang=python3
+#
+# [853] Car Fleet
+#
+
+# @lc code=start
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        stack = []
+        pairs = [[p,s] for p,s in zip(position,speed)]
+
+        for p,s in sorted(pairs)[::-1]:
+            stack.append((target-p)/s)
+            if len(stack)>1 and stack[-1]<=stack[-2]:
+                stack.pop()
+        return len(stack)
+# @lc code=end
+
