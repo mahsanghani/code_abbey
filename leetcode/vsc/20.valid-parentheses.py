ebbe655e4d2d8586cf5f296b35205a51d33dd683
mahsanghani <ghaniahsan@outlook.com>
2023-09-22 16:20:12 -0400

    valid parentheses

--- leetcode/vsc/20.valid-parentheses.py
+++ leetcode/vsc/20.valid-parentheses.py
@@ -8,16 +8,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        braces = {')':'(', ']':'[', '}':'{'}
+        d = {'}':'{', ']':'[', ')':'('}
 
         for c in s:
-            if c in braces:
-                if stack and stack[-1]==braces[c]:
+            if c in d:
+                if stack and stack[-1]==d[c]:
                     stack.pop()
                 else:
                     return False
             else:
                 stack.append(c)
+        
         return True if not stack else False
 # @lc code=end
 
