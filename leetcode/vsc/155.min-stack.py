f804fcbf8081fe3bb4d0831589df259ac23387f5
mahsanghani <ghaniahsan@outlook.com>
2023-09-22 18:00:55 -0400

    design min stack

--- leetcode/vsc/155.min-stack.py
+++ leetcode/vsc/155.min-stack.py
@@ -9,23 +9,22 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
+        self.minst = []
 
     def push(self, val: int) -> None:
-        if not self.stack:
-            self.stack.append((val, val))
-            return
-        
-        current = self.stack[-1][1]
-        self.stack.append((val, min(val, current)))
+        self.stack.append(val)
+        val = min(val, self.minst[-1] if self.minst else val)
+        self.minst.append(val)
 
     def pop(self) -> None:
         self.stack.pop()
+        self.minst.pop()
 
     def top(self) -> int:
-        return self.stack[-1][0]
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.stack[-1][1]
+        return self.minst[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
