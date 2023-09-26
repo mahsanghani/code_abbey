ac6cd1894a0f4f9acba8221d63d19ee9602f6dab
mahsanghani <ghaniahsan@outlook.com>
2023-09-26 13:25:55 -0400

    remove k digits

---
+++ leetcode/vsc/402.remove-k-digits.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=402 lang=python3
+#
+# [402] Remove K Digits
+#
+
+# @lc code=start
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stack = []
+
+        for c in num:
+            while stack and k>0 and c<stack[-1]:
+                k-=1
+                stack.pop()
+
+            stack.append(c)
+# @lc code=end
+
