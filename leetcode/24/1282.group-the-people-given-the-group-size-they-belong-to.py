10c1c4188555b0d29c42be5f46feb4c5cf8ef731
mahsanghani <ghaniahsan@outlook.com>
2023-11-25 09:20:56 -0500

    group the people given the group size they belong to

---
+++ leetcode/24/1282.group-the-people-given-the-group-size-they-belong-to.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=1282 lang=python3
+#
+# [1282] Group the People Given the Group Size They Belong To
+#
+
+# @lc code=start
+from collections import Counter
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        results = []
+        counter = Counter()
+        for i,j in enumerate(groupSizes):
+            if j in counter:
+                counter[j].append(i)
+            else:
+                counter[j] = [i]
+        for k,v in counter.items():
+            while len(v)>k:
+                results.append(v[:k])
+                counter[k] = v[k:]
+            results.append(v)
+        print(results)
+        return results
+# @lc code=end
+
