59534dfd8640898e5271052181131a02c21cc135
mahsanghani <ghaniahsan@outlook.com>
2023-11-25 10:13:59 -0500

    running sum of 1d array

--- leetcode/24/1480.running-sum-of-1-d-array.py
+++ leetcode/24/1480.running-sum-of-1-d-array.py
@@ -7,8 +7,9 @@
 # @lc code=start
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        for i in range(1,len(nums)-1):
-            nums[i]+=nums[i-1]
-        return nums[-1]
+        results = nums[:1]
+        for i in range(1,len(nums)):
+            results.append(results[-1]+nums[i])
+        return results
 # @lc code=end
 
