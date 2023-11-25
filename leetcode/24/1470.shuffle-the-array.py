2067ada724a174028a12398221025187eb5a86dc
mahsanghani <ghaniahsan@outlook.com>
2023-11-25 09:00:53 -0500

    shuffle the array

---
+++ leetcode/24/1470.shuffle-the-array.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=1470 lang=python3
+#
+# [1470] Shuffle the Array
+#
+
+# @lc code=start
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        a = nums[:n]
+        b = nums[n:]
+        c = []
+        for i in range(len(b)):
+            c.append(a[i])
+            c.append(b[i])
+        return c
+# @lc code=end
+
