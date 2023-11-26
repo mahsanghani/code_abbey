6f107e26b7182b116c075777fa98e77fda5272d3
mahsanghani <ghaniahsan@outlook.com>
2023-11-26 11:20:52 -0500

    encode decode

---
+++ leetcode/24/535.encode-and-decode-tiny-url.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=535 lang=python3
+#
+# [535] Encode and Decode TinyURL
+#
+
+# @lc code=start
+class Codec:
+
+    def encode(self, longUrl: str) -> str:
+        """Encodes a URL to a shortened URL.
+        """
+        return longUrl
+
+    def decode(self, shortUrl: str) -> str:
+        """Decodes a shortened URL to its original URL.
+        """
+        return shortUrl
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
+# @lc code=end
+
