2c3280ec1d654325321eb698350e10395fac1f6f
mahsanghani <ghaniahsan@outlook.com>
2023-11-26 08:08:11 -0500

    strictly palindromic number

---
+++ leetcode/24/2396.strictly-palindromic-number.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=2396 lang=python3
+#
+# [2396] Strictly Palindromic Number
+#
+
+# @lc code=start
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        if len(str(n))<2:
+            return False
+        else:
+            if str(n)==str(n)[::-1]:
+                return False
+# @lc code=end
+
