b73f2a87b9d7d82d8569f293a3249462349bd933
mahsanghani <ghaniahsan@outlook.com>
2023-11-26 07:39:40 -0500

    MySQL

---
+++ leetcode/24/1303.find-the-team-size.py
@@ -0,0 +1,10 @@
+#
+# @lc app=leetcode id=1303 lang=SQL
+#
+# [1303] Find the Team Size
+#
+# @lc code=start
+# Write your MySQL query statement below
+select employee_id, count(employee_id) over (partition by team_id) as team_size from Employee
+# @lc code=end
+
