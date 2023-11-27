b34879c6f0afa00492906098ddb8285ebe4bbe85
mahsanghani <ghaniahsan@outlook.com>
2023-11-27 08:56:32 -0500

    allow one function call

--- leetcode/24/2666.allow-one-function-call.py
+++ leetcode/24/2666.allow-one-function-call.py
@@ -5,27 +5,27 @@
 #
 
 # @lc code=start
-/**
- * @param {Function} fn
- * @return {Function}
- */
+# 
+# @param {Function} fn
+# @return {Function}
+#
 var once = function(fn) {
  let hasBeenCalled = False;
  return function(...args){
   if (!hasBeenCalled){
       hasBeenCalled = True;
-      return fn(...args);
+      return fn.apply(this, args);
   }
  }
 };
 
-/**
- * let fn = (a,b,c) => (a + b + c)
- * let onceFn = once(fn)
- *
- * onceFn(1,2,3); // 6
- * onceFn(2,3,6); // returns undefined without calling fn
- */
+#
+# let fn = (a,b,c) => (a + b + c)
+# let onceFn = once(fn)
+#
+# onceFn(1,2,3); // 6
+# onceFn(2,3,6); // returns undefined without calling fn
+#
 
 # @lc code=end
 
