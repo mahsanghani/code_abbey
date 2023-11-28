596b36231112b9e507c778123c82e0a4cc787baa
mahsanghani <ghaniahsan@outlook.com>
2023-11-28 11:31:29 -0500

    create target in the given order

---
+++ leetcode/24/1389.create-target-array-in-the-given-order.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=1389 lang=python3
+#
+# [1389] Create Target Array in the Given Order
+#
+
+# @lc code=start
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        results = []
+
+        for i in range(len(nums)):
+            results.insert(index[i],nums[i])
+
+        return results
+# @lc code=end
+
