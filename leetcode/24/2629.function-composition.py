2eaacbaf6ce3c98ee67c3203fdcf520fec8653f6
mahsanghani <ghaniahsan@outlook.com>
2023-11-28 13:48:43 -0500

    function composition

---
+++ leetcode/24/2629.function-composition.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=2629 lang=JavaScript
+#
+# [2629] Function Composition
+#
+
+# @lc code=start
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function (functions) {
+  return function (x) {
+    if (functions.length === 0) return x;
+    let input = x;
+
+    for (const func of functions.reverse()) {
+      input = func(input);
+    }
+
+    return input;
+  };
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
+# @lc code=end
+
