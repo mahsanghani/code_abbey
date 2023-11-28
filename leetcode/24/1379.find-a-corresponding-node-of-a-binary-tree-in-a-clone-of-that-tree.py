1d906a11d14fa452bc4f13108e33d609c130d924
mahsanghani <ghaniahsan@outlook.com>
2023-11-27 20:46:28 -0500

    DFS iterative inorder traversal

--- leetcode/24/1379.find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
+++ leetcode/24/1379.find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -14,13 +14,25 @@ class TreeNode:
 
 class Solution:
     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
-        def preorder(original: TreeNode, cloned: TreeNode):
-            if original:
-                if original is target:
-                    self.results = cloned
-                preorder(original.left, cloned.left)
-                preorder(original.right, cloned.right)
-        preorder(original,cloned)
-        return self.results
-# @lc code=end
+        stack_original = []
+        stack_cloned = []
+        node_original = original
+        node_cloned = cloned
+
+        while stack_original or node_cloned:
+            while node_original:
+                stack_original.append(node_original)
+                stack_cloned.append(node_cloned)
+                
+                node_original = node_original.left
+                node_cloned = node_cloned.left
+
+            node_original = stack_original.pop()
+            node_cloned = stack_cloned.pop()
+
+            if node_original is target:
+                return node_cloned
+
+            node_original = node_original.right
+            node_cloned = node_cloned.right
 
