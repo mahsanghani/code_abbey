182eb34b3d83708951a90185b36bfb86c085170e
mahsanghani <ghaniahsan@outlook.com>
2023-11-27 20:41:21 -0500

    preorder traversal get target copy

--- leetcode/24/1379.find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
+++ leetcode/24/1379.find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -6,21 +6,21 @@
 
 # @lc code=start
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
-        def inorder(original: TreeNode, cloned: TreeNode):
+        def preorder(original: TreeNode, cloned: TreeNode):
             if original:
-                inorder(original.left, cloned.left)
                 if original is target:
                     self.results = cloned
-                inorder(original.right, cloned.right)
-        inorder(original,cloned)
+                preorder(original.left, cloned.left)
+                preorder(original.right, cloned.right)
+        preorder(original,cloned)
         return self.results
 # @lc code=end
 
