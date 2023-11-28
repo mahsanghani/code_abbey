6f3998672678ec69fbf100e1e7830512d40c64aa
mahsanghani <ghaniahsan@outlook.com>
2023-11-27 20:37:09 -0500

    inorder traversal get target copy

---
+++ leetcode/24/1379.find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=1379 lang=python3
+#
+# [1379] Find a Corresponding Node of a Binary Tree in a Clone of That Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+        def inorder(original: TreeNode, cloned: TreeNode):
+            if original:
+                inorder(original.left, cloned.left)
+                inorder(original.right, cloned.right)
+                if original is target:
+                    self.results = cloned
+        
+        inorder(original,cloned)
+        return self.results
+# @lc code=end
+
