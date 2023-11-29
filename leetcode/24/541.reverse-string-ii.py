7f258ad44525b18146b145ddec1eb763f046fb68
mahsanghani <ghaniahsan@outlook.com>
2023-11-29 14:05:30 -0500

    reverse string 2

--- leetcode/24/541.reverse-string-ii.py
+++ leetcode/24/541.reverse-string-ii.py
@@ -10,6 +10,6 @@ class Solution:
         s = list(s)
         for i in range(0, len(s), 2*k):
             s[i:i+k] = reversed(s[i:i+k])
-        return ''.join(s)
+        return "".join(s)
 # @lc code=end
 
