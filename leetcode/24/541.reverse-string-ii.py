4ce8449ffa63cc9905e661480069fe4a5ed3f724
mahsanghani <ghaniahsan@outlook.com>
2023-11-29 14:03:28 -0500

    reverse string 2

---
+++ leetcode/24/541.reverse-string-ii.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=541 lang=python3
+#
+# [541] Reverse String II
+#
+
+# @lc code=start
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        s = list(s)
+        for i in range(0, len(s), 2*k):
+            s[i:i+k] = reversed(s[i:i+k])
+        return ''.join(s)
+# @lc code=end
+
