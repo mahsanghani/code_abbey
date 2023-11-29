3e3f7a985dbb6c3c097f61c01702dcd9c4162fa5
mahsanghani <ghaniahsan@outlook.com>
2023-11-29 17:22:16 -0500

    product's price for each store

---
+++ leetcode/24/1777.product's-price-for-each-store.py
@@ -0,0 +1,10 @@
+#
+# @lc app=leetcode id=1350 lang=MySQL
+#
+# [1350] Students With Invalid Departments
+#
+# @lc code=start
+# Write your MySQL query statement below
+select s.id, s.name from students as s left join departments d on s.department_id = d.id where d.id is NULL
+# @lc code=end
+
