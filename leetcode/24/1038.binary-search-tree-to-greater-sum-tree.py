7bcdcf9efa5aaf610dc128a5b28142de0100f574
mahsanghani <ghaniahsan@outlook.com>
2023-12-03 11:13:54 -0500

    inorder traversal update value

--- leetcode/24/1038.binary-search-tree-to-greater-sum-tree.py
+++ leetcode/24/1038.binary-search-tree-to-greater-sum-tree.py
@@ -13,6 +13,25 @@ class TreeNode:
         self.right = right
 class Solution:
     def bstToGst(self, root: TreeNode) -> TreeNode:
+        nodes = []
+
+        def inorderTraversal(node):
+            nonlocal nodes
+            if node:
+                inorderTraversal(node.left)
+                nodes.append(node.val)
+                inorderTraversal(node.right)
         
+        inorderTraversal(root)
+
+        def updateValue(node):
+            nonlocal nodes
+            if node:
+                updateValue(node.left)
+                node.val = sum(nodes[node.val:])
+                updateValue(node.right)
+
+        updateValue(root)
+        return root
 # @lc code=end
 
