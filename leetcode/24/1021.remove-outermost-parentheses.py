5f2352679632bdc004a5f0cbd539b6fd07dcc4d1
mahsanghani <ghaniahsan@outlook.com>
2023-12-03 11:58:59 -0500

    remove outermost parentheses

--- leetcode/24/1021.remove-outermost-parentheses.py
+++ leetcode/24/1021.remove-outermost-parentheses.py
@@ -7,6 +7,21 @@ ur#
 # @lc code=start
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
-        
+        count = 0
+        results = ''
+        subarray = []
+
+        for i in range(len(s)):
+            if s[i] == '(':
+                count += 1
+                subarray.append(s[i])
+            if s[i] == ')':
+                count -= 1
+                subarray.append(s[i])
+                print(subarray)
+                if count == 0:
+                    results += ''.join(subarray[1:-1])
+                    subarray = []
+        return results
 # @lc code=end
 
