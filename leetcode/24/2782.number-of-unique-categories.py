aa4b9fb3d6ff5640ca5a9be6d55921de7d036e18
mahsanghani <ghaniahsan@outlook.com>
2023-12-10 14:27:10 -0500

    number of unique categories

--- leetcode/24/2782.number-of-unique-categories.py
+++ leetcode/24/2782.number-of-unique-categories.py
@@ -8,7 +8,8 @@
 # Definition for a category handler.
 class CategoryHandler:
     def haveSameCategory(self, a: int, b: int) -> bool:
-        pass
+        return a==b
+
 class Solution:
     def numberOfCategories(self, n: int, categoryHandler: Optional['CategoryHandler']) -> int:
         unique = []
