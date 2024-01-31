b4754d672d9367e76fdd06352530cc3d93afb280
mahsanghani <ghaniahsan@outlook.com>
2024-01-31 16:55:58 -0500

    nested list weight sum

--- leetcode/24/339.nested-list-weight-sum.py
+++ leetcode/24/339.nested-list-weight-sum.py
@@ -1,18 +1,63 @@
 #
-# @lc app=leetcode id=2798 lang=python3
+# @lc app=leetcode id=339 lang=python3
 #
-# [2798] Number of Employees Who Met the Target
+# [339] Nested List Weight Sum
 #
 
 # @lc code=start
-class Solution:
-    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
-        hours = sorted(hours)
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
 
-        for i,j in enumerate(hours):
-            if j>=target:
-                return len(hours)-i
-            
-        return 0
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        def dfs(nestedList, depth):
+            total = 0
+            for nested in nestedList:
+                if nested.isInteger():
+                    total += nested.getInteger() * depth
+                else:
+                    total += dfs(nested.getList(), depth+1)
+            return total
+        return dfs(nestedList,1)
 # @lc code=end
 
