17d64c24c225c86cb0403a42d89fabbab6c786ac
mahsanghani <ghaniahsan@outlook.com>
2024-01-31 17:16:40 -0500

    split BST

---
+++ leetcode/24/776.split-bst.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=776 lang=python3
+#
+# [776] Split BST
+#
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def splitBST(self, root: Optional[TreeNode], target: int) -> List[Optional[TreeNode]]:
+        
+# @lc code=end
+
