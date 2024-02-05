77e91f38f240ba1d8d50412700db2ab952b17b18
mahsanghani <ghaniahsan@outlook.com>
2024-02-05 14:04:58 -0500

    all possible full binary trees

--- leetcode/24/1628.design-an-expression-tree-with-evaluate-function.py
+++ leetcode/24/1628.design-an-expression-tree-with-evaluate-function.py
@@ -1,18 +1,40 @@
 #
-# @lc app=leetcode id=2331 lang=python3
+# @lc app=leetcode id=1628 lang=python3
 #
-# [2331] Evaluate Boolean Binary Tree
+# [1628] Design an Expression Tree With Evaluate Function
 #
 
 # @lc code=start
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+import abc 
+from abc import ABC, abstractmethod 
+"""
+This is the interface for the expression tree Node.
+You should not remove it, and you can define some classes to implement it.
+"""
+
+class Node(ABC):
+    @abstractmethod
+    # define your fields here
+    def evaluate(self) -> int:
+        pass
+
+
+"""    
+This is the TreeBuilder class.
+You can treat it as the driver code that takes the postinfix input
+and returns the expression tree represnting it as a Node.
+"""
+
+class TreeBuilder(object):
+    def buildTree(self, postfix: List[str]) -> 'Node':
+        
+		
+"""
+Your TreeBuilder object will be instantiated and called as such:
+obj = TreeBuilder();
+expTree = obj.buildTree(postfix);
+ans = expTree.evaluate();
+"""
         
 # @lc code=end
 
--- leetcode/24/894.all-possible-full-binary-trees.py
+++ leetcode/24/894.all-possible-full-binary-trees.py
@@ -6,13 +6,28 @@
 
 # @lc code=start
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+        if n%2==0:
+            return []
+        if n==1:
+            return [TreeNode()]
         
+        results = []
+        for i in range(1,n,2):
+            left = self.allPossibleFBT(i)
+            right = self.allPossibleFBT(n-i-1)
+
+            for l in left:
+                for r in right:
+                    root = TreeNode(0,l,r)
+                    results.append(root)
+
+        return results
 # @lc code=end
 
