69a06f61d7a77cd924d92f9fcba84e2d187c901b
mahsanghani <ghaniahsan@outlook.com>
2024-02-05 13:16:53 -0500

    evaluate boolean binary tree

--- leetcode/24/2331.evaluate-boolean-binary-tree.py
+++ leetcode/24/2331.evaluate-boolean-binary-tree.py
@@ -13,6 +13,26 @@
 #         self.right = right
 class Solution:
     def evaluateTree(self, root: Optional[TreeNode]) -> bool:
-        
+        # 0 = False
+        # 1 = True
+        # 2 = OR
+        # 3 = AND
+        # DFS in post-order
+
+        def dfs(node):
+            if not node:
+                return True
+            if node.left not in [2,3]:
+                dfs(node.left)
+            if node.right not in [2,3]:
+                dfs(node.right)
+            if node.val == 2:
+                print(node.left.val or node.right.val)
+                return (node.left.val or node.right.val)
+            if node.val == 3:
+                print(node.left.val and node.right.val)
+                return (node.left.val and node.right.val)
+            return True
+        dfs(root)
 # @lc code=end
 
