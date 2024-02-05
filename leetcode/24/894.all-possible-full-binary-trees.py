3ff2269f8b1808673f058b700dddb39857709284
mahsanghani <ghaniahsan@outlook.com>
2024-02-05 12:34:02 -0500

    all possible full binary trees

---
+++ leetcode/24/894.all-possible-full-binary-trees.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=894 lang=python3
+#
+# [894] All Possible Full Binary Trees
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+        
+# @lc code=end
+
