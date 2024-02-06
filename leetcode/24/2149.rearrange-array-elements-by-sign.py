09ad5af507ababd134bf5ca0e10ad6f64e291b20
mahsanghani <ghaniahsan@outlook.com>
2024-02-06 18:06:18 -0500

    rearrange array elements by sign

---
+++ leetcode/24/2149.rearrange-array-elements-by-sign.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=2149 lang=python3
+#
+# [2149] Rearrange Array Elements by Sign
+#
+
+# @lc code=start
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        pos = []
+        neg = []
+        res = []
+
+        return res
+# @lc code=end
+
