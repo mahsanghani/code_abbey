06cede90793c7274cf4c1ace85018095ce5d93f5
mahsanghani <ghaniahsan@outlook.com>
2024-02-06 18:10:48 -0500

    pos neg

--- leetcode/24/2149.rearrange-array-elements-by-sign.py
+++ leetcode/24/2149.rearrange-array-elements-by-sign.py
@@ -7,8 +7,8 @@
 # @lc code=start
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        pos = []
-        neg = []
+        pos = [num for num in nums if num>0]
+        neg = [num for num in nums if num<0]
         res = []
 
         return res
