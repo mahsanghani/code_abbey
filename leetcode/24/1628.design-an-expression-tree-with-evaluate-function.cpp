863ac410b7c9b33e2216f2baf8a70176efd1788e
mahsanghani <ghaniahsan@outlook.com>
2024-02-06 11:07:42 -0500

    design an expression tree with evaluate function

--- leetcode/24/1628.design-an-expression-tree-with-evaluate-function.cpp
+++ leetcode/24/1628.design-an-expression-tree-with-evaluate-function.cpp
@@ -1,60 +1,43 @@
-#
-# @lc app=leetcode id=1628 lang=python3
-#
-# [1628] Design an Expression Tree With Evaluate Function
-#
+//
+// @lc app=leetcode id=1628 lang=cpp
+//
+// [1628] Design an Expression Tree With Evaluate Function
+//
 
-# @lc code=start
-import abc 
-from abc import ABC, abstractmethod 
-"""
-This is the interface for the expression tree Node.
-You should not remove it, and you can define some classes to implement it.
-"""
+// @lc code=start
+/**
+ * This is the interface for the expression tree Node.
+ * You should not remove it, and you can define some classes to implement it.
+ */
 
-class Node(ABC):
-    @abstractmethod
-    # define your fields here
-    def evaluate(self) -> int:
-        pass
+class Node {
+public:
+    virtual ~Node () {};
+    virtual int evaluate() const = 0;
+protected:
+    // define your fields here
+};
 
-class TreeNode(Node):
-    def __init__(self,val):
-        self.val = val
-        self.left = None
-        self.right = None
 
-    def evaluate(self) -> int:
-        if self.val.isdigit():
-            return self.val
-        else:
-            results = eval('int(self.left.evaluate())'+self.val+'int(self.right.evaluate())')
-            return int(results)
+/**
+ * This is the TreeBuilder class.
+ * You can treat it as the driver code that takes the postinfix input 
+ * and returns the expression tree represnting it as a Node.
+ */
 
-"""    
-This is the TreeBuilder class.
-You can treat it as the driver code that takes the postinfix input
-and returns the expression tree represnting it as a Node.
-"""
-
-class TreeBuilder(object):
-    def buildTree(self, postfix: List[str]) -> 'Node':
-        stack = []
-        current = None
-        for c in postfix:
-            current = TreeNode(c)
-            if not c.isdigit():
-                current.right = stack.pop()
-                current.left = stack.pop()
-            stack.append(current)
-        return current
-		
-"""
-Your TreeBuilder object will be instantiated and called as such:
-obj = TreeBuilder();
-expTree = obj.buildTree(postfix);
-ans = expTree.evaluate();
-"""
+class TreeBuilder {
+public:
+    Node* buildTree(vector<string>& postfix) {
         
-# @lc code=end
+    }
+};
+
+
+/**
+ * Your TreeBuilder object will be instantiated and called as such:
+ * TreeBuilder* obj = new TreeBuilder();
+ * Node* expTree = obj->buildTree(postfix);
+ * int ans = expTree->evaluate();
+ */
+// @lc code=end
 
