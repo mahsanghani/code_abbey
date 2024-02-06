0b60247e32ef4999d851a7cdb20c0749712a2643
mahsanghani <ghaniahsan@outlook.com>
2024-02-06 18:53:56 -0500

    count asterisks

--- leetcode/24/2315.count-asterisks.py
+++ leetcode/24/2315.count-asterisks.py
@@ -7,6 +7,15 @@
 # @lc code=start
 class Solution:
     def countAsterisks(self, s: str) -> int:
-        
+        count, results = 0, 0
+
+        for i in range(len(s)):
+            if s[i]=='|':
+                count+=1
+            if count%2==0 and s[i]=='*':
+                results+=1
+            i+=1
+
+        return results
 # @lc code=end
 
