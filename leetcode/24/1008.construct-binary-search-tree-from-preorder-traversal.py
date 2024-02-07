cb6223c8f2770b3653ea9ca8a9793869534e52da
mahsanghani <ghaniahsan@outlook.com>
2024-02-07 10:38:53 -0500

    node root low high left right

--- leetcode/24/1008.construct-binary-search-tree-from-preorder-traversal.py
+++ leetcode/24/1008.construct-binary-search-tree-from-preorder-traversal.py
@@ -13,23 +13,13 @@ class TreeNode:
         self.right = right
 class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
-        n = len(preorder)
-        if not n:
-            return None
-        
-        root = TreeNode(preorder[0])
-        stack = [root, ]
+        node = preorder[0]
+        root = TreeNode(node)
+        low = 
+        high = 
 
-        for i in range(1,n):
-            node, child = stack[-1], TreeNode(preorder[i])
-            while stack and stack[-1].val < child.val:
-                node = stack.pop()
-
-            if node.val < child.val:
-                node.right = child
-            else:
-                node.left = child
-            stack.append(child)
+        root.left = 
+        root.right = 
 
         return root
 # @lc code=end
