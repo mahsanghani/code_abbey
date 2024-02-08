e0af05ea02310c1a56b0f249efab28d9b6db36ce
mahsanghani <ghaniahsan@outlook.com>
2024-02-08 14:35:13 -0500

    minimize maximum pair sum in array

---
+++ leetcode/24/1877.minimize-maximum-pair-sum-in-array.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=1877 lang=python3
+#
+# [1877] Minimize Maximum Pair Sum in Array
+#
+
+# @lc code=start
+class Solution:
+    def minPairSum(self, nums: List[int]) -> int:
+        sums = []
+        pairs = []
+        nums.sort()
+        for i in range(len(nums)//2):
+            pairs.append([nums[i],nums[-i-1]])
+            print([nums[i],nums[-i-1]])
+        for j in range(len(pairs)):
+            sums.append(sum(pairs[j]))
+        return max(sums)
+# @lc code=end
+
