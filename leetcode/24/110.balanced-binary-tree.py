fd86de3d164ebec33336aa2926adb821418c5f3c
mahsanghani <ghaniahsan@outlook.com>
2024-02-09 16:34:37 -0500

    balanced binary tree

--- leetcode/24/110.balanced-binary-tree.py
+++ leetcode/24/110.balanced-binary-tree.py
@@ -13,16 +13,14 @@ class TreeNode:
         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def dfs(root):
-            if not root:
-                return [True, 0]
+        def dfs(node):
+            if not node:
+                return [True,0]
             
-            left = dfs(root.left)
-            right = dfs(root.right)
-            balanced = (left[0] and right[0] and abs(left[1]-right[1]) <= 1)
+            left, right = dfs(node.left), dfs(node.right)
+            balanced = left[0] and right[0] and abs(left[1]-right[1])<2
 
-            return [balanced, 1+max(left[1], right[1])]
-        
+            return [balanced, 1+max(left[1],right[1])]
         return dfs(root)[0]
 # @lc code=end
 
