051a77a1330d46aef5bbc27748165115a69deb4a
mahsanghani <ghaniahsan@outlook.com>
2024-02-09 16:54:07 -0500

    balance a binary search tree

--- leetcode/24/1382.balance-a-binary-search-tree.py
+++ leetcode/24/1382.balance-a-binary-search-tree.py
@@ -6,13 +6,31 @@
 
 # @lc code=start
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
+    def dfs(node):
+        if not node:
+            return None
+        
+        mid = len(node)//2
+        root = TreeNode(node[mid])
+        root.left = self.dfs(node[:mid])
+        root.right = self.dfs(node[mid+1:])
+        
+        return root
+
     def balanceBST(self, root: TreeNode) -> TreeNode:
+        results = []
+        if root:
+            self.balanceBST(root.left)
+            results.append(root.val)
+            self.balanceBST(root.right)
+
+        # return results
         
 # @lc code=end
 
