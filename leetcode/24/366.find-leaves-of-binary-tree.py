dffdb75f902fe4f9fed1481be74546c02b535e98
mahsanghani <ghaniahsan@outlook.com>
2024-02-11 12:30:17 -0500

    dfs leaves binary tree

--- leetcode/24/366.find-leaves-of-binary-tree.py
+++ leetcode/24/366.find-leaves-of-binary-tree.py
@@ -13,18 +13,16 @@ class TreeNode:
         self.right = right
 class Solution:
     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
-        results = []
-
         def dfs(node):
             if not node:
                 return -1
-            
-            height = 1+max(dfs(node.left), dfs(node.right))
-            if height == len(results):
+            height = 1+max(dfs(node.left),dfs(node.right))
+            if height==len(results):
                 results.append([])
             results[height].append(node.val)
             return height
         
+        results = []
         dfs(root)
         return results
 # @lc code=end
