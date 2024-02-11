370b0d50ca3e79feb8a106c61338bfd87f1d688f
mahsanghani <ghaniahsan@outlook.com>
2024-02-11 10:33:52 -0500

    convert binary number in a linked list

--- leetcode/24/1290.convert-binary-number-in-a-linked-list-to-integer.py
+++ leetcode/24/1290.convert-binary-number-in-a-linked-list-to-integer.py
@@ -14,7 +14,7 @@ class Solution:
     def getDecimalValue(self, head: ListNode) -> int:
         num = head.val
         while head.next:
-            num = (num<<1) | head.next.val
+            num = num*2 + head.next.val
             head = head.next
         return num
 # @lc code=end
