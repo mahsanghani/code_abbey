05e0ab686277b0c32a42c48cfea8974ebd19245c
mahsanghani <ghaniahsan@outlook.com>
2024-02-11 09:36:58 -0500

    integer and triplets

---
+++ leetcode/24/1534.count-good-triplets.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1534 lang=python3
+#
+# [1534] Count Good Triplets
+#
+
+# @lc code=start
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        
+# @lc code=end
+
