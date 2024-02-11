6d913be62bd9d5c213057402605bf1806e738b56
mahsanghani <ghaniahsan@outlook.com>
2024-02-11 10:16:34 -0500

    count good triplets

--- leetcode/24/1534.count-good-triplets.py
+++ leetcode/24/1534.count-good-triplets.py
@@ -7,6 +7,17 @@
 # @lc code=start
 class Solution:
     def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
-        
+        results = []
+        for i in range(len(arr)-2):
+            for j in range(1,len(arr)-1):
+                for k in range(2,len(arr)):
+                    if i<j<k \
+                    and abs(arr[i]-arr[j])<=a \
+                    and abs(arr[j]-arr[k])<=b \
+                    and abs(arr[i]-arr[k])<=c:
+                        results.append((arr[i],arr[j],arr[k]))
+                        continue
+        # print(results)
+        return len(results)
 # @lc code=end
 
