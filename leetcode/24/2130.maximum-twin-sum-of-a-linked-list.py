78088e88cb2458aeffaa105d17f0f4bb46cf0532
mahsanghani <ghaniahsan@outlook.com>
2024-02-11 10:53:50 -0500

    max twin sum of LL

--- leetcode/24/2130.maximum-twin-sum-of-a-linked-list.py
+++ leetcode/24/2130.maximum-twin-sum-of-a-linked-list.py
@@ -6,12 +6,37 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        
+        fast = head
+        slow = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        prev = None
+        curr = slow
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        res = 0
+        left = head
+        right = prev
+
+        while right:
+            res = max(res, right.val+left.val)
+            left = left.next
+            right = right.next
+
+        return res
 # @lc code=end
 
