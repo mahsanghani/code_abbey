5d92f562caf9006f06f5879eea1ed2928bccd4ef
mahsanghani <ghaniahsan@outlook.com>
2024-02-11 11:15:28 -0500

    find leaves of binary tree

---
+++ leetcode/24/1366.rank-teams-by-votes.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1366 lang=python3
+#
+# [1366] Rank Teams by Votes
+#
+
+# @lc code=start
+class Solution:
+    def rankTeams(self, votes: List[str]) -> str:
+        
+# @lc code=end
+
---
+++ leetcode/24/366.find-leaves-of-binary-tree.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode id=2130 lang=python3
+#
+# [2130] Maximum Twin Sum of a Linked List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        prev = None
+        curr = slow
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        res = 0
+        left = head
+        right = prev
+
+        while right:
+            res = max(res, left.val + right.val)
+            left = left.next
+            right = right.next
+
+        return res
+# @lc code=end
+
