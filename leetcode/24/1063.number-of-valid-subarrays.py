7800ad1368a8968fd829af632fbca70bcbd9ed20
mahsanghani <ghaniahsan@outlook.com>
2024-02-12 18:32:20 -0500

    increasing order search tree

---
+++ leetcode/24/1063.number-of-valid-subarrays.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=1063 lang=python3
+#
+# [1063] Number of Valid Subarrays
+#
+# @lc code=start
+class Solution:
+    def validSubarrays(self, nums: List[int]) -> int:
+        
+# @lc code=end
+
