a87b5b91a6d6334a2e5cb9bc58f61121f272b4bb
mahsanghani <ghaniahsan@outlook.com>
2024-02-12 15:05:15 -0500

    lowest common ancestor

---
+++ leetcode/24/2354.number-of-excellent-pairs.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=2354 lang=python3
+#
+# [2354] Number of Excellent Pairs
+#
+
+# @lc code=start
+class Solution:
+    def countExcellentPairs(self, nums: List[int], k: int) -> int:
+        
+# @lc code=end
+
