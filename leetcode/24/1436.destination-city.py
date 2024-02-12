6ac88da15eb98536923b1a9bb1235af849706c80
mahsanghani <ghaniahsan@outlook.com>
2024-02-12 11:09:54 -0500

    partition labels

---
+++ leetcode/24/1436.destination-city.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1436 lang=python3
+#
+# [1436] Destination City
+#
+
+# @lc code=start
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        
+# @lc code=end
+
