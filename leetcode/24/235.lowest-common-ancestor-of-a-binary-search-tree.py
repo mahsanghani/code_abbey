236091b5822399178e9f035e43a848c572d3d23f
mahsanghani <ghaniahsan@outlook.com>
2024-02-12 15:06:18 -0500

    lowest common ancestor

--- leetcode/24/235.lowest-common-ancestor-of-a-binary-search-tree.py
+++ leetcode/24/235.lowest-common-ancestor-of-a-binary-search-tree.py
@@ -15,6 +15,7 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         node = root
+        
         while node:
             if p.val<node.val and q.val<node.val:
                 node = node.left
