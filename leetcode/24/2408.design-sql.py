4671f2802964151c0db4d85db09fe4a4bca09f12
mahsanghani <ghaniahsan@outlook.com>
2024-02-12 17:16:33 -0500

    SQL

---
+++ leetcode/24/2408.design-sql.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=2408 lang=python3
+#
+# [2408] Design SQL
+#
+# @lc code=start
+class SQL:
+
+    def __init__(self, names: List[str], columns: List[int]):
+        
+
+    def insertRow(self, name: str, row: List[str]) -> None:
+        
+
+    def deleteRow(self, name: str, rowId: int) -> None:
+        
+
+    def selectCell(self, name: str, rowId: int, columnId: int) -> str:
+        
+
+
+# Your SQL object will be instantiated and called as such:
+# obj = SQL(names, columns)
+# obj.insertRow(name,row)
+# obj.deleteRow(name,rowId)
+# param_3 = obj.selectCell(name,rowId,columnId)
+# @lc code=end
+
