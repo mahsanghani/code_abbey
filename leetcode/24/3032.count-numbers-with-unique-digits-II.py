e8dc928468ee9304a2be83be53b22948f74bda69
mahsanghani <ghaniahsan@outlook.com>
2024-02-13 12:09:49 -0500

    count numbers with unique digits II

--- leetcode/24/3032.count-numbers-with-unique-digits-II.py
+++ leetcode/24/3032.count-numbers-with-unique-digits-II.py
@@ -6,7 +6,11 @@
 # @lc code=start
 class Solution:
     def numberCount(self, a: int, b: int) -> int:
-        
+        count = 0
+        for i in range(a,b+1):
+            if len(set(str(i)))==len(str(i)):
+                count+=1
+        return count
         
 # @lc code=end
 
