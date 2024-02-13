df194665941d09febe6ace21e709c6e078637648
mahsanghani <ghaniahsan@outlook.com>
2024-02-13 12:22:40 -0500

    valid subarrays

--- leetcode/24/1063.number-of-valid-subarrays.py
+++ leetcode/24/1063.number-of-valid-subarrays.py
@@ -6,6 +6,13 @@
 # @lc code=start
 class Solution:
     def validSubarrays(self, nums: List[int]) -> int:
-        
+        stack = []
+        results = 0
+        for num in nums:
+            while stack and num<stack[-1]:
+                stack.pop()
+            stack.append(num)
+            results+=len(stack)
+        return results
 # @lc code=end
 
