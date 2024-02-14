21d8be12bf59a5bab04638de2d664947102a786b
mahsanghani <ghaniahsan@outlook.com>
2024-02-14 11:49:12 -0500

    count vowel strings

--- leetcode/24/1641.vowels-4.py
+++ leetcode/24/1641.vowels-4.py
@@ -7,14 +7,13 @@
 # @lc code=start
 class Solution:
     def countVowelStrings(self, n: int) -> int:
-        return self.recursion(n,5)
-    
-    def recursion(self, n,v):
-        if n==1:
-            return v
-        if v==1:
-            return 1
-        return self.recursion(n-1,v) + \
-                self.recursion(n,v-1)
+        dp = [[0]*6]*(n+1)
+        for i in range(1,6):
+            dp[1][i] = i
+        for j in range(2,n+1):
+            dp[j][1] = 1
+            for k in range(2,6):
+                dp[j][k] = dp[j][k-1]+dp[j-1][k]
+        return dp[n][5]
 # @lc code=end
 
