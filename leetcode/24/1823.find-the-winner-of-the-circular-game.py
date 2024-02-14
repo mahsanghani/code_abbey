32946f9f4f4eaf527b6bead80f302ef38f28fc72
mahsanghani <ghaniahsan@outlook.com>
2024-02-14 13:25:24 -0500

    circular linked list find the winner of the circular game

--- leetcode/24/1823.find-the-winner-of-the-circular-game.py
+++ leetcode/24/1823.find-the-winner-of-the-circular-game.py
@@ -5,11 +5,23 @@
 #
 
 # @lc code=start
+class ListNode:
+    def __init__(self,val=0,next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        p=1
-        for i in range(1,n):
-            p=(k+p-1)%(i+1)+1
-        return p
+        # circular linked list
+        head = curr = ListNode()
+        for i in range(n):
+            curr.next = ListNode(i+1)
+            curr = curr.next
+        curr.next = head.next
+        # eliminate each k person
+        while(curr.next!=curr):
+            for j in range(k-1):
+                curr = curr.next
+            curr.next = curr.next.next
+        return curr.val
 # @lc code=end
 
