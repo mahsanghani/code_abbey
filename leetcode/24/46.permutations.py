64cbe5993e293667e735c840ee25666d7d935a59
mahsanghani <ghaniahsan@outlook.com>
2024-02-17 08:57:40 -0800

    itertools permutations

---
+++ leetcode/24/46.permutations.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=46 lang=python3
+#
+# [46] Permutations
+#
+
+# @lc code=start
+from itertools import permutations
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        return list(itertools.permutations(nums))
+# @lc code=end
+
