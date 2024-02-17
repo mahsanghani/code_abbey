61a8feefc9dd8cc55501beace7ceda3bd4cffbf5
mahsanghani <ghaniahsan@outlook.com>
2024-02-17 08:54:57 -0800

    counting bits

--- leetcode/24/338.counting-bits.py
+++ leetcode/24/338.counting-bits.py
@@ -7,6 +7,14 @@
 # @lc code=start
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        
+        count = 0
+        results = []
+        for i in range(n+1):
+            for c in str(bin(i)):
+                if c=="1":
+                    count+=1
+            results.append(count)
+            count = 0
+        return results
 # @lc code=end
 
