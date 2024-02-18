234b7618f2067ddc9945996003de804908389adf
mahsanghani <ghaniahsan@outlook.com>
2024-02-18 09:05:43 -0800

    delete node in a linked list

--- leetcode/24/237.delete-node-in-a-linked-list.py
+++ leetcode/24/237.delete-node-in-a-linked-list.py
@@ -17,6 +17,11 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
+        if not node:
+            return None
         
+        node.val = node.next.val
+        node.next = node.next.next
+
 # @lc code=end
 
