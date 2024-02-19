68fc500b248b078b94b5b31de27452c7ff95a6c1
mahsanghani <ghaniahsan@outlook.com>
2024-02-18 21:53:41 -0800

    subsets DFS

--- leetcode/24/78.subsets.py
+++ leetcode/24/78.subsets.py
@@ -8,12 +8,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         results = []
-        n = len(nums)
-
-        for i in range(2**n, 2**(n+1)):
-            bitmask = bin(i)[3:]
-            results.append([nums[j] for j in range(n) if bitmask[j]=='1'])
+        subsets = []
         
+        def dfs(node):
+            if node>=len(nums):
+                results.append(subsets.copy())
+                return None
+            subsets.append(nums[node])
+            dfs(node+1)
+            subsets.pop()
+            dfs(node+1)
+
+        dfs(0)
         return results
 # @lc code=end
 
