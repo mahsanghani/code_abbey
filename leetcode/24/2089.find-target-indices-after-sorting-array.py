000af8745ef6025dda7822d55d8badeef114b6b3
mahsanghani <ghaniahsan@outlook.com>
2024-02-20 19:05:21 -0800

    find target indices after sorting array

--- leetcode/24/2089.find-target-indices-after-sorting-array.py
+++ leetcode/24/2089.find-target-indices-after-sorting-array.py
@@ -9,11 +9,13 @@ class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
         nums.sort()
         indices = {}
+
         for i,j in enumerate(nums):
             if j in indices:
                 indices[j].append(i)
             else:
                 indices[j] = [i]
+
         if target in indices:
             return indices[target]
         else:
