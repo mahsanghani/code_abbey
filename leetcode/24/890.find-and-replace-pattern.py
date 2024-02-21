ceff4b3b5a5936d827ec4391128cad5d26757797
mahsanghani <ghaniahsan@outlook.com>
2024-02-20 20:12:05 -0800

    find and replace pattern

--- leetcode/24/890.find-and-replace-pattern.py
+++ leetcode/24/890.find-and-replace-pattern.py
@@ -7,6 +7,16 @@
 # @lc code=start
 class Solution:
     def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
-        
+        def match(word):
+            m,n = {},{}
+            for w,p in zip(word,pattern):
+                if w not in m:
+                    m[w] = p
+                if p not in n:
+                    n[p] = w
+                if (m[w], n[p]) != (p, w):
+                    return False
+            return True
+        return filter(match, words)
 # @lc code=end
 
