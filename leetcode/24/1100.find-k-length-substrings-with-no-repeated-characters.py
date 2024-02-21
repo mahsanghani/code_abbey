2aeb555e4068c0881003c8086a32e6268b6a7c31
mahsanghani <ghaniahsan@outlook.com>
2024-02-21 14:05:39 -0800

    find k length substrings with no repeated characters

--- leetcode/24/1100.find-k-length-substrings-with-no-repeated-characters.py
+++ leetcode/24/1100.find-k-length-substrings-with-no-repeated-characters.py
@@ -6,9 +6,9 @@
 # @lc code=start
 class Solution:
     def numKLenSubstrNoRepeats(self, s: str, k: int) -> int:
+        results = []
         if len(s)<k:
             return 0
-        results = []
         for i in range(len(s)-k+1):
             sub = s[i:i+k]
             if len(set(sub))==k:
