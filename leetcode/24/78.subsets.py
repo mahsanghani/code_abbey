d045804cb2707ad8495e075270393f69d9d3a4d2
mahsanghani <ghaniahsan@outlook.com>
2024-02-20 18:02:36 -0800

    subsets

--- leetcode/24/78.subsets.py
+++ leetcode/24/78.subsets.py
@@ -7,18 +7,15 @@
 # @lc code=start
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        results = []
-        subsets = []
-        
-        def dfs(node):
-            if node>=len(nums):
+        subsets,results = [],[]
+        def dfs(num):
+            if num>=len(nums):
                 results.append(subsets.copy())
-                return None
-            subsets.append(nums[node])
-            dfs(node+1)
+                return
+            subsets.append(nums[num])
+            dfs(num+1)
             subsets.pop()
-            dfs(node+1)
-
+            dfs(num+1)
         dfs(0)
         return results
 # @lc code=end
