52329bd2100fffbc57980bfaa83c51fb515a6ee8
mahsanghani <ghaniahsan@outlook.com>
2024-02-20 19:16:47 -0800

    len set zip isomorphic strings

--- leetcode/24/205.isomorphic-strings.py
+++ leetcode/24/205.isomorphic-strings.py
@@ -7,6 +7,6 @@
 # @lc code=start
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        return len(set(s)) == len(set(t)) == len(set(zip(s,t)))
+        return len(set(s)) == len(set(t)) == len(set(zip(s, t)))
 # @lc code=end
 
