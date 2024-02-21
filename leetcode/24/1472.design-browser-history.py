0c31fe11ba9cc819d3c9e1aaeef88563bad9893c
mahsanghani <ghaniahsan@outlook.com>
2024-02-20 18:07:53 -0800

    browser history

--- leetcode/24/1472.design-browser-history.py
+++ leetcode/24/1472.design-browser-history.py
@@ -1,3 +1,4 @@
+
 #
 # @lc app=leetcode id=1472 lang=python3
 #
@@ -6,28 +7,12 @@
 # @lc code=start
 class BrowserHistory:
     def __init__(self, homepage: str):
-        self.history = []
-        self.future = []
-        self.current = homepage
         
     def visit(self, url: str) -> None:
-        self.history.append(self.current)
-        self.future = []
-        self.current = url
 
     def back(self, steps: int) -> str:
-        while steps>0 and self.history:
-            self.future.append(self.current)
-            self.current = self.history.pop()
-            steps -= 1
-        return self.current
         
     def forward(self, steps: int) -> str:
-        while steps>0 and self.future:
-            self.history.append(self.current)
-            self.current = self.future.pop()
-            steps -= 1
-        return self.current
 
 # Your BrowserHistory object will be instantiated and called as such:
 # obj = BrowserHistory(homepage)
