b15707e556394af81c9dbf04d7fbb87e308571d0
mahsanghani <ghaniahsan@outlook.com>
2024-02-20 18:31:29 -0800

    unique number of occurrences

--- leetcode/24/1207.unique-number-of-occurrences.py
+++ leetcode/24/1207.unique-number-of-occurrences.py
@@ -9,6 +9,6 @@ from collections import Counter
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         counts = Counter(arr)
-        return len(set(counts.values()))==len(counts.items())
+        return len(set(counts.values()))==len(counts.values())
 # @lc code=end
 
