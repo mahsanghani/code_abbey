b16d1d40c4544b409ff6416363d1010c3e21e6dd
mahsanghani <ghaniahsan@outlook.com>
2024-02-21 13:53:37 -0800

    find k length substrings with no repeated characters

---
+++ leetcode/24/1100.find-k-length-substrings-with-no-repeated-characters.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=1100 lang=python3
+#
+# [1100] Find K-Length Substrings With No Repeated Characters
+#
+# @lc code=start
+class Solution:
+    def numKLenSubstrNoRepeats(self, s: str, k: int) -> int:
+        if len(s)<k:
+            return 0
+
+        results = []
+        
+        for i in range(len(s)-k+1):
+            sub = s[i:i+k]
+            if len(set(sub)) == k:
+                results.append(sub)
+
+        return len(results)
+# @lc code=end
+
--- leetcode/24/1676.lowest-common-ancestor-of-a-binary-tree-IV.py
+++ leetcode/24/1676.lowest-common-ancestor-of-a-binary-tree-IV.py
@@ -10,7 +10,6 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
-
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', nodes: 'List[TreeNode]') -> 'TreeNode':
         nodes = set(nodes)
