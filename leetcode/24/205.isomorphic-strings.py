70effe6ae70050f07aa359cb94fd429768f2a45f
mahsanghani <ghaniahsan@outlook.com>
2024-02-20 19:13:39 -0800

    len set zip isomorphic strings

---
+++ leetcode/24/205.isomorphic-strings.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=205 lang=python3
+#
+# [205] Isomorphic Strings
+#
+
+# @lc code=start
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        return len(set(s))==len(set(t))==len(set(zip(s,t)))
+# @lc code=end
+
---
+++ leetcode/24/890.find-and-replace-pattern.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=890 lang=python3
+#
+# [890] Find and Replace Pattern
+#
+
+# @lc code=start
+class Solution:
+    def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
+        
+# @lc code=end
+
