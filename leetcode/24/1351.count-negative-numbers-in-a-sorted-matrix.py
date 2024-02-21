01bd4f67aa595012aa1b2a71b47d64a215d8ebe9
mahsanghani <ghaniahsan@outlook.com>
2024-02-20 18:17:37 -0800

    binary search count negatives

--- leetcode/24/1351.count-negative-numbers-in-a-sorted-matrix.py
+++ leetcode/24/1351.count-negative-numbers-in-a-sorted-matrix.py
@@ -7,19 +7,8 @@
 # @lc code=start
 class Solution:
     def binary_search(self, row: List[int]) -> int:
-        left,right = 0,len(row)-1
-        while left<=right:
-            mid = left + (right-left) // 2
-            if row[mid] < 0:
-                right = mid - 1
-            else:
-                left = mid + 1
-        return len(row) - left
 
     def countNegatives(self, grid: List[List[int]]) -> int:
-        results = 0
-        for row in grid:
-            results += self.binary_search(row)
-        return results
+        
 # @lc code=end
 
