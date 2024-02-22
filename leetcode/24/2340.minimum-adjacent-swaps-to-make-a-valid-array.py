066304d81ee27e724427a6235301aad6db8d463e
mahsanghani <ghaniahsan@outlook.com>
2024-02-22 15:23:00 -0800

    minimum adjacent swaps to make a valid array

---
+++ leetcode/24/2340.minimum-adjacent-swaps-to-make-a-valid-array.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=2340 lang=python3
+#
+# [2340] Minimum Adjacent Swaps to Make a Valid Array
+#
+
+# @lc code=start
+class Solution:
+    def minimumSwaps(self, nums: List[int]) -> int:
+        mini = min(nums)
+        maxi = max(nums)
+        idx1 = nums.index(mini)
+        idx2 = nums.index(maxi)
+        return idx1 + idx2
+# @lc code=end
+
