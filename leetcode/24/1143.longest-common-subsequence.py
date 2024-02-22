f5abb2319ddc288387d3ac4805c8f9421ecb6864
mahsanghani <ghaniahsan@outlook.com>
2024-02-22 15:15:40 -0800

    dynamic programming with space optimization

--- leetcode/24/1143.longest-common-subsequence.py
+++ leetcode/24/1143.longest-common-subsequence.py
@@ -6,15 +6,20 @@
 # @lc code=start
 class Solution:
     def longestCommonSubsequence(self, t1: str, t2: str) -> int:
-        grid = [[0]*(len(t2)+1) for _ in range(len(t1)+1)]
+        if len(t1)<len(t2):
+            t1,t2=t2,t1
+
+        previous = [0]*(len(t1)+1)
 
         for col in reversed(range(len(t2))):
+            current = [0]*(len(t1)+1)
             for row in reversed(range(len(t1))):
                 if t2[col]==t1[row]:
-                    grid[row][col] = 1+grid[row+1][col+1]
+                    current[row]=1+previous[row+1]
                 else:
-                    grid[row][col] = max(grid[row+1][col], grid[row][col+1])
+                    current[row] = max(previous[row], current[row+1])
+            previous = current
 
-        return grid[0][0]
+        return previous[0]
 # @lc code=end
     
