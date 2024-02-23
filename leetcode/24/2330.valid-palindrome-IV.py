89f43e34c3029a9c5d40d54fe6ca61e9e8339ace
mahsanghani <ghaniahsan@outlook.com>
2024-02-22 18:23:14 -0800

    valid palindrome 4

--- leetcode/24/2330.valid-palindrome-IV.py
+++ leetcode/24/2330.valid-palindrome-IV.py
@@ -7,6 +7,13 @@
 # @lc code=start
 class Solution:
     def makePalindrome(self, s: str) -> bool:
-        
+        i,j,op = 0,len(s)-1,2
+
+        while i<j and op>=0:
+            if s[i]!=s[j]:
+                op-=1
+            i+=1
+            j-=1
+        return op>=0 and j<=i
 # @lc code=end
 
