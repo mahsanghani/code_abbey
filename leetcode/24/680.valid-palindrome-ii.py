eaaa626f0d09f87448673e2252ff7d4356f18553
mahsanghani <ghaniahsan@outlook.com>
2024-02-22 16:39:24 -0800

    valid palindrome 2

--- leetcode/24/680.valid-palindrome-ii.py
+++ leetcode/24/680.valid-palindrome-ii.py
@@ -5,8 +5,16 @@
 #
 
 # @lc code=start
+from collections import Counter
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        
+        counts = Counter(s)
+        odd,even = 0,0
+        for k,v in counts.items():
+            if v%2==0:
+                even+=1
+            else:
+                odd+=1
+        return odd<2 if len(s)%2==0 else odd<3
 # @lc code=end
 
