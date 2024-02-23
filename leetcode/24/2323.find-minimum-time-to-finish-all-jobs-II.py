35a09223f99df64a92ee35a48cae4df87376c940
mahsanghani <ghaniahsan@outlook.com>
2024-02-22 20:38:40 -0800

    find minimum time to finish all jobs 2

---
+++ leetcode/24/2323.find-minimum-time-to-finish-all-jobs-II.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=2323 lang=python3
+#
+# [2323] Find Minimum Time to Finish All Jobs II
+#
+# @lc code=start
+class Solution:
+    def minimumTime(self, jobs: List[int], workers: List[int]) -> int:
+        
+# @lc code=end
+
