660190db325475c00608a123e81d980e46f33cba
mahsanghani <ghaniahsan@outlook.com>
2024-02-22 22:29:03 -0800

    jump game 8

--- leetcode/24/2297.jump-game-VIII.py
+++ leetcode/24/2297.jump-game-VIII.py
@@ -4,8 +4,22 @@
 # [2297] Jump Game VIII
 #
 # @lc code=start
+import math
 class Solution:
     def minCost(self, nums: List[int], costs: List[int]) -> int:
-        
+        # monotonic stack dynamic programming
+        n = len(nums)
+        ng,nl = []*n,[]*n
+        dp = [math.inf]*n
+        dp[0] = 0
+
+        for i in range(n):
+            while ng and nums[ng[-1]] <= nums[i]:
+                dp[i] = min(dp[i], dp[ng.pop()] + costs[i])
+            while nl and nums[nl[-1]] > nums[i]:
+                dp[i] = min(dp[i], dp[nl.pop()] + costs[i])
+            ng.append(i)
+            nl.append(i)
+        return dp[n-1]
 # @lc code=end
 
