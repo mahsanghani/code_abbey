f3c66b7089501620892b18f046156add49711192
mahsanghani <ghaniahsan@outlook.com>
2024-02-22 16:32:30 -0800

    valid palindrome 1

--- leetcode/24/125.valid-palindrome.py
+++ leetcode/24/125.valid-palindrome.py
@@ -7,6 +7,7 @@
 # @lc code=start
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
+        s = ''.join(c for c in s if c.isalnum())
+        return s.lower().strip()==s[::-1].lower().strip()
 # @lc code=end
 
