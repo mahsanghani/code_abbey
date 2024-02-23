b19da93dd136ede38f0ec25f0173c6d1207a8ea1
mahsanghani <ghaniahsan@outlook.com>
2024-02-22 20:46:26 -0800

    jump game nums cost

---
+++ leetcode/24/2297.jump-game-VIII.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=2297 lang=python3
+#
+# [2297] Jump Game VIII
+#
+# @lc code=start
+class Solution:
+    def minCost(self, nums: List[int], costs: List[int]) -> int:
+        
+# @lc code=end
+
