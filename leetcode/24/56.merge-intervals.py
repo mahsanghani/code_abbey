12113826d89dce059efe7a8c35e7c515767226e2
mahsanghani <ghaniahsan@outlook.com>
2024-02-23 18:39:44 -0800

    merge intervals

---
+++ leetcode/24/56.merge-intervals.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=56 lang=python3
+#
+# [56] Merge Intervals
+#
+
+# @lc code=start
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        results = []
+        intervals.sort()
+        for interval in intervals:
+            if not results or results[-1][1]<interval[0]:
+                results.append(interval)
+            else:
+                results[-1][1] = max(results[-1][1],interval[1])
+        return results
+# @lc code=end
+
---
+++ leetcode/24/565.array-nesting.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=565 lang=python3
+#
+# [565] Array Nesting
+#
+
+# @lc code=start
+class Solution:
+    def arrayNesting(self, nums: List[int]) -> int:
+        
+# @lc code=end
+
