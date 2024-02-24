21d196776b900606daac812a8af1a08c710d90ee
mahsanghani <ghaniahsan@outlook.com>
2024-02-24 09:20:15 -0800

    remove intervals

---
+++ leetcode/24/1272.remove-interval.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=1272 lang=python3
+#
+# [1272] Remove Interval
+#
+# @lc code=start
+class Solution:
+    def removeInterval(self, intervals: List[List[int]], toBeRemoved: List[int]) -> List[List[int]]:
+        results = []
+        for interval in intervals:
+            if interval[1]<toBeRemoved[0] or interval[0]>toBeRemoved[1]:
+                results.append(interval)
+            else:
+                if interval[1]>toBeRemoved[0]:
+                    results.append([interval[0],toBeRemoved[0]])
+                if interval[0]<toBeRemoved[1]:
+                    results.append([toBeRemoved[1],interval[1]])
+        return results
+# @lc code=end
+
