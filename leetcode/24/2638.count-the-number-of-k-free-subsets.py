b804a7c6f6a162174a794a1866b59889f4bac90a
mahsanghani <ghaniahsan@outlook.com>
2024-02-24 11:07:51 -0800

    count the number of k free subsets

--- leetcode/24/2638.count-the-number-of-k-free-subsets.py
+++ leetcode/24/2638.count-the-number-of-k-free-subsets.py
@@ -4,8 +4,21 @@
 # [2638] Count the Number of K-Free Subsets
 #
 # @lc code=start
+from collections import defaultdict
 class Solution:
     def countTheNumOfKFreeSubsets(self, nums: List[int], k: int) -> int:
+        size = defaultdict(int)
         
+        for i in sorted(nums):
+            size[i] = size[i-k]+1
+            size.pop(i-k)
+
+        m = max(size.values())
+        fib = [1]*(m+2)
+        
+        for i in range(2,m+2):
+            fib[i] = fib[i-2]+fib[i-1]
+        
+        return reduce(mul, (fib[v+1] for v in size.values()))
 # @lc code=end
 
