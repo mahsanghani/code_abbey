aa41ea6e8b19e81ab667768e75c60c5b2f90b4bb
mahsanghani <ghaniahsan@outlook.com>
2024-02-24 10:27:05 -0800

    reorder data in log files

--- leetcode/24/937.reorder-data-in-log-files.py
+++ leetcode/24/937.reorder-data-in-log-files.py
@@ -3,10 +3,16 @@
 #
 # [937] Reorder Data in Log Files
 #
-
 # @lc code=start
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        digits = []
+        letters = []
+
+        def get_key(log):
+            id, rest = log.split(" ", maxsplit=1)
+            return (0,rest,id) if rest[0].isalpha() else (1,)
         
+        return sorted(logs, key=get_key)
 # @lc code=end
 
