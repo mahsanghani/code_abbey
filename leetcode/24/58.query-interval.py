b6439df13e2ff4f3b2bd04fe5e11c33b8e2f179f
mahsanghani <ghaniahsan@outlook.com>
2024-02-23 19:05:18 -0800

    TreeNode

--- leetcode/24/58.query-interval.py
+++ leetcode/24/58.query-interval.py
@@ -1,12 +1,15 @@
 #
 # @lc app=leetcode id=57 lang=python3
 #
-# [57] Insert Interval
+# [57] Query Interval
 #
-
 # @lc code=start
-class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        
+class TreeNode:
+    def __init__(self, start, end, middle) -> None:
+        self.start = start
+        self.middle = middle
+        self.end = end
+        self.left = None
+        self.right = None
 # @lc code=end
 
