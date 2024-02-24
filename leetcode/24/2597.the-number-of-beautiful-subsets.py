b506a0f6f73c865aaec50b2692f44fa2fe7776f0
mahsanghani <ghaniahsan@outlook.com>
2024-02-24 11:21:26 -0800

    beautiful subsets

--- leetcode/24/2597.the-number-of-beautiful-subsets.py
+++ leetcode/24/2597.the-number-of-beautiful-subsets.py
@@ -4,8 +4,14 @@
 # [2597] The Number of Beautiful Subsets
 #
 # @lc code=start
+from itertools import combinations
 class Solution:
     def beautifulSubsets(self, nums: List[int], k: int) -> int:
-        
+        count = 0
+        for i in range(1,len(nums)+1):
+            for subset in combinations(nums,i):
+                if max(subset) - min(subset) <= k:
+                    count += k
+        return count
 # @lc code=end
 
