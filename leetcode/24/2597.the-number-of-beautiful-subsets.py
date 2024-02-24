3983d1c9d7f632820db28f67ce11e92122517358
mahsanghani <ghaniahsan@outlook.com>
2024-02-24 10:48:58 -0800

    beautiful subsets

---
+++ leetcode/24/2597.the-number-of-beautiful-subsets.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=2597 lang=python3
+#
+# [2597] The Number of Beautiful Subsets
+#
+# @lc code=start
+class Solution:
+    def beautifulSubsets(self, nums: List[int], k: int) -> int:
+        
+# @lc code=end
+
---
+++ leetcode/24/2638.count-the-number-of-k-free-subsets.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=2638 lang=python3
+#
+# [2638] Count the Number of K-Free Subsets
+#
+# @lc code=start
+class Solution:
+    def countTheNumOfKFreeSubsets(self, nums: List[int], k: int) -> int:
+        
+# @lc code=end
+
