9cc1694034f8d1af06615aa9f1f530fada1da5e6
mahsanghani <ghaniahsan@outlook.com>
2024-02-24 21:29:01 -0800

    maximum sum of distint subarrays with length k

--- leetcode/24/2461.maximum-sum-of-distinct-subarrays-with-length-k.py
+++ leetcode/24/2461.maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -3,14 +3,28 @@
 #
 # [2461] Maximum Sum of Distinct Subarrays With Length K
 #
-
 # @lc code=start
+from collections import Counter
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        results = 0
-        for i in range(len(nums)-k+1):
-            if len(set(nums[i:i+k]))==len(nums[i:i+k]):
-                results = max(results, sum(nums[i:i+k]))
-        return results
+        n = len(nums)
+        ctr = Counter()
+        l,s,r = 0,0,0
+
+        for i in range(n):
+            s+=nums[i]
+            if ctr[nums[i]]==0:
+                l+=1
+            ctr[nums[i]]+=1
+            if i>=k:
+                s-=nums[i-k]
+                ctr[nums[i-k]]-=1
+                if ctr[nums[i-k]]==0:
+                    l-=1
+            if i>=k-1:
+                if l==k:
+                    r=max(r,s)
+        return r
+
 # @lc code=end
 
