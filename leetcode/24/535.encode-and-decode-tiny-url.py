3a43f54ed4c606d82b349ff813771147f871dbcf
mahsanghani <ghaniahsan@outlook.com>
2024-02-26 10:32:39 -0800

    hash encode decode tinyurl

--- leetcode/24/535.encode-and-decode-tiny-url.py
+++ leetcode/24/535.encode-and-decode-tiny-url.py
@@ -6,22 +6,26 @@
 # @lc code=start
 class Codec:
     def __init__(self):
-        self.prefix = 
+        self.prefix = "https://tinyurl.com/"
         self.map = dict()
 
     def _hash(self, s):
         base = 13331
         mod = 1e9+7
+        res = 0
         for c in s:
             res = (res*base + ord(c))%mod
         return res
 
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL."""
+        key = str(self._hash(longUrl))
+        self.map[key] = longUrl
+        return self.prefix + key
 
     def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL."""
-        
+        return self.map[shortUrl.split("/")[-1]]
         
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
