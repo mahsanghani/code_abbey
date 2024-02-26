5c322519b4069e806d1a49b35ed055a8b542aef0
mahsanghani <ghaniahsan@outlook.com>
2024-02-26 15:13:20 -0800

    subrectangle queries

--- leetcode/24/1476.subrectangle-queries.py
+++ leetcode/24/1476.subrectangle-queries.py
@@ -6,15 +6,15 @@
 # @lc code=start
 class SubrectangleQueries:
     def __init__(self, rectangle: List[List[int]]):
-        self.rectangle = rectangle
+        self.rectangles = rectangle
 
     def updateSubrectangle(self, row1: int, col1: int, row2: int, col2: int, newValue: int) -> None:
         for row in range(row1, row2 + 1):
             for col in range(col1, col2 + 1):
-                self.rectangle[row][col] = newValue
+                self.rectangles[row][col] = newValue
 
     def getValue(self, row: int, col: int) -> int:
-        return self.rectangle[row][col]
+        return self.rectangles[row][col]
 
 # Your SubrectangleQueries object will be instantiated and called as such:
 # obj = SubrectangleQueries(rectangle)
