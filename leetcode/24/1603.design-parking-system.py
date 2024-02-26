5a954c6eee6694961796771767c4c8f7f2e968ac
mahsanghani <ghaniahsan@outlook.com>
2024-02-26 15:15:19 -0800

    design parking system

--- leetcode/24/1603.design-parking-system.py
+++ leetcode/24/1603.design-parking-system.py
@@ -6,14 +6,13 @@
 # @lc code=start
 class ParkingSystem:
     def __init__(self, big: int, medium: int, small: int):
-        self.garage = [big, medium, small]
+        self.parkade = [big, medium, small]
 
     def addCar(self, carType: int) -> bool:
-        if self.garage[carType-1]>0:
-            self.garage[carType-1]-=1
+        if self.parkade[carType-1]>0:
+            self.parkade[carType-1]-=1
             return True
-        else:
-            return False
+        return False
 
 # Your ParkingSystem object will be instantiated and called as such:
 # obj = ParkingSystem(big, medium, small)
