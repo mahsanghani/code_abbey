8745e4ae2e35544b004c2c429bda360e2cf09558
mahsanghani <ghaniahsan@outlook.com>
2024-02-26 10:19:56 -0800

    subrectangle queries

--- leetcode/24/1476.subrectangle-queries.py
+++ leetcode/24/1476.subrectangle-queries.py
@@ -9,9 +9,9 @@ class SubrectangleQueries:
         self.rectangle = rectangle
 
     def updateSubrectangle(self, row1: int, col1: int, row2: int, col2: int, newValue: int) -> None:
-        for r in range(row1,row2+1):
-            for c in range(col1,col2+1):
-                self.rectangle[r][c] = newValue
+        for row in range(row1, row2 + 1):
+            for col in range(col1, col2 + 1):
+                self.rectangle[row][col] = newValue
 
     def getValue(self, row: int, col: int) -> int:
         return self.rectangle[row][col]
