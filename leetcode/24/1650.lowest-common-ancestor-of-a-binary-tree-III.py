a5482f58a453c3b304070e6a592b46fb56ff739c
mahsanghani <ghaniahsan@outlook.com>
2024-02-26 13:37:51 -0800

    lowest common ancestor of a binary tree 3

--- leetcode/24/1650.lowest-common-ancestor-of-a-binary-tree-III.py
+++ leetcode/24/1650.lowest-common-ancestor-of-a-binary-tree-III.py
@@ -14,27 +14,12 @@ class Node:
         self.parent = None
 
 class Solution:
-    def depth(self, p):
-        if not p:
-            return -1
-        return 1 + self.depth(p.parent)
-
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
-        p_d = self.depth(p)
-        q_d = self.depth(q)
-
-        while p_d > q_d:
+        path = set()
+        while p:
+            path.add(p)
             p = p.parent
-            p_d -= 1
-
-        while q_d > p_d:
+        while q not in path:
             q = q.parent
-            q_d -= 1
-
-        while p != q:
-            p = p.parent
-            q = q.parent
-
-        return p
-# @lc code=end
+        return q
 
