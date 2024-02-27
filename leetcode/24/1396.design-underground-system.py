3d58016b8a474eb090ea2290e4cf4a46b91ba13a
mahsanghani <ghaniahsan@outlook.com>
2024-02-27 13:11:35 -0800

    design an underground system

--- leetcode/24/1396.design-underground-system.py
+++ leetcode/24/1396.design-underground-system.py
@@ -10,18 +10,18 @@ class UndergroundSystem:
         self.total = {}
 
     def checkIn(self, id: int, start: str, time: int) -> None:
-        self.check[id] = (start,time)
+        self.check[id] = (start, time)
 
     def checkOut(self, id: int, end: str, time: int) -> None:
         start, t = self.check[id]
-        route = (start,end)
+        route = (start, end)
         if route not in self.total:
             self.total[route] = [0,0]
         self.total[route][0] += time-t
         self.total[route][1] += 1
 
     def getAverageTime(self, start: str, end: str) -> float:
-        total, count = self.total[start,end]
+        total, count = self.total[(start,end)]
         return total / count
 
 # Your UndergroundSystem object will be instantiated and called as such:
