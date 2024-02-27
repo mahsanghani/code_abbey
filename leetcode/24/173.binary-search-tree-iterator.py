f4b58546da4f0145fefd8a8deaf8d1e9d6080ad1
mahsanghani <ghaniahsan@outlook.com>
2024-02-27 15:44:49 -0800

    binary search tree iterator

---
+++ leetcode/24/173.binary-search-tree-iterator.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=173 lang=python3
+#
+# [173] Binary Search Tree Iterator
+#
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class BSTIterator:
+    def __init__(self, root: Optional[TreeNode]):
+        if not root:
+            return None
+
+    def next(self) -> int:
+        
+
+    def hasNext(self) -> bool:
+        
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
+# @lc code=end
+
