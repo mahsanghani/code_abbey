85a6166b990c4ae3bd6ff475d2dc6d09cf9257a4
mahsanghani <ghaniahsan@outlook.com>
2024-02-27 13:47:38 -0800

    combination iterator

--- leetcode/24/1286.iterator-for-combination.py
+++ leetcode/24/1286.iterator-for-combination.py
@@ -3,18 +3,21 @@
 #
 # [1286] Iterator for Combination
 #
-
 # @lc code=start
 class CombinationIterator:
-
     def __init__(self, characters: str, combinationLength: int):
-        
+        self.chars = characters
+        self.length = combinationLength
+        self.results = []
+        self.backtrack(0,[])
 
     def next(self) -> str:
-        
+        item = self.results[0]
+        self.results = self.results[:-1]
+        return item
 
     def hasNext(self) -> bool:
-        
+        return len(self.results)>0
 
 
 # Your CombinationIterator object will be instantiated and called as such:
