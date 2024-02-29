ca392bc1f5e3771773083aad5ee4f4f5491ac867
mahsanghani <ghaniahsan@outlook.com>
2024-02-29 10:34:21 -0500

    hashmap

--- leetcode/24/706.design-hash-map.py
+++ leetcode/24/706.design-hash-map.py
@@ -5,7 +5,7 @@
 #
 # @lc code=start
 class ListNode:
-    def __init__(self,key,val,next):
+    def __init__(self, key=-1, val=-1, next=None):
         self.key = key
         self.val = val
         self.next = next
