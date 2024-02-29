26db582ceb9fe68e50044523cb79c97bc84490a6
mahsanghani <ghaniahsan@outlook.com>
2024-02-29 15:04:23 -0500

    dfs

---
+++ leetcode/24/1600.throne-inheritance.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=1600 lang=python3
+#
+# [1600] Throne Inheritance
+#
+
+# @lc code=start
+class ThroneInheritance:
+
+    def __init__(self, kingName: str):
+        
+
+    def birth(self, parentName: str, childName: str) -> None:
+        
+
+    def death(self, name: str) -> None:
+        
+
+    def getInheritanceOrder(self) -> List[str]:
+        
+
+
+# Your ThroneInheritance object will be instantiated and called as such:
+# obj = ThroneInheritance(kingName)
+# obj.birth(parentName,childName)
+# obj.death(name)
+# param_3 = obj.getInheritanceOrder()
+# @lc code=end
+
---
+++ leetcode/24/208.implement-trie-prefix-tree.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=208 lang=python3
+#
+# [208] Implement Trie (Prefix Tree)
+#
+
+# @lc code=start
+class Trie:
+
+    def __init__(self):
+        
+
+    def insert(self, word: str) -> None:
+        
+
+    def search(self, word: str) -> bool:
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+# @lc code=end
+
