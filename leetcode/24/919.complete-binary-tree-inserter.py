e6f949d93244813a44fecda17292dfa0f0bcbbfd
mahsanghani <ghaniahsan@outlook.com>
2024-02-29 13:09:17 -0500

    design a complete binary tree inverter

---
+++ leetcode/24/919.complete-binary-tree-inserter.py
@@ -0,0 +1,48 @@
+#
+# @lc app=leetcode id=919 lang=python3
+#
+# [919] Complete Binary Tree Inserter
+#
+# @lc code=start
+# Definition for a binary tree node.
+from collections import deque
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class CBTInserter:
+    def __init__(self, root: Optional[TreeNode]):
+        self.root = root
+        self.deque = deque()
+        q = deque([root])
+        while q:
+            node = q.popleft()
+            if not node.left or not node.right:
+                self.deque.append(node)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+
+    def insert(self, val: int) -> int:
+        node = self.deque[0]
+        self.deque.append(TreeNode(val))
+        if not node.left:
+            node.left = self.deque[-1]
+        else:
+            node.right = self.deque[-1]
+            self.deque.popleft()
+        return node.val
+
+    def get_root(self) -> Optional[TreeNode]:
+        return self.root
+
+
+# Your CBTInserter object will be instantiated and called as such:
+# obj = CBTInserter(root)
+# param_1 = obj.insert(val)
+# param_2 = obj.get_root()
+# @lc code=end
+
