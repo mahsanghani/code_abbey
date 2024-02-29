5365f2efd45859b1ca39c105c9501f17a46b7cb5
mahsanghani <ghaniahsan@outlook.com>
2024-02-29 09:58:18 -0500

    design hashset

--- leetcode/24/706.design-hash-map.py
+++ leetcode/24/706.design-hash-map.py
@@ -3,21 +3,24 @@
 #
 # [706] Design HashMap
 #
-
 # @lc code=start
-class MyHashMap:
+class ListNode:
+    def __init__(self, key):
+        self.key = key
+        self.next = None
 
+class MyHashMap:
     def __init__(self):
-        
+        self.set = [ListNode(0) for i in range(10**4)]
 
     def put(self, key: int, value: int) -> None:
-        
+        cur = self.set[key%len(self.set)]
 
     def get(self, key: int) -> int:
-        
+        cur = self.set[key%len(self.set)]
 
     def remove(self, key: int) -> None:
-        
+        cur = self.set[key%len(self.set)]
 
 
 # Your MyHashMap object will be instantiated and called as such:
