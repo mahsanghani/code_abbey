b64915c0dcd8fd294b2e5a4a2e2e2696a5db4172
mahsanghani <ghaniahsan@outlook.com>
2024-02-29 19:18:01 -0500

    def lock

--- leetcode/24/1993.operations-on-tree.py
+++ leetcode/24/1993.operations-on-tree.py
@@ -13,7 +13,10 @@ class LockingTree:
             self.child[parent[i]].append(i)
 
     def lock(self, num: int, user: int) -> bool:
-        
+        if self.locked[num]:
+            return False
+        self.locked[num] = user
+        return True
 
     def unlock(self, num: int, user: int) -> bool:
         
