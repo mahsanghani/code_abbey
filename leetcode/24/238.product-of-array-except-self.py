ade73097949ac24d1a5db559e4d50becbc7f96b4
mahsanghani <ghaniahsan@outlook.com>
2024-03-02 15:28:30 -0500

    product of array except self

--- leetcode/24/238.product-of-array-except-self.py
+++ leetcode/24/238.product-of-array-except-self.py
@@ -9,6 +9,12 @@ import math
 from collections import defaultdict
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        
+        results = []
+        products = defaultdict(int)
+        for j,k in enumerate(nums):
+            if k not in products:
+                products[k] = math.prod([x for i,x in enumerate(nums) if i!=j])
+            results.append(products[k])
+        return results
 # @lc code=end
 
