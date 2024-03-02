b6c3322180801d06784feecd9aede05bb0bca35e
mahsanghani <ghaniahsan@outlook.com>
2024-03-02 15:23:08 -0500

    top k frequent

--- leetcode/24/347.top-k-frequent-elements.py
+++ leetcode/24/347.top-k-frequent-elements.py
@@ -7,6 +7,8 @@
 from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
+        counts = Counter(nums)
+        counts = dict(sorted(counts.items(), key=lambda item:item[1], reverse=True))
+        return list(counts.keys())[:k]
 # @lc code=end
 
