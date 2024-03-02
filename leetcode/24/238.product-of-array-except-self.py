9a52c4a03a1e150dfc32761ce08e80ea475ced31
mahsanghani <ghaniahsan@outlook.com>
2024-03-02 15:21:28 -0500

    sudoku

--- leetcode/24/238.product-of-array-except-self.py
+++ leetcode/24/238.product-of-array-except-self.py
@@ -9,12 +9,6 @@ import math
 from collections import defaultdict
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        results = []
-        products = defaultdict(int)
-        for i,j in enumerate(nums):
-            if j not in products:
-                products[j] = math.prod([x for k,x in enumerate(nums) if k!=i])
-            results.append(products[j])
-        return results
+        
 # @lc code=end
 
--- leetcode/24/347.top-k-frequent-elements.py
+++ leetcode/24/347.top-k-frequent-elements.py
@@ -7,8 +7,6 @@
 from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counts = Counter(nums)
-        counts = dict(sorted(counts.items(), key = lambda item: item[1], reverse = True))
-        return list(counts.keys())[:k]
+        
 # @lc code=end
 
