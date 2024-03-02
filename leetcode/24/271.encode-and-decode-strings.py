28be51d32f9e6bc7aa72c6047ad0ff4db28417c6
mahsanghani <ghaniahsan@outlook.com>
2024-03-02 15:54:43 -0500

    encode decode

---
+++ leetcode/24/271.encode-and-decode-strings.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=271 lang=python3
+#
+# [271] Encode and Decode Strings
+#
+# @lc code=start
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.encode(strs)
+# codec.decode(codec.encode(strs))
+# @lc code=end
+
