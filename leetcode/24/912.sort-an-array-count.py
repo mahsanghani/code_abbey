38c9cb29bcc4fd84812c4f4d029abafac9742448
mahsanghani <ghaniahsan@outlook.com>
2024-03-02 07:22:43 -0500

    counting sort

--- leetcode/24/912.sort-an-array-count.py
+++ leetcode/24/912.sort-an-array-count.py
@@ -3,10 +3,21 @@
 #
 # [912] Sort an Array
 #
-
 # @lc code=start
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
+        counts = {}
+        minval,maxval = min(nums),max(nums)
+        for val in nums:
+            counts[val] = counts.get(val,0)+1
+        index = 0
+
+        for val in range(minval,maxval+1,1):
+            while counts.get(val,0)>0:
+                nums[index] = val
+                index += 1
+                counts[val] -= 1
         
+        return nums
 # @lc code=end
 
