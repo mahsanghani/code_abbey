966ea0d1cd57f7a4e0179890e76871bda3dc41ed
mahsanghani <ghaniahsan@outlook.com>
2024-03-02 07:40:17 -0500

    buckets

--- leetcode/24/912.sort-an-array-radix.py
+++ leetcode/24/912.sort-an-array-radix.py
@@ -5,7 +5,7 @@
 #
 # @lc code=start
 class Solution:
-    def radix_sort(self, nums: List[int]) -> List[int]:
+    def sortArray(self, nums: List[int]) -> List[int]:
         max_element = nums[0]
         for val in nums:
             max_element = max(abs(val), max_element)
@@ -16,7 +16,26 @@ class Solution:
             max_element = max_element // 10
 
         place_value = 1
+        
+        def buckets():
+            buckets = [[] for i in range(10)]
+            for val in nums:
+                digit = abs(val) / place_value
+                digit = int(digit%10)
+                buckets[digit].append(val)
 
+            index = 0
+            for digit in range(10):
+                for val in buckets[digit]:
+                    nums[index] = val
+                    index += 1
 
+        for _ in range(max_digits):
+            buckets()
+            place_value *= 10
+
+        positives = [val for val in nums if val>=0]
+        negatives = [val for val in nums if val<0]
+        return positives + negatives
 # @lc code=end
 
