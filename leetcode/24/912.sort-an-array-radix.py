8f4fa93e9b9772e7ed27c39d3028a7588ea397f0
mahsanghani <ghaniahsan@outlook.com>
2024-03-02 07:36:03 -0500

    radix sort

--- leetcode/24/912.sort-an-array-radix.py
+++ leetcode/24/912.sort-an-array-radix.py
@@ -3,10 +3,20 @@
 #
 # [912] Sort an Array
 #
-
 # @lc code=start
 class Solution:
-    def sortArray(self, nums: List[int]) -> List[int]:
-        
+    def radix_sort(self, nums: List[int]) -> List[int]:
+        max_element = nums[0]
+        for val in nums:
+            max_element = max(abs(val), max_element)
+
+        max_digits = 0
+        while max_element > 0:
+            max_digits += 1
+            max_element = max_element // 10
+
+        place_value = 1
+
+
 # @lc code=end
 
