dd7427c7310f34954ce5ffe64de95e466072ba73
mahsanghani <ghaniahsan@outlook.com>
2024-03-02 14:15:09 -0500

    counter reverse keys

--- leetcode/24/347.top-k-frequent-elements.py
+++ leetcode/24/347.top-k-frequent-elements.py
@@ -8,7 +8,7 @@ from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counts = Counter(nums)
-        counts = dict(sorted(counts.items(), key=lambda item: item[1], reverse=True))
+        counts = dict(sorted(counts.items(), key = lambda item: item[1], reverse = True))
         return list(counts.keys())[:k]
 # @lc code=end
 
