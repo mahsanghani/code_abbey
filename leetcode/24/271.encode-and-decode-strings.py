288f4fc5b560b014708382fe08693c0120a15bf5
mahsanghani <ghaniahsan@outlook.com>
2024-03-02 16:08:07 -0500

    encode decode

--- leetcode/24/271.encode-and-decode-strings.py
+++ leetcode/24/271.encode-and-decode-strings.py
@@ -8,10 +8,12 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
+        return '!$!$'.join(strs)
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
+        return s.split('!$!$')
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
