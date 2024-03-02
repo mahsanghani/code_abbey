c5eeba009068d22f5625ed947db487642ff8e77b
mahsanghani <ghaniahsan@outlook.com>
2024-03-02 16:07:07 -0500

    longest consecutive sequence

--- leetcode/24/128.longest-consecutive-sequence.py
+++ leetcode/24/128.longest-consecutive-sequence.py
@@ -6,6 +6,17 @@
 # @lc code=start
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        maxlength = 0
+        unique = set(nums)
+
+        for num in nums:
+            if num-1 not in unique:
+                length = 0
+                while num+length in unique:
+                    length+=1
+                if length>maxlength:
+                    maxlength=length
         
+        return maxlength
 # @lc code=end
 
