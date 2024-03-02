2b1e2eec982f921ff13b8ea4c81dcb651537f48c
mahsanghani <ghaniahsan@outlook.com>
2024-03-02 15:21:17 -0500

    sudoku

---
+++ leetcode/24/36.valid-sudoku.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=36 lang=python3
+#
+# [36] Valid Sudoku
+#
+
+# @lc code=start
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+# @lc code=end
+
