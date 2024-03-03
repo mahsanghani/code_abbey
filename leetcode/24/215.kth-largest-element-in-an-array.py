dd2d8c469d980e7f2c898932d76db683fc3e62bd
mahsanghani <ghaniahsan@outlook.com>
2024-03-03 14:44:18 -0500

    kth largest element in an array

--- leetcode/24/215.kth-largest-element-in-an-array.py
+++ leetcode/24/215.kth-largest-element-in-an-array.py
@@ -5,32 +5,26 @@
 #
 # @lc code=start
 class Solution:
-    def partition(self,nums,l,r):
-        ptr,pvt = l,nums[r]
-
-        for i in range(l,r):
-            if nums[i]<=pvt:
-                nums[ptr],nums[i] = nums[i],nums[ptr]
-                ptr+=1
-        
-        nums[ptr],nums[r] = nums[r],nums[ptr]
-
-        return ptr
-
     def findKthLargest(self, nums: List[int], k: int) -> int:
         k = len(nums)-k
-        l,r = 0,len(nums)-1
-        
-        while l<r:
-            pvt = self.partition(nums,l,r)
-            
-            if pvt<k:
-                l=pvt+1
-            elif pvt>k:
-                r=pvt-1
+        # l,r = 0,len(nums)-1
+
+        def quickselect(l,r):
+            ptr,pvt = l,nums[r]
+
+            for i in range(l,r):
+                if nums[i]<=pvt:
+                    nums[ptr],nums[i] = nums[i],nums[ptr]
+                    ptr+=1
+            nums[ptr],nums[r] = nums[r],nums[ptr]
+
+            if k<ptr:
+                return quickselect(l,ptr-1)
+            if k>ptr:
+                return quickselect(ptr+1,r)
             else:
-                break
-    
-        return nums[k]
+                return nums[ptr]
+            
+        return quickselect(0,len(nums)-1)
 # @lc code=end
 
