2d3c0304d52bc983317b3bcefb41fbae7170f545
mahsanghani <ghaniahsan@outlook.com>
2024-03-03 18:25:30 -0500

    meetings rooms 2

--- leetcode/24/253.meeting-rooms-ii.py
+++ leetcode/24/253.meeting-rooms-ii.py
@@ -6,6 +6,21 @@
 # @lc code=start
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        s,e,results,rooms = 0,0,0,0
+
+        start = sorted([i[0] for i in intervals])
+        end = sorted([i[1] for i in intervals])
         
+        while s<len(intervals):
+            if start[s]<end[e]:
+                s+=1
+                rooms+=1
+            else:
+                e+=1
+                rooms-=1
+            results = max(results,rooms)
+
+        return results
+
 # @lc code=end
 
