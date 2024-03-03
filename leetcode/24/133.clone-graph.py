8a8e2be3583b8f66aaf9b19a397e524d647e6fb2
mahsanghani <ghaniahsan@outlook.com>
2024-03-03 14:21:03 -0500

    clone graph

--- leetcode/24/133.clone-graph.py
+++ leetcode/24/133.clone-graph.py
@@ -8,15 +8,27 @@
 # Definition for a Node.
 class Node:
     def __init__(self, val = 0, neighbors = None):
-        
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
 
 from typing import Optional
 class Solution:
     def __init__(self):
-        
+        self.visit = {}
 
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return node
+        if node in self.visit:
+            return self.visit[node]
         
+        clone = Node(node.val)
+        self.visit[node] = clone
+
+        if node.neighbors:
+            clone.neighbors = [self.cloneGraph(node) for node in node.neighbors]
+        
+        return clone
 
 # @lc code=end
 
