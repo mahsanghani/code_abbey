edaa0cbd0b5f96cb1e14552e63002c91f8ef0a2a
mahsanghani <ghaniahsan@outlook.com>
2024-03-03 15:26:35 -0500

    dfs

---
+++ leetcode/24/332.reconstruct-itinerary.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=332 lang=python3
+#
+# [332] Reconstruct Itinerary
+#
+
+# @lc code=start
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        
+# @lc code=end
+
