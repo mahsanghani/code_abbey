74dabbde478c0f05d02cf7f101b4192f8f66cc5c
mahsanghani <ghaniahsan@outlook.com>
2024-03-03 15:15:20 -0500

    verifying alien dictionary

--- leetcode/24/953.verifying-an-alien-dictionary.py
+++ leetcode/24/953.verifying-an-alien-dictionary.py
@@ -6,7 +6,7 @@
 # @lc code=start
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        d = {c:i for i,c in enumerate(order)}
+        adj = {c:i for i,c in enumerate(order)}
 
         for i in range(len(words)-1):
             w1,w2 = words[i],words[i+1]
@@ -14,9 +14,9 @@ class Solution:
             for j in range(len(w1)):
                 if j==len(w2):
                     return False
-
+                
                 if w1[j]!=w2[j]:
-                    if d[w2[j]]<d[w1[j]]:
+                    if adj[w1[j]]>adj[w2[j]]:
                         return False
                     break
 
