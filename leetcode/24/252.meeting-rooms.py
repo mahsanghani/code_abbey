3b1b78252a4b48dded2dbd51fe2111c6008c133f
mahsanghani <ghaniahsan@outlook.com>
2024-03-03 18:17:54 -0500

    meetings rooms 1

--- leetcode/24/252.meeting-rooms.py
+++ leetcode/24/252.meeting-rooms.py
@@ -6,6 +6,14 @@
 # @lc code=start
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-                
+        intervals.sort()
+
+        for i in range(len(intervals)-1):
+            i1 = intervals[i]
+            i2 = intervals[i+1]
+            if i2[0]<i1[1]:
+                return False
+            
+        return True
 # @lc code=end
 
