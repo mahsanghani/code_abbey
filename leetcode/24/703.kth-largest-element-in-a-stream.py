194aa73d0b747748cd450ea9ede96c141d9bdf9c
mahsanghani <ghaniahsan@outlook.com>
2024-03-04 11:48:29 -0500

    kth largest

--- leetcode/24/703.kth-largest-element-in-a-stream.py
+++ leetcode/24/703.kth-largest-element-in-a-stream.py
@@ -7,13 +7,17 @@
 import heapq
 class KthLargest:
     def __init__(self, k: int, nums: List[int]):
-        self.k = self.k
-        self.nums = heapq.heapify(nums)
-        while len(self.nums)>self.k:
+        self.k = k
+        self.nums = nums
+        heapq.heapify(self.nums)
+        while len(self.nums) > self.k:
             heapq.heappop(self.nums)
 
     def add(self, val: int) -> int:
-        
+        heapq.heappush(self.nums, val)
+        if len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+        return self.nums[0]
 
 
 # Your KthLargest object will be instantiated and called as such:
