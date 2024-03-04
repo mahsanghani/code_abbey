1d10f02d007f5efdbaed3b6cb9b7e053bdc54025
mahsanghani <ghaniahsan@outlook.com>
2024-03-04 11:19:34 -0500

    heapq heapify

--- leetcode/24/703.kth-largest-element-in-a-stream.py
+++ leetcode/24/703.kth-largest-element-in-a-stream.py
@@ -4,9 +4,13 @@
 # [703] Kth Largest Element in a Stream
 #
 # @lc code=start
+import heapq
 class KthLargest:
     def __init__(self, k: int, nums: List[int]):
-        
+        self.k = self.k
+        self.nums = heapq.heapify(nums)
+        while len(self.nums)>self.k:
+            heapq.heappop(self.nums)
 
     def add(self, val: int) -> int:
         
