94f3c5034f328d46ca85e74e570b263d03480f83
mahsanghani <ghaniahsan@outlook.com>
2024-03-03 19:02:48 -0500

    meetings rooms 3

--- leetcode/24/2402.meeting-rooms-iii.py
+++ leetcode/24/2402.meeting-rooms-iii.py
@@ -4,8 +4,28 @@
 # [2402] Meeting Rooms III
 #
 # @lc code=start
+from heapq import heappush, heappop
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-        
+        used = []
+        unused = [i for i in range(n)]
+        counts = [0]*n
+        meetings.sort()
+
+        for s,e in meetings:
+            while used and s>=used[0][0]:
+                _, room = heappop(used)
+                heappush(unused, room)
+
+            if not unused:
+                time, room = heappop(used)
+                e = time + (e - s)
+                heappush(unused, room)
+
+            room = heappop(unused)
+            heappush(used, (e,room))
+            counts[room] += 1
+
+        return counts.index(max(counts))
 # @lc code=end
 
