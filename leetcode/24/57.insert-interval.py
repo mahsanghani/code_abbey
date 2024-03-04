6ac43f242973577bf3fd0ed27af56fb22d0a3436
mahsanghani <ghaniahsan@outlook.com>
2024-03-03 19:22:16 -0500

    insert interval

--- leetcode/24/57.insert-interval.py
+++ leetcode/24/57.insert-interval.py
@@ -9,11 +9,13 @@ class Solution:
         results = []
         intervals.append(newInterval)
         intervals.sort()
+
         for interval in intervals:
-            if not results or results[-1][1]<interval[0]:
+            if not results or results[-1][1] < interval[0]:
                 results.append(interval)
             else:
-                results[-1][1] = max(results[-1][1],interval[1])
+                results[-1][1] = max(results[-1][1], interval[1])
+
         return results
 # @lc code=end
 
