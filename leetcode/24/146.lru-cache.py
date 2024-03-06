d39f3076b4df20df0e7ab2cedabbfc1406b03ad9
mahsanghani <ghaniahsan@outlook.com>
2024-03-06 15:02:07 -0500

    ListNode LRU Cache

---
+++ leetcode/24/146.lru-cache.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=146 lang=python3
+#
+# [146] LRU Cache
+#
+# @lc code=start
+class ListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+
+    def get(self, key: int) -> int:
+        
+
+    def put(self, key: int, value: int) -> None:
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+# @lc code=end
+
--- leetcode/24/937.reorder-data-in-log-files.py
+++ leetcode/24/937.reorder-data-in-log-files.py
@@ -6,12 +6,14 @@
 # @lc code=start
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        return sorted(logs,key=self.sort)
-    def sort(self,logs):
-        a,b = logs.split(' ',1)
-        if b[0].isalpha():
-            return (0,b,a)
-        else:
-            return (1,None,None)
+        digits = []
+        letters = []
+
+        def sort(logs):
+            for log in logs:
+                id,rest = log.split(' ',1)
+                return (0,rest,id) if rest[0].isalpha() else (1,rest,id)
+            
+        return sorted(logs,key=sort)
 # @lc code=end
 
