294d6771f140a36929fe49828eb6d91229f2768f
mahsanghani <ghaniahsan@outlook.com>
2024-03-07 21:15:52 -0500

    two sum

--- leetcode/24/1.two-sum.py
+++ leetcode/24/1.two-sum.py
@@ -8,10 +8,10 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         d = {}
         for i,j in enumerate(nums):
-            comp = target-j
-            d[j] = i
+            comp = target - j
             if comp in d:
-                return (d[comp],i)
+                return [d[comp],i]
+            d[j] = i
         return []
 # @lc code=end
 
