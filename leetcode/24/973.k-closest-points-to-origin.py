2d66ab4dc720189043a6f589f27ce45712354a04
mahsanghani <ghaniahsan@outlook.com>
2024-03-07 21:05:42 -0500

    k closest points to origin

--- leetcode/24/973.k-closest-points-to-origin.py
+++ leetcode/24/973.k-closest-points-to-origin.py
@@ -4,8 +4,19 @@
 # [973] K Closest Points to Origin
 #
 # @lc code=start
+import math
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        distances = []
 
+        def euclidean(x,y):
+            return math.sqrt(x**2+y**2)
+
+        for point in points:
+            distances.append((euclidean(point[0],point[1]),point))
+
+        distances.sort(key=lambda x:x[0])
+
+        return [point for distance,point in distances[:k]] 
 # @lc code=end
 
