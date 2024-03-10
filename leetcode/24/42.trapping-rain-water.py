e30b6faf2e619762a16709db4ee91408c3bf9375
mahsanghani <ghaniahsan@outlook.com>
2024-03-10 18:41:05 -0400

    trapping rain water

--- leetcode/24/42.trapping-rain-water.py
+++ leetcode/24/42.trapping-rain-water.py
@@ -8,21 +8,20 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         if not height:
             return 0
-        
-        l,r = 0, len(height)-1
-        results = 0
-        leftmax, rightmax = height[l], height[r]
+
+        l,p,r = 0,0,len(height)-1
+        left,right = height[l],height[r]
 
         while l<r:
-            if leftmax<rightmax:
+            if left<right:
                 l+=1
-                leftmax = max(leftmax, height[l])
-                results += leftmax-height[l]
+                left = max(left,height[l])
+                p += left - height[l]
             else:
                 r-=1
-                rightmax = max(rightmax, height[r])
-                results += rightmax-height[r]
+                right = max(right,height[r])
+                p += right - height[r]
 
-        return results
+        return p
 # @lc code=end
 
