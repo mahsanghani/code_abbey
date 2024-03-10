c7e0ab8a7bb666455dfd677f2933e54c6aa7c79f
mahsanghani <ghaniahsan@outlook.com>
2024-03-10 18:48:55 -0400

    plates between candles

--- leetcode/24/2055.plates-between-candles-pfx-sum.py
+++ leetcode/24/2055.plates-between-candles-pfx-sum.py
@@ -6,22 +6,22 @@
 # @lc code=start
 class Solution:
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
-        res = []
-        pfx = [0]*(len(s)+1)
+        results = []
+        prefix = [0]*(len(s)+1)
         prev = [0]*(len(s)+1)
         next = [float('inf')]*(len(s)+1)
 
         for i,j in enumerate(s):
-            pfx[i+1] = pfx[i] + (j=='|')
+            prefix[i+1] = prefix[i] + (j=='|')
             prev[i+1] = i if j=='|' else prev[i]
 
         for i,j in reversed(list(enumerate(s))):
             next[i] = i if j=='|' else next[i+1]
 
         for q in queries:
-            l,r = next[q[0]], prev[q[1]+1]
-            res.append(r-l-(pfx[r]-pfx[l]) if l<r else 0)
+            left = next[q[0]]
+            right = prev[q[1]+1]
+            results.append(right-left-(prefix[right]-prefix[left]) if left<right else 0)
 
-        return res
-
+        return results
 # @lc code=end
