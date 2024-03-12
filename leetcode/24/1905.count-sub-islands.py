85c43cd9e04444ba274d76593f86e78d1bfbd7a9
mahsanghani <ghaniahsan@outlook.com>
2024-03-12 10:49:02 -0400

    count sub islands

--- leetcode/24/1905.count-sub-islands.py
+++ leetcode/24/1905.count-sub-islands.py
@@ -6,15 +6,20 @@
 # @lc code=start
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-        rows,cols,visit,islands=len(grid1),len(grid1[0]),set(),0
+        rows = len(grid1)
+        cols = len(grid1[0])
+        visit = set()
+        islands = 0
 
         def dfs(r,c):
-            if r<0 or c<0 or r==rows or c==cols or grid2[r][c]==0 or (r,c) in visit:
+            if r<0 or c<0 or r==rows or c==cols or (r,c) in visit or grid2[r][c]==0:
                 return True
+
             visit.add((r,c))
-            results=True
+            results = True
+
             if grid1[r][c]==0:
-                results=False
+                results = False
 
             results = dfs(r-1,c) and results
             results = dfs(r+1,c) and results
@@ -22,11 +27,12 @@ class Solution:
             results = dfs(r,c+1) and results
 
             return results
-        
+
         for r in range(rows):
             for c in range(cols):
                 if grid2[r][c] and (r,c) not in visit and dfs(r,c):
                     islands+=1
+
         return islands
 # @lc code=end
 
