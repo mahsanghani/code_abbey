02bb6cda2e7ba1dfc8b930dd24bfc2339909a4d8
mahsanghani <ghaniahsan@outlook.com>
2024-03-21 11:23:38 -0400

    graph valid tree

--- leetcode/24/261.graph-valid-tree.py
+++ leetcode/24/261.graph-valid-tree.py
@@ -8,27 +8,30 @@ class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         if not n:
             return True
-        
-        adj = {i:[] for i in range(n)}
-        
-        for j,k in edges:
-            adj[j].append(k)
-            adj[k].append(j)
-        
+
+        res = 0
         visit = set()
+        adj = {i:[] for i in range(n)}
+
+        for k,v in edges:
+            adj[k].append(v)
+            adj[v].append(k)
+
         def dfs(i,j):
             if i in visit:
                 return False
             
             visit.add(i)
+
             for k in adj[i]:
                 if k==j:
                     continue
                 if not dfs(k,i):
                     return False
-            
+
             return True
-        
+
         return dfs(0,-1) and n==len(visit)
+
 # @lc code=end
 
