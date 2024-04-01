82371392874d04cee94b30cd7d8a71879f625cd1
mahsanghani <ghaniahsan@outlook.com>
2024-04-01 13:57:39 -0400

    sum root to leaf numbers

--- leetcode/24/129.sum-root-to-leaf-numbers.py
+++ leetcode/24/129.sum-root-to-leaf-numbers.py
@@ -12,14 +12,13 @@ class TreeNode:
         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        total = 0
-        def dfs(node,num):
+        def dfs(node, num):
             if not node:
                 return 0
             num = num*10 + node.val
             if not node.left and not node.right:
                 return num
             return dfs(node.left,num) + dfs(node.right,num)
-        return dfs(root, 0)
+        return dfs(root,0)
 # @lc code=end
 
