e18b166f4025b1028d45026ccdf10496f9737501
mahsanghani <ghaniahsan@outlook.com>
2024-04-01 12:21:35 -0400

    unique binary search trees

---
+++ leetcode/24/96.unique-binary-search-trees.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=96 lang=python3
+#
+# [96] Unique Binary Search Trees
+#
+# @lc code=start
+class Solution:
+    def numTrees(self, n: int) -> int:
+        trees = [1]*(n+1)
+
+        for nodes in range(2,n+1):
+            total = 0
+            for root in range(1,nodes+1):
+                left = root-1
+                right = nodes-root
+                total += trees[left] * trees[right]
+            trees[nodes] = total
+        return trees[n]
+# @lc code=end
+
