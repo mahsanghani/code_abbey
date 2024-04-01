38f776b030cbcb46a737a77a1eda2795321736ac
mahsanghani <ghaniahsan@outlook.com>
2024-04-01 13:39:53 -0400

    dynamic programming

---
+++ leetcode/24/95.unique-binary-search-trees-II.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=94 lang=python3
+#
+# [94] Binary Tree Inorder Traversal
+#
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        results = []
+        def dfs(node):
+            if not node:
+                return None
+            dfs(node.left)
+            results.append(node.val)
+            dfs(node.right)
+        dfs(root)
+        return results
+# @lc code=end
+
