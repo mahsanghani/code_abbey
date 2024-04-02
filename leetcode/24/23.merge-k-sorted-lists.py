9415796afe9fbad825f3b12671072b2233cb98ff
mahsanghani <ghaniahsan@outlook.com>
2024-04-02 18:26:38 -0400

    merge k sorted lists

--- leetcode/24/23.merge-k-sorted-lists.py
+++ leetcode/24/23.merge-k-sorted-lists.py
@@ -16,32 +16,32 @@ class Solution:
 
         while len(lists)>1:
             merged = []
-
+            
             for i in range(0,len(lists),2):
                 list1 = lists[i]
                 list2 = lists[i+1] if (i+1)<len(lists) else None
-                merged.append(self.mergeLists(list1,list2))
+                merged.append(self.merge2lists(list1,list2))
             lists = merged
         return lists[0]
 
-    def mergeLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    def merge2lists(self,list1,list2):
         dummy = ListNode()
-        tail = dummy
+        temp = dummy
 
         while list1 and list2:
             if list1.val < list2.val:
-                tail.next = list1
+                temp.next = list1
                 list1 = list1.next
             else:
-                tail.next = list2
+                temp.next = list2
                 list2 = list2.next
-            tail = tail.next
+            temp = temp.next
 
         if list1:
-            tail.next = list1
-        elif list2:
-            tail.next = list2
-        
+            temp.next = list1
+        if list2:
+            temp.next = list2
+
         return dummy.next
 
 # @lc code=end
