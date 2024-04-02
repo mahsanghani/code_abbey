26033503bfcd5edc8d8d0aebb88df161c6c65bd4
mahsanghani <ghaniahsan@outlook.com>
2024-04-02 13:28:09 -0400

    serialize

--- leetcode/24/297.serialize-and-deserialize-binary-tree.py
+++ leetcode/24/297.serialize-and-deserialize-binary-tree.py
@@ -12,14 +12,23 @@ class TreeNode(object):
         self.right = None
 
 class Codec:
-
     def serialize(self, root):
         """Encodes a tree to a single string.
         
         :type root: TreeNode
         :rtype: str
         """
-        
+        res = []
+
+        def dfs(node):
+            if not node:
+                res.append('N')
+                return node
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return ','.join(res)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
