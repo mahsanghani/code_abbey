ea2e163a8dfc22c2937c973e91158a1e9acb07d1
mahsanghani <ghaniahsan@outlook.com>
2024-04-19 19:16:02 -0400

    maximum number of accepted invitations

--- leetcode/24/1820.maximum-number-of-accepted-invitations.py
+++ leetcode/24/1820.maximum-number-of-accepted-invitations.py
@@ -8,14 +8,22 @@ from collections import defaultdict
 
 class Solution:
     def maximumInvitations(self, grid):
-        rows = len(grid)
-        cols = len(grid[0])
+        boys = len(grid)
+        girls = len(grid[0])
         kids = defaultdict(int)
 
         def dfs(boy, visit):
+            for girl in range(girls):
+                if grid[boy][girl] and girl not in visit:
+                    visit.add(girl)
+
+                    if girl not in kids or dfs(kids[girl],visit):
+                        kids[girl] = boy
+                        return True
+                    
             return False
 
-        for boy in range(rows):
+        for boy in range(boys):
             dfs(boy,set())
 
         return len(kids)
