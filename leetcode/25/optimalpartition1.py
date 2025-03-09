4a1ae4ca8c693d5dd7ae0a8915a0745e50eaa970
mahsanghani <ghaniahsan@outlook.com>
2025-03-09 01:32:43 -0500

    optimal partition of a string

---
+++ leetcode/25/optimalpartition1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def partitionString(self, s: str) -> int:
+        results = []
+        unique = set()
+        for c in s:
+            if c in unique:
+                results.append(unique)
+                unique = set(c)
+            else:
+                unique.add(c)
+        results.append(unique)
+        return len(results)
