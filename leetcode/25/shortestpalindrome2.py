df0adde0a3ab2da466c51c098dfc803af34c1f18
mahsanghani <ghaniahsan@outlook.com>
2025-05-06 12:31:26 -0400

    shortest palindrome

--- leetcode/25/shortestpalindrome2.py
+++ leetcode/25/shortestpalindrome2.py
@@ -1,11 +1,22 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
         length = len(s)
-        rvrsd = s[::-1]
-        
-        for i in range(length):
-            if s[:length-i] == rvrsd[i:]:
-                return rvrsd[:i]+s
+        if length == 0:
+            return s
 
-        return ""
-    
+        left = 0
+        for right in range(length-1,-1,-1):
+            if s[right] == s[left]:
+                left += 1
+
+        if left == length:
+            return s
+
+        non_palindrome_suffix = s[left:]
+        reverse_suffix = non_palindrome_suffix[::-1]
+
+        return (
+            reverse_suffix +
+            self.shortestPalindrome(s[:left]) +
+            non_palindrome_suffix
+        )
