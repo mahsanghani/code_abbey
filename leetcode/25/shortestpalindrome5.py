03d36dd0a0f3635793003e7e547c20a878ea55cb
mahsanghani <ghaniahsan@outlook.com>
2025-05-06 13:40:56 -0400

    shortest palindrome

--- leetcode/25/shortestpalindrome5.py
+++ leetcode/25/shortestpalindrome5.py
@@ -1,11 +1,36 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-        length = len(s)
-        rvrsd = s[::-1]
-        
-        for i in range(length):
-            if s[:length-i] == rvrsd[i:]:
-                return rvrsd[:i]+s
+        if not s:
+            return s
 
-        return ""
-    
+        modified = self.preprocess(s)
+        n = len(modified)
+        palindrome_radius_array = [0]*n
+        center = 0
+        right_boundary = 0
+        max_palindrome_length = 0
+
+        for i in range(1,n-1):
+            mirror_index = 2*center-i
+            if right_boundary>i:
+                palindrome_radius_array[i] = min(
+                    right_boundary-i, palindrome_radius_array[mirror_index]
+                )
+
+            while (
+                modified[i+1+palindrome_radius_array[i]] == modified[i-1-palindrome_radius_array[i]]):
+                palindrome_radius_array[i] += 1
+
+            if i+palindrome_radius_array[i] > right_boundary:
+                center = i
+                right_boundary = i+palindrome_radius_array[i]
+
+            if i-palindrome_radius_array[i]==1:
+                max_palindrome_length = max(
+                    max_palindrome_length,palindrome_radius_array[i])
+
+        suffix = s[max_palindrome_length:][::-1]
+        return suffix+s
+        
+    def preprocess(self, s: str) -> str:
+        return "^" + "#" + "#".join(s) + "#$"
