650c1d94eb785703daba98b3c81100bfa7e740ee
mahsanghani <ghaniahsan@outlook.com>
2025-05-12 15:22:42 -0400

    grumpy bookstore owner

---
+++ leetcode/25/grumpybookstore4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        base = sum(customers[i] for i in range(len(customers)) if grumpy[i]==0)
+        curr = sum(customers[i] for i in range(minutes) if grumpy[i])
+        extra = curr
+
+        for i in range(minutes,len(customers)):
+            if grumpy[i-minutes]:
+                curr -= customers[i-minutes]
+
+            if grumpy[i]:
+                curr += customers[i]
+
+            extra = max(curr, extra)
+
+        return base + extra
