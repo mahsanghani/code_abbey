e89b72890fe7d0e21956d3e4e6e7a233856a3bee
mahsanghani <ghaniahsan@outlook.com>
2025-05-21 09:55:07 -0400

    greedy flourist

---
+++ leetcode/25/greedyflourist.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the getMinimumCost function below.
+def getMinimumCost(k, c):
+    cost = 0
+    c.sort(reverse=True)
+    
+    for i in range(len(c)):
+        multiplier = i//k+1
+        cost += c[i]*multiplier
+        
+    return cost
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    minimumCost = getMinimumCost(k, c)
+
+    fptr.write(str(minimumCost) + '\n')
+
+    fptr.close()
