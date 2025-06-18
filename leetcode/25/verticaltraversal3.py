5c95c9ba21aa2d84e0bb42a892dfb6bc46d4a3c1
mahsanghani <ghaniahsan@outlook.com>
2025-06-18 10:46:28 -0400

    binary tree vertical order traversal

---
+++ leetcode/25/verticaltraversal3.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        cols = defaultdict(list)
+        queue = deque([(root, 0)])
+        minCol = 0
+        maxCol = 0
+        
+        while queue:
+            node, col = queue.popleft()
+            cols[col].append(node.val)
+            minCol = min(minCol, col)
+            maxCol = max(maxCol, col)
+
+            if node.left:
+                queue.append((node.left, col-1))
+            if node.right:
+                queue.append((node.right, col+1))
+                
+        return [cols[col] for col in range(minCol, maxCol+1)]
